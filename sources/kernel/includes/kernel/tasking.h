a1b41788de16ac97f479b7c8fd316fc66e457d41
Nicolas Van Bossuyt <nicovb30@hotmail.com>
2018-08-27 19:22:09 +0200

    Added SCRENNSHOT

--- sources/kernel/includes/kernel/tasking.h
+++ sources/kernel/includes/kernel/tasking.h
@@ -1,16 +1,12 @@
 #pragma once
 #include "types.h"
 
-
-
-
 #define TASK_MAX_COUNT 16
 #define TASK_NAME_SIZE 64
 #define TASK_STACK_SIZE 4096
 
 typedef u32 esp_t;
 typedef void (*thread_entry_t)();
-
 
 typedef PACKED(struct)
 {
--- sources/test-app/sources/main.c
+++ sources/test-app/sources/main.c
@@ -3,7 +3,5 @@
 int main(int argc, const char ** argv)
 {
     syscall_print("Hello world!\n");
-    syscall_print("Hello world!\n");
-    syscall_print("Hello world!\n");
     syscall_exit(0);
 }

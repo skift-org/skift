58e2025121a00850168f6530a417b9e1fe690312
Nicolas Van Bossuyt <nicolas.van.bossuyt@gmail.com>
2018-10-10 07:46:37 +0200

    Fixing circular dependancies

---
+++ sources/libc-shared/includes/skift/types.h
@@ -0,0 +1,15 @@
+#pragma once
+
+typedef char s8;
+typedef short s16;
+typedef long s32;
+typedef long long s64;
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned long u32;
+typedef unsigned long long u64;
+
+typedef unsigned char uchar;
+typedef unsigned short ushort;
+typedef unsigned int uint;

cc57abb04605b921e7af13619f694d12d578e41e
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2018-10-21 22:22:41 +0200

    Working on PROTOCOL

--- pakages/maker.skift.kernel/includes/kernel/ipc.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include <skift/types.h>
-#include "kernel/tasking.h"
--- pakages/maker.skift.kernel/sources/ipc.c
+++
@@ -1,5 +0,0 @@
-/* Copyright © 2018 MAKER.                                                    */
-/* This code is licensed under the MIT License.                               */
-/* See: LICENSE.md                                                            */
-
-#include "kernel/ipc.h"

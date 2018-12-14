54819ccd300ab4599d448eea5c4ac3977f310159
VAN BOSSUYT Nicolas <nicovb30@gmail.com>
2018-12-14 13:19:40 +0100

    Update file header

--- pakages/maker.skift.kernel/includes/kernel/system.h
+++ pakages/maker.skift.kernel/includes/kernel/system.h
@@ -1,6 +1,6 @@
 #pragma once
 
-/* Copyright © 2018 MAKER.                                                    */
+/* Copyright © 2018-2019 MAKER.                                               */
 /* This code is licensed under the MIT License.                               */
 /* See: LICENSE.md                                                            */
 
--- pakages/maker.skift.kernel/sources/cpu/irq.c
+++ pakages/maker.skift.kernel/sources/cpu/irq.c
@@ -1,4 +1,4 @@
-/* Copyright © 2018 MAKER.                                                    */
+/* Copyright © 2018-2019 MAKER.                                               */
 /* This code is licensed under the MIT License.                               */
 /* See: LICENSE.md                                                            */
 

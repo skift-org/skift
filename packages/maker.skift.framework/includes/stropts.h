b2226b3716772e27b248d80ade0ed7271b5da65c
VAN BOSSUYT Nicolas <nicovb30@gmail.com>
2019-02-07 19:58:03 +0100

    Refactoring skift.framework

---
+++ packages/maker.skift.framework/includes/stropts.h
@@ -0,0 +1,8 @@
+#pragma once
+
+/* Copyright © 2018-2019 MAKER.                                               */
+/* This code is licensed under the MIT License.                               */
+/* See: LICENSE.md                                                            */
+
+/* stropts.h: stream operations.                                              */
+

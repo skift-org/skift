559503e0cd20a1c1c062d23a82c76e1c5bbcf770
VAN BOSSUYT Nicolas <nicovb30@gmail.com>
2019-03-06 18:43:04 +0100

    Bump version

--- packages/skift.hjert.kernel/sources/version.c
+++ packages/skift.hjert.kernel/sources/version.c
@@ -7,9 +7,9 @@
 char *__kernel_name = "hjert";
 
 int __kernel_version_major = 0;
-int __kernel_version_minor = 1;
+int __kernel_version_minor = 2;
 int __kernel_version_patch = 0;
-char *__kernel_version_codename = "quinoa";
+char *__kernel_version_codename = "wheat";
 
 /* kernel version format major.minor.patch-codename */
 char *__kernel_version_format = "%d.%d.%d-%s @ " __COMMIT__;

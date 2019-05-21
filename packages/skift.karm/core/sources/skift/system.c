9d8694959e4efd5a6e6c009e34eacd302e367ef2
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2019-05-21 20:41:47 +0200

    Framework: disabled caching for system_get_info

--- packages/skift.karm/core/sources/skift/system.c
+++ packages/skift.karm/core/sources/skift/system.c
@@ -2,18 +2,9 @@
 #include <skift/cstring.h>
 #include <skift/system.h>
 
-static bool system_info_cached = false;
-static system_info_t system_info_cache = {0};
-
 void system_get_info(system_info_t* info)
 {
-    if (!system_info_cached)
-    {
-        __plug_system_get_info(&system_info_cache);
-        system_info_cached = true;
-    }
-
-    memcpy(info, &system_info_cache, sizeof(system_info_t));
+    __plug_system_get_info(info);
 }
 
 void system_get_status(system_status_t* status)

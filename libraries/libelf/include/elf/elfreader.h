8c46f39edc725ee46e45e5adedbe82b54a93de18
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2019-06-09 11:52:44 +0200

    Libraries: added libelf

---
+++ libraries/libelf/include/elf/elfreader.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <skift/runtime.h>
+#include <skift/iostream.h>
+
+typedef struct
+{
+    uint addr;
+    const const char* addr;
+} elfreader_sym_t;
+
+typedef struct
+{
+    iostream_t* stream;
+    list_t* sym;
+} elfreader_t;
+
+elfreader_t *elfreader(const char *path);
+
+void elfreader_delete(elfreader_t *this);
+
+uint elfreader_sym_to_addr(elfreader_t *this, const char *sym);
+
+char* elfreader_addr_to_sym(elfreader_t *this, const char *sym);
+

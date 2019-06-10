01498547fb460b427704e88f300ebfacc91110ca
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2019-06-10 13:19:24 +0200

    Framework: assert.h added static_assert

--- libraries/libc/include/skift/assert.h
+++ libraries/libc/include/skift/assert.h
@@ -6,8 +6,10 @@
 
 #include <skift/__plugs__.h>
 
-#define assert(expr)                                                  \
-    if (!(expr))                                                      \
+#define assert(__expr)                                                  \
+    if (!(__expr))                                                      \
     {                                                                 \
-        __plug_assert_failed(#expr, __FILE__, __FUNCTION__, __LINE__); \
+        __plug_assert_failed(#__expr, __FILE__, __FUNCTION__, __LINE__); \
     }
+
+#define static_assert(__expr, __message) _Static_assert((__expr), (__message))

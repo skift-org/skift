6e45444e44a09e42889a68671aa22f7c6b469ee5
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2019-06-15 23:36:28 +0200

    Window Manager: don't repaint the whole screen

--- userspace/wm.c
+++ userspace/wm.c
@@ -14,6 +14,9 @@ typedef struct
 {
     point_t position;
     mouse_cursor_state_t state;
+    bool has_mask;
+    painter_t *paint;
+    bitmap_t *mask;
 } wmmouse_t;
 
 typedef struct
@@ -70,38 +73,90 @@ void wmanager_open_display(wmanager_t *wm)
     wm->paint = painter(wm->framebuffer);
 }
 
+void wmanager_damage_region(wmanager_t* wm, rectangle_t region)
+{
+    framebuffer_region_t damaged_region = (framebuffer_region_t){.src = wm->framebuffer->buffer, .bound = region};
+    iostream_ioctl(wm->device, FRAMEBUFFER_IOCTL_BLITREGION, &damaged_region);
+}
+
+rectangle_t wmanager_mouse_bound(wmmouse_t *mouse)
+{
+    rectangle_t damage;
+
+    damage.position = point_add(mouse->position, (point_t){-28, -28});
+    damage.size = (point_t){56, 56};
+    
+    return damage;
+}
+
 void wmanager_handle_mouse_move_event(wmanager_t *wm, mouse_move_event_t *event)
 {
-    UNUSED(wm); UNUSED(event);
-    wm->mouse.position = point_add(wm->mouse.position, (point_t){event->offx, event->offy});
+    wmmouse_t *mouse = &wm->mouse;
 
-    painter_blit_bitmap(wm->paint, wm->wallpaper, bitmap_bound(wm->wallpaper), bitmap_bound(wm->framebuffer));
-    
+    // Cleanup the backbuffer
+    if (mouse->has_mask)
+    {
+        painter_blit_bitmap(wm->paint, mouse->mask, bitmap_bound(mouse->mask), wmanager_mouse_bound(mouse));
+        wmanager_damage_region(wm, wmanager_mouse_bound(mouse));
+    }
+
+    // Move the mouse
+    wm->mouse.position = point_add(wm->mouse.position, (point_t){event->offx, event->offy});
+    wm->mouse.position = point_clamp(wm->mouse.position, point_zero, bitmap_bound(wm->framebuffer).size);
+
+    // Save the backbuffer
+    painter_blit_bitmap(mouse->paint, wm->framebuffer, wmanager_mouse_bound(mouse), bitmap_bound(mouse->mask));
+    mouse->has_mask = true;
+
+    // Draw the mouse cursor to the backbuffer
+    point_t offset;
+    switch (mouse->state)
+    {
+    case MOUSE_CURSOR_STATE_TEXT:
+        offset = (point_t){-14, 0};
+        break;
+
+    case MOUSE_CURSOR_STATE_BUSY:
+    case MOUSE_CURSOR_STATE_MOVE:
+    case MOUSE_CURSOR_STATE_RESIZEH:
+    case MOUSE_CURSOR_STATE_RESIZEV:
+    case MOUSE_CURSOR_STATE_RESIZEHV:
+    case MOUSE_CURSOR_STATE_RESIZEVH:
+        offset = (point_t){-14, -14};
+        break;
+
+    default:
+        offset = (point_t){0, 0};
+        break;
+    }
+
     painter_blit_bitmap(
         wm->paint,
-        wm->cursors[wm->mouse.state],
-        bitmap_bound(wm->cursors[wm->mouse.state]),
-        (rectangle_t){.position = wm->mouse.position, .size = bitmap_bound(wm->cursors[wm->mouse.state]).size});
+        wm->cursors[mouse->state],
+        bitmap_bound(wm->cursors[mouse->state]),
+        (rectangle_t){.position = point_add(wm->mouse.position, offset), .size = bitmap_bound(wm->cursors[mouse->state]).size});
 
+    wmanager_damage_region(wm, wmanager_mouse_bound(mouse));
 }
 
 void wmanager_handle_mouse_scroll_event(wmanager_t *wm, mouse_scroll_event_t *event)
 {
-    UNUSED(wm); UNUSED(event);
-
+    UNUSED(wm);
+    UNUSED(event);
 }
 
 void wmanager_handle_mouse_btnup_event(wmanager_t *wm, mouse_button_event_t *event)
 {
-    UNUSED(wm); UNUSED(event);
-
+    UNUSED(wm);
+    UNUSED(event);
 }
 
 void wmanager_handle_mouse_btndown_event(wmanager_t *wm, mouse_button_event_t *event)
 {
-    UNUSED(wm); UNUSED(event);
+    UNUSED(wm);
+    UNUSED(event);
 
-    wm->mouse.state = (wm->mouse.state + 1) % MOUSE_CURSOR_STATE_COUNT; 
+    wm->mouse.state = (wm->mouse.state + 1) % MOUSE_CURSOR_STATE_COUNT;
 }
 
 int main(int argc, char **argv)
@@ -111,15 +166,21 @@ int main(int argc, char **argv)
 
     wmanager_t wm;
     wm.exited = false;
-    wm.mouse.position = (point_t){0,0};
-    wm.mouse.state = MOUSE_CURSOR_STATE_DEFAULT;
 
     wmanager_load_assets(&wm);
     wmanager_open_display(&wm);
 
+    wm.mouse.position = (point_t){wm.framebuffer->width / 2, wm.framebuffer->height / 2};
+    wm.mouse.state = MOUSE_CURSOR_STATE_DEFAULT;
+    wm.mouse.has_mask = false;
+    wm.mouse.mask = bitmap(56, 56);
+    wm.mouse.paint = painter(wm.mouse.mask);
+
     messaging_subscribe(MOUSE_CHANNEL);
     messaging_subscribe(KEYBOARD_CHANNEL);
-    
+
+    painter_blit_bitmap(wm.paint, wm.wallpaper, bitmap_bound(wm.wallpaper), bitmap_bound(wm.framebuffer));
+
     while (!wm.exited)
     {
         message_t message;
@@ -145,8 +206,6 @@ int main(int argc, char **argv)
         {
             logger_log(LOG_WARNING, "Unknow message %s!", message_label(message));
         }
-
-        iostream_ioctl(wm.device, FRAMEBUFFER_IOCTL_BLIT, wm.framebuffer->buffer);
     }
 
     return 0;

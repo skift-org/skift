87d9489a6cfa495f6d71815e6c85659e4788b2d7
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2019-07-30 22:45:22 +0200

    framework: added skift/memory.h

--- include/skift/memory.h
+++ include/skift/memory.h
@@ -6,7 +6,17 @@
 
 #include <skift/runtime.h>
 
-int shared_memory_alloc(int pagecount);
+void memory_zero(void *where, uint how_many);
+
+void memory_set(void *where, byte what, uint how_many);
+
+void memory_uset(void *where, ubyte what, uint how_many);
+
+void memory_copy(void *from, uint from_size, void *to, uint to_size);
+
+void memory_move(void *from, uint from_size, void *to, uint to_size);
+
+int shared_memory_alloc(uint how_many_pages);
 
 int shared_memory_acquire(int shm, uint *addr);
 

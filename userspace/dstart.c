cfde0c26cb2e2550c35b56e8c48cc289c582beb7
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-01-26 14:27:09 +0100

    userspace: Dstart use launchpad now.

--- userspace/dstart.c
+++ userspace/dstart.c
@@ -4,36 +4,31 @@
 
 /* dstart.c: start a process as a daemon                                      */
 
-#include <abi/Process.h>
-
-#include <libsystem/error.h>
 #include <libsystem/io/Stream.h>
-#include <libsystem/process.h>
+#include <libsystem/process/Launchpad.h>
 
 int main(int argc, char const *argv[])
 {
-    if (argc >= 2)
+    if (argc < 2)
     {
-        const char *deamon_argv[PROCESS_ARG_COUNT];
-
-        for (int i = 0; i < PROCESS_ARG_COUNT - 1; i++)
-        {
-            deamon_argv[i] = argv[i + 1];
-        }
-
-        if (process_exec(argv[1], deamon_argv) < 0)
-        {
-            error_print("Failled to start deamon");
-            return -1;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-    else
-    {
-        printf("No executable specified!\n");
+        stream_printf(err_stream, "dstart: No executable specified!\n");
         return -1;
     }
+
+    Launchpad *launchpad = launchpad_create(argv[1], argv[1]);
+
+    for (int i = 0; i < argc - 1; i++)
+    {
+        launchpad_argument(launchpad, argv[i + 1]);
+    }
+
+    int result = launchpad_launch(launchpad);
+
+    if (result < 0)
+    {
+        stream_printf(err_stream, "dstart: Failled to start %s: %s\n", argv[1], error_to_string(-result));
+        return -1;
+    }
+
+    return 0;
 }

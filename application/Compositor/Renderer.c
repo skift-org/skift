31d5f2f36c7429e1fcf3639cc034453c7f752aa1
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-02-15 20:52:33 +0100

    Compositor: Added wallpaper.

--- application/Compositor/Renderer.c
+++ application/Compositor/Renderer.c
@@ -7,11 +7,13 @@
 
 static framebuffer_t *_framebuffer;
 static Painter *_painter;
+static Bitmap *_wallpaper;
 
 void renderer_initialize(void)
 {
     _framebuffer = framebuffer_open();
     _painter = _framebuffer->painter;
+    _wallpaper = bitmap_load_from("/res/wallpaper/brand.png");
 
     renderer_region(framebuffer_bound(_framebuffer));
     renderer_blit();
@@ -19,7 +21,7 @@ void renderer_initialize(void)
 
 void renderer_region(Rectangle region)
 {
-    painter_fill_rect(_painter, region, COLOR_CORNFLOWERBLUE);
+    painter_blit_bitmap(_painter, _wallpaper, region, region);
 
     list_foreach(Window, window, manager_get_windows())
     {

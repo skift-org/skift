09bd892f5173254a5777a9a5688eed2dd99c229c
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-05-22 01:30:06 +0200

    Applications: Added layout preview tool.

---
+++ applications/layout-viewer/.build.mk
@@ -0,0 +1,4 @@
+APPS += LAYOUT_VIEWER
+
+LAYOUT_VIEWER_NAME = layout-viewer
+LAYOUT_VIEWER_LIBS = widget markup json graphic math
---
+++ applications/layout-viewer/main.c
@@ -0,0 +1,25 @@
+#include <libsystem/BuildInfo.h>
+#include <libwidget/Application.h>
+#include <libwidget/Button.h>
+#include <libwidget/Container.h>
+#include <libwidget/Image.h>
+#include <libwidget/Label.h>
+#include <libwidget/Markup.h>
+
+int main(int argc, char **argv)
+{
+    if (argc >= 2)
+    {
+        application_initialize(argc, argv);
+
+        Window *window = window_create_from_file(argv[1]);
+
+        window_show(window);
+
+        return application_run();
+    }
+    else
+    {
+        return -1;
+    }
+}

6be2270a962a33183b37868bc3b32efb082e4d4e
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-07-15 14:59:31 +0200

    BGA Driver: Added support for changing resolution.

--- kernel/devices/BGA.c
+++ kernel/devices/BGA.c
@@ -12,8 +12,12 @@
 
 #define VBE_DISPI_BANK_ADDRESS 0xA0000
 #define VBE_DISPI_BANK_SIZE_KB 64
-#define VBE_DISPI_MAX_XRES 1024
-#define VBE_DISPI_MAX_YRES 768
+
+#define VBE_DISPI_DEFAULT_XRES 1368
+#define VBE_DISPI_DEFAULT_YRES 768
+#define VBE_DISPI_MAX_XRES 1920
+#define VBE_DISPI_MAX_YRES 1080
+
 #define VBE_DISPI_IOPORT_INDEX 0x01CE
 #define VBE_DISPI_IOPORT_DATA 0x01CF
 #define VBE_DISPI_INDEX_ID 0x0
@@ -35,6 +39,11 @@
 
 #define VBE_DISPI_LFB_PHYSICAL_ADDRESS 0xE0000000
 
+static uintptr_t framebuffer_physical = 0;
+static uintptr_t framebuffer_virtual = 0;
+static int framebuffer_width = 0;
+static int framebuffer_height = 0;
+
 void bga_write_register(u16 IndexValue, u16 DataValue)
 {
     out16(VBE_DISPI_IOPORT_INDEX, IndexValue);
@@ -54,10 +63,10 @@ void bga_set_mode(u32 width, u32 height)
     bga_write_register(VBE_DISPI_INDEX_YRES, height);
     bga_write_register(VBE_DISPI_INDEX_BPP, 32);
     bga_write_register(VBE_DISPI_INDEX_ENABLE, VBE_DISPI_ENABLED | VBE_DISPI_LFB_ENABLED);
-}
 
-static uintptr_t framebuffer_physical = 0;
-static uintptr_t framebuffer_virtual = 0;
+    framebuffer_width = width;
+    framebuffer_height = height;
+}
 
 Result bga_iocall(FsNode *node, FsHandle *handle, IOCall iocall, void *args)
 {
@@ -68,8 +77,8 @@ Result bga_iocall(FsNode *node, FsHandle *handle, IOCall iocall, void *args)
     {
         IOCallDisplayModeArgs *mode = (IOCallDisplayModeArgs *)args;
 
-        mode->width = VBE_DISPI_MAX_XRES;
-        mode->height = VBE_DISPI_MAX_YRES;
+        mode->width = framebuffer_width;
+        mode->height = framebuffer_height;
 
         return SUCCESS;
     }
@@ -77,9 +86,9 @@ Result bga_iocall(FsNode *node, FsHandle *handle, IOCall iocall, void *args)
     {
         IOCallDisplayBlitArgs *blit = (IOCallDisplayBlitArgs *)args;
 
-        for (int y = MAX(0, blit->blit_y); y < MIN(VBE_DISPI_MAX_YRES, blit->blit_y + blit->blit_height); y++)
+        for (int y = MAX(0, blit->blit_y); y < MIN(framebuffer_height, blit->blit_y + blit->blit_height); y++)
         {
-            for (int x = MAX(0, blit->blit_x); x < MIN(VBE_DISPI_MAX_XRES, blit->blit_x + blit->blit_width); x++)
+            for (int x = MAX(0, blit->blit_x); x < MIN(framebuffer_width, blit->blit_x + blit->blit_width); x++)
             {
                 uint32_t pixel = blit->buffer[y * blit->buffer_width + x];
 
@@ -87,10 +96,16 @@ Result bga_iocall(FsNode *node, FsHandle *handle, IOCall iocall, void *args)
                                            ((pixel)&0xff00ff00) |
                                            ((pixel << 16) & 0x00ff0000);
 
-                ((uint32_t *)framebuffer_virtual)[y * VBE_DISPI_MAX_XRES + x] = converted_pixel;
+                ((uint32_t *)framebuffer_virtual)[y * framebuffer_width + x] = converted_pixel;
             }
         }
 
+        return SUCCESS;
+    }
+    else if (iocall == IOCALL_DISPLAY_SET_MODE)
+    {
+        IOCallDisplayModeArgs *mode = (IOCallDisplayModeArgs *)args;
+        bga_set_mode(mode->width, mode->height);
         return SUCCESS;
     }
     else
@@ -107,7 +122,7 @@ bool bga_match(DeviceInfo info)
 
 void bga_initialize(DeviceInfo info)
 {
-    bga_set_mode(VBE_DISPI_MAX_XRES, VBE_DISPI_MAX_YRES);
+    bga_set_mode(VBE_DISPI_DEFAULT_XRES, VBE_DISPI_DEFAULT_YRES);
     framebuffer_physical = pci_device_read(info.pci_device, PCI_BAR0, 4);
     framebuffer_virtual = virtual_alloc(
                               &kpdir,

88b25faa914ad04ce84f18a5b4a85dd4d0d9fbb1
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-07-22 17:26:10 +0200

    LibSystem: Fixed a stack overflow in memset.

--- libraries/libsystem/.build.mk
+++ libraries/libsystem/.build.mk
@@ -1,3 +1,4 @@
 LIBS += SYSTEM
 
 SYSTEM_NAME = system
+SYSTEM_CFLAGS = -fno-tree-loop-distribute-patterns
--- libraries/libsystem/CString.c
+++ libraries/libsystem/CString.c
@@ -129,11 +129,11 @@ void *memcpy(void *s1, const void *s2, size_t n)
 
 void *memset(void *str, int c, size_t n)
 {
-    char *s = (char *)str;
+    uint8_t *s = (uint8_t *)str;
 
     for (size_t i = 0; i < n; i++)
     {
-        *(s + i) = c;
+        *(s + i) = (uint8_t)c;
     }
 
     return str;

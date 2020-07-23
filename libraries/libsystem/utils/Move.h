1aa74e40643698f49664b85d13aba3a1496a3a6e
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-07-23 23:58:47 +0200

    LibSYstem: Updated move() and

--- libraries/libsystem/utils/Move.h
+++ libraries/libsystem/utils/Move.h
@@ -1,9 +1,39 @@
 #pragma once
 
 template <typename T>
-T &&move(T &arg)
+struct Identity
 {
-    return static_cast<T &&>(arg);
+    using Type = T;
+};
+
+template <typename T>
+struct Identity<T &>
+{
+    using Type = T;
+};
+
+template <typename T>
+struct Identity<T &&>
+{
+    using Type = T;
+};
+
+template <typename T>
+constexpr typename Identity<T>::Type &&move(T &&arg)
+{
+    return static_cast<typename Identity<T>::Type &&>(arg);
+}
+
+template <class T>
+constexpr T &&forward(typename Identity<T>::Type &param)
+{
+    return static_cast<T &&>(param);
+}
+
+template <class T>
+constexpr T &&forward(typename Identity<T>::Type &&param)
+{
+    return static_cast<T &&>(param);
 }
 
 template <typename T>
@@ -12,16 +42,4 @@ void swap(T &left, T &right)
     T tmp = move(left);
     left = move(right);
     right = move(tmp);
-}
-
-template <typename T>
-struct Identity
-{
-    typedef T Type;
-};
-
-template <class T>
-constexpr T &&forward(typename Identity<T>::Type &param)
-{
-    return static_cast<T &&>(param);
 }

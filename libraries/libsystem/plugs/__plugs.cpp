f2ff3a11533dd739e09e375a79aab529684c547c
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-07-25 14:13:03 +0200

    LibSystem: Call __init_array().

--- libraries/libsystem/plugs/__plugs.cpp
+++ libraries/libsystem/plugs/__plugs.cpp
@@ -30,6 +30,13 @@ void __plug_init(void)
     log_stream = stream_open_handle(3, OPEN_WRITE | OPEN_BUFFERED);
 
     _init();
+
+    extern void (*__init_array_start[])(int, char **, char **) __attribute__((visibility("hidden")));
+    extern void (*__init_array_end[])(int, char **, char **) __attribute__((visibility("hidden")));
+
+    const size_t size = __init_array_end - __init_array_start;
+    for (size_t i = 0; i < size; i++)
+        (*__init_array_start[i])(0, NULL, NULL);
 }
 
 void __plug_fini(int exit_code)

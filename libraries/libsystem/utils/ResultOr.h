6c892f40a394dd2e35836f86c227f14d04f04e41
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-07-30 12:45:01 +0200

    libutils: Added ResultOr template.

---
+++ libraries/libsystem/utils/ResultOr.h
@@ -0,0 +1,46 @@
+#include <libsystem/Result.h>
+#include <libsystem/utils/Move.h>
+
+template <typename T>
+class ResultOr
+{
+private:
+    Result _result;
+    T _value;
+
+public:
+    bool success() { return _result == SUCCESS; }
+
+    T &&take_value() { return move(_value); }
+
+    Result result() { return _result; }
+
+    ResultOr(Result result) : _result(result), _value() {}
+
+    ResultOr(T value) : _result(SUCCESS), _value(value) {}
+
+    ResultOr(Result result, T value) : _result(result), _value(value) {}
+
+    template <typename U>
+    ResultOr(ResultOr<U> result, T value) : _result(result.result()), _value(value) {}
+
+    bool operator==(Result result)
+    {
+        return _result == result;
+    }
+
+    bool operator!=(Result result)
+    {
+        return _result != result;
+    }
+
+    bool operator==(T value)
+    {
+        return _value == value;
+    }
+
+    bool operator!=(T value)
+    {
+        return _value != value;
+    }
+};

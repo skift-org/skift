ea0236a386dcec35d25fa1894b4493636afd68e9
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-08-03 00:07:43 +0200

    build: Improved assets inclusion for applications.

--- applications/.build.mk
+++ applications/.build.mk
@@ -5,20 +5,25 @@ $(1)_BINARY  = $(BUILD_DIRECTORY_APPS)/$($(1)_NAME)/$($(1)_NAME)
 $(1)_SOURCES = $$(wildcard applications/$($(1)_NAME)/*.cpp) \
 			   $$(wildcard applications/$($(1)_NAME)/*/*.cpp)
 
-$(1)_OTHER = $$(wildcard applications/$($(1)_NAME)/*.markup) \
-			 $$(wildcard applications/$($(1)_NAME)/*.json) \
-			 $$(wildcard applications/$($(1)_NAME)/*.png)
+$(1)_ASSETS := $$(wildcard applications/$($(1)_NAME)/*.markup) \
+			   $$(wildcard applications/$($(1)_NAME)/*.json) \
+			   $$(wildcard applications/$($(1)_NAME)/*.png)
+
+$(1)_ASSETS := $$(patsubst applications/$($(1)_NAME)/%, $(BUILD_DIRECTORY_APPS)/$($(1)_NAME)/%, $$($(1)_ASSETS))
 
 $(1)_OBJECTS = $$(patsubst applications/%.cpp, $$(BUILD_DIRECTORY)/%.o, $$($(1)_SOURCES))
 
-TARGETS += $$($(1)_BINARY)
+TARGETS += $$($(1)_BINARY) $$($(1)_ASSETS)
 OBJECTS += $$($(1)_OBJECTS)
 ICONS += $$($(1)_ICONS)
+
+$(BUILD_DIRECTORY_APPS)/$($(1)_NAME)/%: applications/$($(1)_NAME)/%
+	$$(DIRECTORY_GUARD)
+	cp $$< $$@
 
 $$($(1)_BINARY): $$($(1)_OBJECTS) $$(patsubst %, $$(BUILD_DIRECTORY_LIBS)/lib%.a, $$($(1)_LIBS) system) $(CRTS)
 	$$(DIRECTORY_GUARD)
 	@echo [$(1)] [LD] $($(1)_NAME)
-	@cp $$($(1)_OTHER) $(BUILD_DIRECTORY_APPS)/$($(1)_NAME) || true
 	@$(CC) $(LDFLAGS) -o $$@ $$($(1)_OBJECTS) $$(patsubst %, -l%, $$($(1)_LIBS))
 
 $$(BUILD_DIRECTORY)/$$($(1)_NAME)/%.o: applications/$$($(1)_NAME)/%.cpp

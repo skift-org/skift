1c74a7d467de0e5f85ce18b2f289ecf3413ac6b6
Jakob Kirsch <jakob.kirsch@web.de>
2020-08-02 16:34:37 +0200

    Kill: Make command working

--- coreutils/kill.cpp
+++ coreutils/kill.cpp
@@ -1,9 +1,31 @@
 #include <libsystem/process/Process.h>
+#include <libsystem/utils/NumberParser.h>
+
+int kill(const char * id)
+{
+    int i = parse_uint_inline(PARSER_DECIMAL, id, -1);
+    if (i == -1)
+    {
+        return -1;
+    }
+    return process_cancel(i);
+}
 
 int main(int argc, char **argv)
 {
-    __unused(argc);
-    __unused(argv);
+    if (argc >= 2)
+    {
+        int result = 0;
+
+        for (int i = 1; i < argc; i++)
+        {
+            result |= kill(argv[i]);
+        }
+
+        return result;
+    }
+
+    return -1;
 
     return 0;
 }

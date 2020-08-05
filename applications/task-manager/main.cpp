6e7faf74d9f53d0317121da1cc0d81788e705e17
Cael Rasmussen <53895123+caelwithcats@users.noreply.github.com>
2020-08-05 23:21:11 +1000

    task-manager: Show the most greedest process

--- applications/task-manager/main.cpp
+++ applications/task-manager/main.cpp
@@ -16,7 +16,7 @@ struct TaskManagerWindow
     Timer *ram_timer;
     Widget *cpu_graph;
     Timer *cpu_timer;
-    
+
     // --- CPU/RAM stats --- //
     Widget *ram_usage;
     Widget *ram_avaliable;
@@ -30,20 +30,58 @@ struct TaskManagerWindow
     Timer *table_timer;
 };
 
+const char* get_greedy_process(TaskModel *model, int ram_cpu) {
+    const char* greedy = "";
+
+    int row_count = json_array_length(model->data);
+    int list[row_count];
+    for (int row = 0; row < row_count; row++)
+    {
+        // 0 means memory. 1 means processor
+        if (ram_cpu == 0)
+        {
+          JsonValue *task = json_array_get(model->data, (size_t)row);
+          list[row] = json_integer_value(json_object_get(task, "ram"));
+        }
+        else if (ram_cpu == 1)
+        {
+          JsonValue *task = json_array_get(model->data, (size_t)row);
+          list[row] = json_integer_value(json_object_get(task, "cpu"));
+        }
+    }
+    int greedy_index = 0;
+    for (int i = 0; i < row_count; i++) {
+        if (list[0] < list[i]) {
+            greedy_index = i;
+        }
+    }
+    JsonValue *task = json_array_get(model->data, (size_t)greedy_index);
+    greedy = json_string_value(json_object_get(task, "name"));
+    return greedy;
+}
+
 void widget_ram_update(TaskManagerWindow *window)
 {
     SystemStatus status = system_get_status();
-    
+
     graph_record((Graph*)window->ram_graph, status.used_ram / (double)status.total_ram);
-    
+
     char buffer1[200];
     char buffer2[200];
+    // This not ideal, when dealing with program names longer than 400 characters. Hopefully that shouldn't happen
+    char buffer3[400];
+
     int usage = (int)status.used_ram / 1024 / 1024;
     int avaliable = (int)status.total_ram / 1024 / 1024;
+    const char* greedy =  get_greedy_process(window->table_model, 0);
+
     snprintf(buffer1, 200, "Usage: %i Mio", usage);
     snprintf(buffer2, 200, "Avaliable: %i Mio", avaliable);
+    snprintf(buffer3, 400, "Most greedy: %s", greedy);
+
     label_set_text(window->ram_usage, buffer1);
     label_set_text(window->ram_avaliable, buffer2);
+    label_set_text(window->ram_greedy, buffer3);
 }
 
 void widget_cpu_update(TaskManagerWindow *window)
@@ -51,11 +89,16 @@ void widget_cpu_update(TaskManagerWindow *window)
     SystemStatus status = system_get_status();
 
     graph_record((Graph*)window->cpu_graph, status.cpu_usage / 100.0);
-    
-    char buffer[200];
+
+    char buffer1[200];
+    char buffer2[400];
+
+    const char* greedy = get_greedy_process(window->table_model, 1);
     int percentage = (int)status.cpu_usage;
-    snprintf(buffer, 200, "Average: %i%%", percentage);
-    label_set_text(window->cpu_average, buffer);
+    snprintf(buffer1, 200, "Average: %i%%", percentage);
+    snprintf(buffer2, 400, "Most greedy: %s", greedy);
+    label_set_text(window->cpu_average, buffer1);
+    label_set_text(window->cpu_greedy, buffer2);
 }
 
 void widget_table_update(TaskManagerWindow *window)
@@ -119,7 +162,7 @@ int main(int argc, char **argv)
     cpu_icon_and_text->layout = HFLOW(4);
     icon_panel_create(cpu_icon_and_text, Icon::get("memory"));
     label_create(cpu_icon_and_text, "Processor");
-    
+
     window->cpu_timer = timer_create(window, 100, (TimerCallback)widget_cpu_update);
     timer_start(window->cpu_timer);
 
@@ -134,12 +177,20 @@ int main(int argc, char **argv)
     ram_icon_and_text->layout = HFLOW(4);
     icon_panel_create(ram_icon_and_text, Icon::get("chip"));
     label_create(ram_icon_and_text, "Memory");
-    
+
+    container_create(window->cpu_graph)->layout_attributes = LAYOUT_FILL;
+
     window->ram_usage = label_create(window->ram_graph, "Usage: nil Mio");
+    label_set_text_position(window->ram_usage, Position::RIGHT);
     window->ram_avaliable = label_create(window->ram_graph, "Avaliable: nil Mio");
-    window->ram_greedy = label_create(window->ram_graph, "Most greedy: the compositor eats a lot");
+    label_set_text_position(window->ram_avaliable, Position::RIGHT);
+    window->ram_greedy = label_create(window->ram_graph, "Most greedy: nil");
+    label_set_text_position(window->ram_greedy, Position::RIGHT);
+
     window->cpu_average = label_create(window->cpu_graph, "Average: nil%");
-    window->cpu_greedy = label_create(window->cpu_graph, "Most greedy: nil");
+    label_set_text_position(window->cpu_average, Position::BOTTOM_RIGHT);
+    window->cpu_greedy = label_create(window->cpu_graph, "Most greedy: the compositor eats a lot");
+    label_set_text_position(window->cpu_greedy, Position::BOTTOM_RIGHT);
 
     window->ram_timer = timer_create(window, 500, (TimerCallback)widget_ram_update);
     timer_start(window->ram_timer);

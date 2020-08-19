47de65cd7db80de2b0e57952ce2daf1959af940d
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-08-19 15:16:23 +0200

    libutils: Added a constructor for creating a string storage from two other StringStorage.

--- libraries/libutils/StringStorage.h
+++ libraries/libutils/StringStorage.h
@@ -24,6 +24,17 @@ public:
         _buffer[length] = '\0';
     }
 
+    StringStorage(StringStorage &left, StringStorage &right)
+    {
+        _length = left.length() + right.length();
+        _buffer = new char[_length + 1];
+
+        memcpy(_buffer, left.cstring(), left.length());
+        memcpy(_buffer + left.length(), right.cstring(), right.length());
+
+        _buffer[left.length() + right.length()] = '\0';
+    }
+
     ~StringStorage()
     {
         delete _buffer;

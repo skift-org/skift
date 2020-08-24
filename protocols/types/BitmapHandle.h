ef15ceffa14c35c6e07a01fa54b422559eaeaac2
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-08-24 18:23:26 +0200

    meta: Added a simple ipc code generator.

---
+++ protocols/types/BitmapHandle.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <libsystem/math/Vec2.h>
+
+struct BitmapHandle
+{
+    int memory_object;
+    Vec2i size;
+};
---
+++ protocols/types/CursorState.h
@@ -0,0 +1,16 @@
+#pragma once
+
+enum CursorState
+{
+    CURSOR_DEFAULT,
+    CURSOR_TEXT,
+    CURSOR_DISABLED,
+    CURSOR_BUSY,
+    CURSOR_MOVE,
+    CURSOR_RESIZEH,
+    CURSOR_RESIZEHV,
+    CURSOR_RESIZEV,
+    CURSOR_RESIZEVH,
+
+    __CURSOR_COUNT
+};

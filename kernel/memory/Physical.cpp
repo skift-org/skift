644ca7f4b1216ad993d3454b1df2d53a3f857ff2
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-09-09 03:42:03 +0200

    kernel: Added a best bet to the physical memory allocator.

--- kernel/memory/Physical.cpp
+++ kernel/memory/Physical.cpp
@@ -8,6 +8,7 @@
 size_t TOTAL_MEMORY = 0;
 size_t USED_MEMORY = 0;
 
+size_t best_bet = 0;
 uint8_t MEMORY[1024 * 1024 / 8] = {};
 
 bool physical_page_is_used(uintptr_t address)
@@ -21,12 +22,22 @@ void physical_page_set_used(uintptr_t address)
 {
     uintptr_t page = address / ARCH_PAGE_SIZE;
 
+    if (page == best_bet)
+    {
+        best_bet++;
+    }
+
     MEMORY[page / 8] |= 1 << (page % 8);
 }
 
 void phyisical_page_set_used(uintptr_t address)
 {
     uintptr_t page = address / ARCH_PAGE_SIZE;
+
+    if (page < best_bet)
+    {
+        best_bet = page;
+    }
 
     MEMORY[page / 8] &= ~(1 << (page % 8));
 }
@@ -37,7 +48,7 @@ MemoryRange physical_alloc(size_t size)
 
     assert(IS_PAGE_ALIGN(size));
 
-    for (size_t i = 0; i < ((TOTAL_MEMORY - size) / ARCH_PAGE_SIZE); i++)
+    for (size_t i = best_bet; i < ((TOTAL_MEMORY - size) / ARCH_PAGE_SIZE); i++)
     {
         MemoryRange range(i * ARCH_PAGE_SIZE, size);
 

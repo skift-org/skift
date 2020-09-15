866b091a188a3a06a0779fb2ae1005daad1cbe20
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-09-15 10:11:12 +0200

    kernel: Fixed virtio device matching.

--- kernel/devices/VirtioDevice.h
+++ kernel/devices/VirtioDevice.h
@@ -30,13 +30,22 @@ public:
 
     bool match(DeviceAddress address)
     {
-        uint16_t vendor = address.pci().read16(PCI_VENDOR_ID);
-        uint16_t device = address.pci().read16(PCI_DEVICE_ID);
-        uint16_t subsystem = address.pci().read16(PCI_SUBSYSTEM_ID);
+        if (address.pci().read16(PCI_VENDOR_ID) != 0x1AF4)
+        {
+            return false;
+        }
 
-        return vendor == 0x1AF4 &&
-               device >= 0x1000 && device < 0x1040 &&
-               subsystem == _address;
+        uint16_t device = address.pci().read16(PCI_DEVICE_ID);
+
+        if (device >= 0x1000 && device < 0x1040)
+        {
+            uint16_t subsystem = address.pci().read16(PCI_SUBSYSTEM_ID);
+            return subsystem == _address;
+        }
+        else
+        {
+            return (device - 0x1040) == _address;
+        }
     }
 
     RefPtr<Device> instance(DeviceAddress address)

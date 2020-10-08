ae4974a9421d3d5a67277f0148659c487b2d13e6
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-10-08 12:56:52 +0200

    build: Use the right compiler for x86_64.

--- arch/x86_32/.build.mk
+++ arch/x86_32/.build.mk
@@ -1,3 +1,9 @@
+CC:=i686-pc-skift-gcc
+CXX:=i686-pc-skift-g++
+LD:=i686-pc-skift-ld
+AR:=i686-pc-skift-ar
+AS=nasm
+
 KERNEL_SOURCES += \
 	$(wildcard arch/x86/*.cpp) \
 	$(wildcard arch/x86_32/*.cpp)
--- arch/x86_64/.build.mk
+++ arch/x86_64/.build.mk
@@ -1,3 +1,9 @@
+CC:=x86_64-pc-skift-gcc
+CXX:=x86_64-pc-skift-g++
+LD:=x86_64-pc-skift-ld
+AR:=x86_64-pc-skift-ar
+AS=nasm
+
 KERNEL_SOURCES += \
 	$(wildcard arch/x86/*.cpp) \
 	$(wildcard arch/x86_64/*.cpp)

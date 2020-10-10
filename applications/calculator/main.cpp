ce580f014c1c389b237d7d0472bcdff3f0f6afac
Supercip971 <supercyp971@gmail.com>
2020-10-10 08:33:14 -0400

    calculator: better button name + [...] remove window dependecies with the calculator

--- applications/calculator/main.cpp
+++ applications/calculator/main.cpp
@@ -1,3 +1,4 @@
+#include "libsystem/Macros.h"
 #include <libsystem/BuildInfo.h>
 #include <libsystem/utils/NumberFormatter.h>
 #include <libwidget/Application.h>
@@ -7,11 +8,18 @@
 
 Window *window;
 
-#define CALCULATOR_BUTTON(stringcodename, is_number, code)         \
-    target->with_widget<Button>(stringcodename, [&](auto button) { \
-        button->on(Event::ACTION, [&](auto) {                      \
-            calculator_update(is_number, code);                    \
-        });                                                        \
+#define CALCULATOR_BUTTON(stringcodename, is_number, code)                       \
+    window->with_widget<Button>(stringcodename, [&](auto button) {               \
+        button->on(Event::ACTION, [&](auto) {                                    \
+            main_calculator_system.calculator_update(is_number, code);           \
+                                                                                 \
+            if (main_calculator_system.buffer_changed_and_clear())               \
+            {                                                                    \
+                window->with_widget<Label>(CALC_TEXT_BOX_NAME, [&](auto label) { \
+                    label->text((buffer));                                       \
+                });                                                              \
+            }                                                                    \
+        });                                                                      \
     });
 
 enum CALCULATOR_OPERATOR
@@ -25,72 +33,50 @@ enum CALCULATOR_OPERATOR
     OPERATOR_CLEAR_ALL = 6
 };
 
-#define CALC_ID_BUTTON_0 "0"
-#define CALC_ID_BUTTON_1 "1"
-#define CALC_ID_BUTTON_2 "2"
-#define CALC_ID_BUTTON_3 "3"
-#define CALC_ID_BUTTON_4 "4"
-#define CALC_ID_BUTTON_5 "5"
-#define CALC_ID_BUTTON_6 "6"
-#define CALC_ID_BUTTON_7 "7"
-#define CALC_ID_BUTTON_8 "8"
-#define CALC_ID_BUTTON_9 "9"
-#define CALC_ID_BUTTON_PLUS "plus"
-#define CALC_ID_BUTTON_MINUS "minus"
-#define CALC_ID_BUTTON_EQUAL "equal"
-#define CALC_ID_BUTTON_CLEAR "clear"
-#define CALC_TEXT_BOX_NAME "calcscreen"
+#define CALC_ID_BUTTON_0 "calculator_button_0"
+#define CALC_ID_BUTTON_1 "calculator_button_1"
+#define CALC_ID_BUTTON_2 "calculator_button_2"
+#define CALC_ID_BUTTON_3 "calculator_button_3"
+#define CALC_ID_BUTTON_4 "calculator_button_4"
+#define CALC_ID_BUTTON_5 "calculator_button_5"
+#define CALC_ID_BUTTON_6 "calculator_button_6"
+#define CALC_ID_BUTTON_7 "calculator_button_7"
+#define CALC_ID_BUTTON_8 "calculator_button_8"
+#define CALC_ID_BUTTON_9 "calculator_button_9"
+#define CALC_ID_BUTTON_PLUS "calculator_button_plus"
+#define CALC_ID_BUTTON_MINUS "calculator_button_minus"
+#define CALC_ID_BUTTON_EQUAL "calculator_button_equal"
+#define CALC_ID_BUTTON_CLEAR "calculator_button_clear"
+#define CALC_TEXT_BOX_NAME "calculator_screen"
 
 class calculator_system
 {
-
-    Window *target;
+private:
     double main_number = 0;
     double second_number = 0;
     int selectionned_number = 0;
     int current_number_count = 0;
     double current_operator = 0;
     bool do_reset_after = true;
+    bool buffer_changed = false;
     int calculator_text_length = 0;
-    char calculator_text[32];
+    char *calculator_text;
 
 public:
-    void init_button()
-    {
-
-        CALCULATOR_BUTTON(CALC_ID_BUTTON_0, true, 0)
-        CALCULATOR_BUTTON(CALC_ID_BUTTON_1, true, 1)
-        CALCULATOR_BUTTON(CALC_ID_BUTTON_2, true, 2)
-        CALCULATOR_BUTTON(CALC_ID_BUTTON_3, true, 3)
-        CALCULATOR_BUTTON(CALC_ID_BUTTON_4, true, 4)
-        CALCULATOR_BUTTON(CALC_ID_BUTTON_5, true, 5)
-        CALCULATOR_BUTTON(CALC_ID_BUTTON_6, true, 6)
-        CALCULATOR_BUTTON(CALC_ID_BUTTON_7, true, 7)
-        CALCULATOR_BUTTON(CALC_ID_BUTTON_8, true, 8)
-        CALCULATOR_BUTTON(CALC_ID_BUTTON_9, true, 9)
-        CALCULATOR_BUTTON(CALC_ID_BUTTON_PLUS, false, OPERATOR_ADD);
-        CALCULATOR_BUTTON(CALC_ID_BUTTON_MINUS, false, OPERATOR_SUBSTRACT);
-        CALCULATOR_BUTTON(CALC_ID_BUTTON_EQUAL, false, OPERATOR_EQUAL);
-        CALCULATOR_BUTTON(CALC_ID_BUTTON_CLEAR, false, OPERATOR_CLEAR);
-    }
-
     calculator_system()
     {
     }
-    calculator_system(Window *target_window)
+    calculator_system(char *result_buffer, int calculator_buffer_length)
     {
-        target = target_window;
-        calculator_text_length = 32;
+        buffer_changed = true;
+        calculator_text = result_buffer;
+        //  target = target_window;
+        calculator_text_length = calculator_buffer_length;
         for (int i = 0; i < calculator_text_length; i++)
         {
             calculator_text[i] = ' ';
         }
         calculator_text[calculator_text_length - 1] = 0;
-
-        window->with_widget<Label>(CALC_TEXT_BOX_NAME, [&](auto label) {
-            label->text("0");
-        });
-        init_button();
     };
 
     void update_calculator_text()
@@ -103,11 +89,15 @@ public:
         {
             format_double(FORMAT_DECIMAL, second_number, (char *)calculator_text, 32);
         }
-        window->with_widget<Label>(CALC_TEXT_BOX_NAME, [&](auto label) {
-            label->text((calculator_text));
-        });
-    }
 
+        buffer_changed = true;
+    }
+    bool buffer_changed_and_clear()
+    {
+        bool result = buffer_changed;
+        buffer_changed = false;
+        return result;
+    }
     void enter_switch_operator()
     {
         if (selectionned_number == 1)
@@ -209,7 +199,28 @@ int main(int argc, char **argv)
 {
     application_initialize(argc, argv);
     window = window_create_from_file("/Applications/calculator/calculator.markup");
-    main_calculator_system = calculator_system(window);
+
+    char *buffer = reinterpret_cast<char *>(malloc(32));
+    main_calculator_system = calculator_system(buffer, 32);
+
+    CALCULATOR_BUTTON(CALC_ID_BUTTON_0, true, 0)
+    CALCULATOR_BUTTON(CALC_ID_BUTTON_1, true, 1)
+    CALCULATOR_BUTTON(CALC_ID_BUTTON_2, true, 2)
+    CALCULATOR_BUTTON(CALC_ID_BUTTON_3, true, 3)
+    CALCULATOR_BUTTON(CALC_ID_BUTTON_4, true, 4)
+    CALCULATOR_BUTTON(CALC_ID_BUTTON_5, true, 5)
+    CALCULATOR_BUTTON(CALC_ID_BUTTON_6, true, 6)
+    CALCULATOR_BUTTON(CALC_ID_BUTTON_7, true, 7)
+    CALCULATOR_BUTTON(CALC_ID_BUTTON_8, true, 8)
+    CALCULATOR_BUTTON(CALC_ID_BUTTON_9, true, 9)
+    CALCULATOR_BUTTON(CALC_ID_BUTTON_PLUS, false, OPERATOR_ADD);
+    CALCULATOR_BUTTON(CALC_ID_BUTTON_MINUS, false, OPERATOR_SUBSTRACT);
+    CALCULATOR_BUTTON(CALC_ID_BUTTON_EQUAL, false, OPERATOR_EQUAL);
+    CALCULATOR_BUTTON(CALC_ID_BUTTON_CLEAR, false, OPERATOR_CLEAR);
+
+    window->with_widget<Label>(CALC_TEXT_BOX_NAME, [&](auto label) {
+        label->text((buffer));
+    });
     window->show();
 
     return application_run();

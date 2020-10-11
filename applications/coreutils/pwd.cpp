2e8ef6e83906869b84431eedb74815b19a5a1312
Cael Rasmussen <53895123+caelwithcats@users.noreply.github.com>
2020-10-11 12:16:59 +1100

    coreutils: add pwd

---
+++ applications/coreutils/pwd.cpp
@@ -0,0 +1,11 @@
+#include <libsystem/io/Stream.h>
+#include <libsystem/process/Process.h>
+
+int main(int argc, char **argv)
+{
+    __unused(argc);
+    __unused(argv);
+    char buffer[PATH_LENGTH];
+    process_get_directory(buffer, PATH_LENGTH);
+    printf("%s", buffer);
+}

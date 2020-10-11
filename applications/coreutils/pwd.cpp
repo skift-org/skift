493ea4895f01e61206a390f6c87332357b7cf596
Cael Rasmussen <53895123+caelwithcats@users.noreply.github.com>
2020-10-11 22:46:28 +1100

    coreutils: pwd, fix some formatting issues

--- applications/coreutils/pwd.cpp
+++ applications/coreutils/pwd.cpp
@@ -5,7 +5,10 @@ int main(int argc, char **argv)
 {
     __unused(argc);
     __unused(argv);
+
     char buffer[PATH_LENGTH];
     process_get_directory(buffer, PATH_LENGTH);
     printf("%s", buffer);
+
+    return 0;
 }

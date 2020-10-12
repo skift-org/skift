4d927c1b1c023b79c1cc8f74b615c3c87188c107
Jimil Desai <jimildesai42@gmail.com>
2020-10-13 00:47:58 +0530

    code refractor

--- applications/coreutils/basename.cpp
+++ applications/coreutils/basename.cpp
@@ -2,7 +2,7 @@
 #include <libsystem/io/Path.h>
 #include <libsystem/io/Stream.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
     if (argc == 1)
     {
@@ -13,12 +13,16 @@ int main(int argc, char *argv[])
     Path *path = path_create(argv[1]);
     const char *basename = path_filename(path);
 
-    if (basename == NULL)
+    if (basename == nullptr)
     {
         if (strcmp(argv[1], "") == 0)
+        {
             printf(".");
+        }
         else
+        {
             printf("/");
+        }
     }
     else
     {

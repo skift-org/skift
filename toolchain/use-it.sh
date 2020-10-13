8eccf63cdf7bedb3aae1e3526d719a3882b30cd8
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-10-13 23:23:55 +0200

    toolchaine: Cleanup use-it.sh.

--- toolchain/use-it.sh
+++ toolchain/use-it.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export PATH="$DIR/local/bin:$PATH"
-export TOOLCHAIN="$DIR"
-export SYSROOT="$DIR/../build/sysroot"
 
-export SKIFT_TOOLCHAIN="$DIR"
-export SKIFT_SYSROOT="$DIR/../build/sysroot"
-export SKIFT_CONTRIBROOT="$DIR/../contrib"
+export SKIFT_SOURCEROOT  = "$DIR/.."
+export SKIFT_TOOLCHAIN   = "$SKIFT_SOURCEROOT/toolchain"
+export SKIFT_SYSROOT     = "$SKIFT_SOURCEROOT/sysroot"
+export SKIFT_CONTRIBROOT = "$SKIFT_SOURCEROOT/contrib"
 
 echo "$DIR/local/bin"

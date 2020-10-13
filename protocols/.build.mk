eda09e1f2abb37ca7fd0788083ea5b0bbf225837
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-10-13 17:03:54 +0200

    ipc-compiler: Generate ServerConnection and ClientConnection.h.

--- protocols/.build.mk
+++ protocols/.build.mk
@@ -3,15 +3,23 @@ PROTOCOLS=$(wildcard protocols/*.ipc)
 PROTOCOLS_SOURCES= \
 	$(patsubst %.ipc,%Protocol.h, $(PROTOCOLS)) \
 	$(patsubst %.ipc,%Server.h, $(PROTOCOLS)) \
-	$(patsubst %.ipc,%Client.h, $(PROTOCOLS))
+	$(patsubst %.ipc,%Client.h, $(PROTOCOLS)) \
+	$(patsubst %.ipc,%ServerConnection.h, $(PROTOCOLS)) \
+	$(patsubst %.ipc,%ClientConnection.h, $(PROTOCOLS))
 
 %Protocol.h: %.ipc
-	ipc-compiler.py --protocol $< | clang-format > $@
+	ipc-compiler.py --protocol $< > $@
 
 %Server.h: %.ipc
-	ipc-compiler.py --server $< | clang-format > $@
+	ipc-compiler.py --server $< > $@
 
 %Client.h: %.ipc
-	ipc-compiler.py --client $< | clang-format > $@
+	ipc-compiler.py --client $< > $@
+
+%ServerConnection.h: %.ipc
+	ipc-compiler.py --server-connection $< > $@
+
+%ClientConnection.h: %.ipc
+	ipc-compiler.py --client-connection $< > $@
 
 TARGETS+=$(PROTOCOLS_SOURCES)
--- toolbox/ipc-compiler.py
+++ toolbox/ipc-compiler.py
@@ -6,7 +6,7 @@ import pprint
 
 from utils.Lexer import Lexer
 from utils.Generator import Generator
-from ipc import Parser, EmitProtocol, EmitPeer
+from ipc import Parser, EmitProtocol, EmitPeer, EmitConnection
 
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -31,5 +31,9 @@ elif sys.argv[1] == "--server" and "server" in prot:
     EmitPeer.peer(gen, prot, prot["server"])
 elif sys.argv[1] == "--client" and "client" in prot:
     EmitPeer.peer(gen, prot, prot["client"])
+elif sys.argv[1] == "--client-connection" and "client" in prot:
+    EmitConnection.connection(gen, prot, prot["client"])
+elif sys.argv[1] == "--server-connection" and "client" in prot:
+    EmitConnection.connection(gen, prot, prot["server"])
 
 print(gen.finalize())

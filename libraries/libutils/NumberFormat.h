5e4ecbd09636dd0d97d4b22e8209fedba73c6ee9
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-10-15 18:04:26 +0200

    libutils: Added number formater.

---
+++ libraries/libutils/NumberFormat.h
@@ -0,0 +1,104 @@
+#pragma once
+
+#include <libsystem/math/Math.h>
+#include <libutils/StringBuilder.h>
+
+struct NumberFormat
+{
+    int _base;
+    int _precision;
+    bool _capitalized;
+
+    static NumberFormat binary()
+    {
+        return {2, 4, false};
+    }
+
+    static NumberFormat octal()
+    {
+        return {8, 4, false};
+    }
+
+    static NumberFormat decimal()
+    {
+        return {10, 4, false};
+    }
+
+    static NumberFormat hexadecimal()
+    {
+        return {16, 4, false};
+    }
+
+    NumberFormat capitalized()
+    {
+        NumberFormat copy = *this;
+        copy._capitalized = true;
+        return copy;
+    }
+
+    static constexpr const char *XDIGITS = "0123456789abcdefghijklmnopqrstuvwxyz";
+    static constexpr const char *XDIGITS_CAPITALIZED = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    void format(StringBuilder &builder, unsigned int value)
+    {
+        if (value == 0)
+        {
+            builder.append('0');
+            return;
+        }
+
+        size_t i = 0;
+        char buffer[64] = {};
+
+        while (value != 0)
+        {
+            if (_capitalized)
+            {
+                buffer[i] = XDIGITS[value % _base];
+            }
+            else
+            {
+                buffer[i] = XDIGITS_CAPITALIZED[value % _base];
+            }
+
+            value /= _base;
+            i++;
+        }
+
+        strrvs(buffer);
+
+        builder.append(buffer, i);
+    }
+
+    void format(StringBuilder &builder, int value)
+    {
+        if (value < 0)
+        {
+            builder.append('-');
+            value = -value;
+        }
+
+        format(builder, (unsigned int)value);
+    }
+
+    void format(StringBuilder &builder, float value)
+    {
+        format(builder, (double)value);
+    }
+
+    void format(StringBuilder &builder, double value)
+    {
+        int ipart = (int)value;
+        float fpart = value - (float)ipart;
+
+        format(builder, ipart);
+
+        if (_precision > 0)
+        {
+            builder.append('.');
+
+            int ifpart = fpart * pow(_base, _precision);
+            format(builder, ifpart);
+        }
+    }
+};

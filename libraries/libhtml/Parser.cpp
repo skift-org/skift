d47dbd087625b9b16ca7ec048169993319482699
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-10-21 23:38:37 +0200

    libhtml: More element parsing.

--- libraries/libhtml/Parser.cpp
+++ libraries/libhtml/Parser.cpp
@@ -1,5 +1,7 @@
 #include <libhtml/Parser.h>
 
+// https://www.w3.org/TR/2012/WD-html5-20120329/index.html#contents
+
 namespace html
 {
 
@@ -7,6 +9,9 @@ static constexpr const char *WHITESPACE = " \n\r\t";
 static constexpr const char *COMMENT_START = "<!--";
 static constexpr const char *COMMENT_END = "-->";
 static constexpr const char *DOCTYPE_START = "<!DOCTYPE";
+static constexpr const char *NEWLINE = "\r\n";
+static constexpr const char *CLOSING_TAG = "</";
+static constexpr const char *SELF_CLOSING_TAG = "/>";
 
 void whitespace(Lexer &lexer)
 {
@@ -54,7 +59,7 @@ void attribute(Lexer &lexer)
     __unused(lexer);
 }
 
-void start_tag(Lexer &lexer)
+RefPtr<Node> start_tag(Lexer &lexer)
 {
     lexer.skip('<');
 
@@ -62,17 +67,39 @@ void start_tag(Lexer &lexer)
 
     attribute(lexer);
 
+    if (lexer.current_is_word(SELF_CLOSING_TAG))
+    {
+        lexer.skip_word(SELF_CLOSING_TAG);
+    }
+    else
+    {
+        lexer.skip('>');
+    }
+}
+
+void end_tag(Lexer &lexer, RefPtr<Node> node)
+{
+    lexer.skip_word(CLOSING_TAG);
+    __unused(lexer);
+
     lexer.skip('>');
 }
 
-void end_tag(Lexer &lexer)
+RefPtr<Node> element(Lexer &lexer)
 {
-    __unused(lexer);
-}
+    auto node = start_tag(lexer);
 
-void element(Lexer &lexer)
-{
-    __unused(lexer);
+    if (!node->self_closing())
+    {
+        while (!lexer.current_is_word(CLOSING_TAG))
+        {
+            node->append(element(lexer));
+        }
+
+        end_tag(lexer, node);
+    }
+
+    return node;
 }
 
 RefPtr<Node> parse(Lexer &lexer)
--- libraries/libhtml/dom/Node.h
+++ libraries/libhtml/dom/Node.h
@@ -9,8 +9,12 @@ class Node : public RefCounted<Node>
 {
 private:
     Vector<RefPtr<Node>> _childs{};
+    bool _self_closing = false;
 
 public:
+    bool self_closing() { return _self_closing; }
+    void self_closing(bool v) { _self_closing = v; }
+
     Node()
     {
     }

a2dc16c52f3df95b13d25610996e7573bc19e75e
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-10-28 16:04:13 +0100

    media-player: Use the new slider widget.

--- applications/media-player/.build.mk
+++ applications/media-player/.build.mk
@@ -2,4 +2,4 @@ APPS += MEDIA_PLAYER
 
 MEDIA_PLAYER_NAME = media-player
 MEDIA_PLAYER_LIBS = widget markup json graphic
-MEDIA_PLAYER_ICONS = movie
+MEDIA_PLAYER_ICONS = movie play pause stop volume-high volume-off
--- applications/media-player/main.cpp
+++ applications/media-player/main.cpp
@@ -1,11 +1,18 @@
 #include <libwidget/Application.h>
 #include <libwidget/Markup.h>
+#include <libwidget/Widgets.h>
 
 int main(int argc, char **argv)
 {
     application_initialize(argc, argv);
 
     Window *window = window_create_from_file("/Applications/media-player/main.markup");
+
+    window->with_widget<Slider>("volume_slider", [&](Slider *slider) {
+        slider->on(Event::VALUE_CHANGE, [&](auto) {
+            logger_trace("Value is %d", (int)(slider->value() * 100));
+        });
+    });
 
     window->show();
 

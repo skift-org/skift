0138c938d5d4517c5c46f8fec4d8e6ceb6f4ece7
Supercip971 <supercyp971@gmail.com>
2020-11-02 03:46:15 -0400

    kernel: (x64) added more information in the interrupt stack

--- architectures/x86_64/kernel/Interrupts.h
+++ architectures/x86_64/kernel/Interrupts.h
@@ -7,4 +7,10 @@ struct InterruptStackFrame
     uint64_t r15, r14, r13, r12, r11, r10, r9, r8;
     uint64_t rbp, rdi, rsi, rdx, rcx, rbx, rax;
     uint32_t intno, err;
+    // the interrupt stackframe
+    uint64_t rip;
+    uint64_t cs;
+    uint64_t rflags;
+    uint64_t rsp;
+    uint64_t ss;
 };

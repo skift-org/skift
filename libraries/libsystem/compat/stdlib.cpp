f3e7d555d92bf31c502f8069a4f15a6b0505bd37
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-11-04 20:07:29 +0100

    libsystem: Added atoi to the compatibility layer..

--- libraries/libsystem/compat/stdlib.cpp
+++ libraries/libsystem/compat/stdlib.cpp
@@ -1,8 +1,17 @@
 #include <stdlib.h>
 
+#include <libsystem/core/CString.h>
 #include <libsystem/process/Process.h>
+#include <libsystem/utils/NumberParser.h>
 
 void abort()
 {
     process_exit(-1);
+}
+
+int atoi(const char *nptr)
+{
+    int value;
+    parse_int(PARSER_DECIMAL, nptr, strlen(nptr), &value);
+    return value;
 }

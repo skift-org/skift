5d1c8c42878520c70c5f226372b2f539eef40ac4
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-11-10 09:32:46 +0100

    ci: Add missing libuuid.

--- toolchain/install-dependencies.sh
+++ toolchain/install-dependencies.sh
@@ -3,4 +3,4 @@ apt update
 apt install -y nasm gcc make binutils grub-pc-bin qemu-system-x86
 apt install -y build-essential bison flex libgmp3-dev libmpc-dev
 apt install -y xorriso mtools inkscape libmpfr-dev texinfo libfuse-dev
-apt install -y gcc-multilib clang-tidy
+apt install -y gcc-multilib clang-tidy uuid-dev

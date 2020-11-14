5ada6b66b5090023533c29d4214dbcc5b5975fc4
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-11-14 17:51:38 +0100

    init: Start a shell if no framebuffer is found.

--- applications/coreutils/init.cpp
+++ applications/coreutils/init.cpp
@@ -14,26 +14,25 @@ int main(int argc, char **argv)
 
     logger_level(LOGGER_TRACE);
 
-    int splash_pid = -1;
-    process_run("splash-screen", &splash_pid);
-    process_wait(splash_pid, nullptr);
-
-    // Start a "DEBUG" shell
-    // process_run("shell", NULL);
-
     environment() = json::parse_file("/System/Configs/environment.json");
 
     if (filesystem_exist(FRAMEBUFFER_DEVICE_PATH, FILE_TYPE_DEVICE))
     {
+        int splash_pid = -1;
+        process_run("splash-screen", &splash_pid);
+        process_wait(splash_pid, nullptr);
+
         int compositor_pid = -1;
         process_run("compositor", &compositor_pid);
         process_wait(compositor_pid, nullptr);
     }
-    else if (filesystem_exist(TEXTMODE_DEVICE_PATH, FILE_TYPE_DEVICE))
+    else
     {
-        int terminal_pid = -1;
-        process_run("terminal", &terminal_pid);
-        process_wait(terminal_pid, nullptr);
+        stream_format(err_stream, "No framebuffer! starting a shell, good luck :^)\n");
+
+        int shell_pid = -1;
+        process_run("shell", &shell_pid);
+        process_wait(shell_pid, nullptr);
     }
 
     printf("\n\n\t\e[1;34mGoodbye!\e[m - n°1\n\n");

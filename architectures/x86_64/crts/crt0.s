8f45adf1751f681fe95152a5264378a963f33c88
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-11-19 18:08:11 +0100

    x86_64: Fixed broken crt0.

--- architectures/x86_64/crts/crt0.s
+++ architectures/x86_64/crts/crt0.s
@@ -2,10 +2,12 @@ section .text
 
 extern __entry_point
 
-global _start
+global _start:function (_start.end - _start)
+_start:
     pop rdi ; argc
     pop rsi ; argv
     pop rdx ; env
 
     call __entry_point
     ud2
+.end:

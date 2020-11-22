d622f40808538162d0f47720bfc5c5a61147e381
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-11-22 18:21:08 +0100

    kill: Allow killing processes by name.

--- applications/coreutils/kill.cpp
+++ applications/coreutils/kill.cpp
@@ -1,33 +1,70 @@
 #include <libsystem/io/Stream.h>
+#include <libsystem/json/Json.h>
 #include <libsystem/process/Process.h>
 #include <libsystem/utils/NumberParser.h>
+#include <libutils/ArgParse.h>
 
-int kill(const char *id)
+int kill(int pid)
 {
-    int i = parse_uint_inline(PARSER_DECIMAL, id, -1);
-
-    if (i == -1)
+    if (process_cancel(pid) != SUCCESS)
     {
         return PROCESS_FAILURE;
     }
 
-    return process_cancel(i);
+    return PROCESS_SUCCESS;
 }
 
-int main(int argc, char **argv)
+int killall(String name)
 {
-    if (argc == 1)
+    auto processes = json::parse_file("/System/processes");
+
+    for (size_t i = 0; i < processes.length(); i++)
     {
-        stream_format(err_stream, "%s: not enough arguments\n", argv[0]);
-        return PROCESS_FAILURE;
+        auto &proc = processes.get(i);
+
+        if (proc.get("name").as_string() == name)
+        {
+            kill(proc.get("id").as_integer());
+        }
     }
 
-    int result = 0;
+    return SUCCESS;
+}
 
-    for (int i = 1; i < argc; i++)
+int main(int argc, char const *argv[])
+{
+    ArgParse args;
+
+    args.show_help_if_no_operand_given();
+    args.should_abort_on_failure();
+
+    bool option_all;
+    args.option(option_all, 'a', "all", "Kill all processes with a matching name.");
+
+    args.eval(argc, argv);
+
+    if (option_all)
     {
-        result |= kill(argv[i]);
+        for (size_t i = 0; i < args.argc(); i++)
+        {
+            if (killall(args.argv()[i]) != PROCESS_SUCCESS)
+            {
+                return PROCESS_FAILURE;
+            }
+        }
+    }
+    else
+    {
+        for (size_t i = 0; i < args.argc(); i++)
+        {
+            int pid = parse_uint_inline(PARSER_DECIMAL, args.argv()[i].cstring(), -1);
+
+            if (kill(pid) != PROCESS_SUCCESS)
+            {
+                return PROCESS_FAILURE;
+            }
+        }
     }
 
-    return result;
+    return PROCESS_SUCCESS;
 }

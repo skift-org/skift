403e5929446dabe0c792fe17192371aa13dff8b8
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-11-22 12:52:01 +0100

    demo: Replaced missing test image.

--- applications/demo/Graphics.cpp
+++ applications/demo/Graphics.cpp
@@ -12,7 +12,7 @@ void graphics_draw(Painter &painter, Rectangle screen, float time)
 
     if (_test_image == nullptr)
     {
-        _test_image = Bitmap::load_from_or_placeholder("/System/skift.png");
+        _test_image = Bitmap::load_from_or_placeholder("/Applications/demo/test.png");
     }
 
     for (int x = 0; x < screen.width(); x++)

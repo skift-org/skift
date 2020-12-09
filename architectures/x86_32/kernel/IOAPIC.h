3302953d4a809ddafe99353ce2237284efb0178f
OptimisticSide <47266822+optimisticside@users.noreply.github.com>
2020-12-09 06:42:26 -0800

    Removed IOAPIC address from parameters

--- architectures/x86_32/kernel/IOAPIC.h
+++ architectures/x86_32/kernel/IOAPIC.h
@@ -4,6 +4,6 @@
 
 void ioapic_found(uintptr_t address);
 
-uint32_t apic_read(void *ioapicaddr, uint32_t reg);
+uint32_t apic_read(uint32_t reg);
 
-void ioapic_write(void *ioapicaddr, uint32_t reg, uint32_t value);
+void ioapic_write(uint32_t reg, uint32_t value);

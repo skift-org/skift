a86c953229da898972a29b1fcef229a62b120072
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-12-24 21:10:14 +0100

    init: Added some messages.

--- applications/coreutils/init.cpp
+++ applications/coreutils/init.cpp
@@ -14,10 +14,14 @@ int main(int argc, char **argv)
 
     logger_level(LOGGER_TRACE);
 
+    logger_info("Loading environement variables...");
+    
     environment() = json::parse_file("/Configs/environment.json");
 
     if (filesystem_exist(FRAMEBUFFER_DEVICE_PATH, FILE_TYPE_DEVICE))
     {
+        logger_info("Starting desktop environement...");
+
         int splash_pid = -1;
         process_run("splash-screen", &splash_pid);
         process_wait(splash_pid, nullptr);
@@ -28,7 +32,7 @@ int main(int argc, char **argv)
     }
     else
     {
-        stream_format(err_stream, "No framebuffer! starting a shell, good luck :^)\n");
+        stream_format(err_stream, "No framebuffer! Starting a shell, good luck :^)\n");
 
         int shell_pid = -1;
         process_run("shell", &shell_pid);

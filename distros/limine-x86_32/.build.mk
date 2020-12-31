2b363539f08d6845f12b356aa266c1dc75f473c8
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-12-31 01:40:05 +0100

    meta: Updated limine.

--- distros/limine-x86_32/.build.mk
+++ distros/limine-x86_32/.build.mk
@@ -1,4 +1,10 @@
-$(BOOTDISK): $(RAMDISK) $(KERNEL_BINARY) $(DISTRO_DIRECTORY)/limine.cfg $(ECHFS) $(LIMINE) $(LIMINE_LOADER)
+$(KERNEL_BINARY).gz: $(KERNEL_BINARY)
+	@gzip -c $(KERNEL_BINARY) > $(KERNEL_BINARY).gz
+
+$(RAMDISK).gz: $(RAMDISK)
+	@gzip -c $(RAMDISK) > $(RAMDISK).gz
+
+$(BOOTDISK): $(RAMDISK).gz $(KERNEL_BINARY).gz $(DISTRO_DIRECTORY)/limine.cfg $(ECHFS) $(LIMINE) $(LIMINE_LOADER)
 	$(DIRECTORY_GUARD)
 
 	rm -f $(BOOTDISK)
@@ -9,8 +15,8 @@ $(BOOTDISK): $(RAMDISK) $(KERNEL_BINARY) $(DISTRO_DIRECTORY)/limine.cfg $(ECHFS)
 	parted -s $(BOOTDISK) mkpart primary 1 100%
 
 	$(ECHFS) -m -p0 $(BOOTDISK) quick-format 32768
-	$(ECHFS) -m -p0 $(BOOTDISK) import $(KERNEL_BINARY) kernel.bin
-	$(ECHFS) -m -p0 $(BOOTDISK) import $(RAMDISK) ramdisk.tar
+	$(ECHFS) -m -p0 $(BOOTDISK) import $(KERNEL_BINARY).gz kernel.bin.gz
+	$(ECHFS) -m -p0 $(BOOTDISK) import $(RAMDISK).gz ramdisk.tar.gz
 	$(ECHFS) -m -p0 $(BOOTDISK) import $(DISTRO_DIRECTORY)/limine.cfg limine.cfg
 	$(ECHFS) -m -p0 $(BOOTDISK) import $(DISTRO_DIRECTORY)/background.bmp background.bmp
 

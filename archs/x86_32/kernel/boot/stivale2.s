2323c9abb5171a86aee09361318fc7215e6d96de
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2021-01-06 17:46:54 +0100

    meta: Updated limine and enabled write combining.

--- archs/x86_32/kernel/boot/stivale2.s
+++ archs/x86_32/kernel/boot/stivale2.s
@@ -8,10 +8,14 @@ __stivale2_framebuffer_tag:
     dw 0
     dw 32
 
+__stivale2_write_combining_tag:
+    dq 0x4c7bb07731282e00
+    dq __stivale2_framebuffer_tag
+
 section .stivale2hdr
 align 4
 __stivale2_header:
     dq _kstart_stivale2
     dq __stack_top
     dq 0
-    dq __stivale2_framebuffer_tag
+    dq __stivale2_write_combining_tag

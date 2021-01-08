17aec0094a5ec0ec8c0e5e6da2ad350d23f9c99a
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2021-01-08 12:15:02 +0100

    libutils: Improved ResultOr using Optional.

---
+++ libraries/libutils/Optional.h
@@ -0,0 +1,122 @@
+#pragma once
+
+#include <libsystem/Assert.h>
+#include <libutils/Move.h>
+#include <libutils/New.h>
+
+template <typename T>
+class Optional
+{
+private:
+    bool _present = false;
+    char _storage[sizeof(T)];
+
+public:
+    bool present() const
+    {
+        return _present;
+    }
+
+    T &value()
+    {
+        assert(present());
+        return *reinterpret_cast<T *>(_storage);
+    }
+
+    const T &value() const
+    {
+        assert(present());
+        return *reinterpret_cast<const T *>(_storage);
+    }
+
+    T take_value()
+    {
+        assert(present());
+
+        T v = move(value());
+        clear();
+
+        return v;
+    }
+
+    T value_or(const T &defaut_value) const
+    {
+        if (present())
+        {
+            return value();
+        }
+        else
+        {
+            return defaut_value;
+        }
+    }
+
+    Optional() {}
+
+    Optional(const T &value)
+    {
+        _present = true;
+        new (&_storage) T(value);
+    }
+
+    Optional(T &&value)
+    {
+        _present = true;
+        new (&_storage) T(move(value));
+    }
+
+    Optional(const Optional &other)
+    {
+        if (other.present())
+        {
+            _present = true;
+            new (&_storage) T(other.value());
+        }
+    }
+
+    Optional(Optional &&other)
+    {
+        if (other.present())
+        {
+            new (&_storage) T(other.take_value());
+            _present = true;
+        }
+    }
+
+    Optional &operator=(const Optional &other)
+    {
+        return *this = Optional(other);
+    }
+
+    Optional &operator=(Optional &&other)
+    {
+        swap(_storage, other._storage);
+        swap(_present, other._present);
+
+        return *this;
+    }
+
+    ~Optional()
+    {
+        clear();
+    }
+
+    void clear()
+    {
+        if (_present)
+        {
+            value().~T();
+            _present = false;
+        }
+    }
+
+    operator bool() const { return _present; }
+
+    T &operator*() { return value(); }
+
+    const T &operator*() const { return value(); }
+
+    T *operator->() { return &value(); }
+
+    const T *operator->() const { return &value(); }
+};
--- libraries/libutils/ResultOr.h
+++ libraries/libutils/ResultOr.h
@@ -2,14 +2,16 @@
 
 #include <libsystem/Assert.h>
 #include <libsystem/Result.h>
+
 #include <libutils/Move.h>
+#include <libutils/Optional.h>
 
 template <typename T>
 class ResultOr
 {
 private:
     Result _result;
-    T _value;
+    Optional<T> _value;
 
 public:
     bool success() { return _result == SUCCESS; }
@@ -17,43 +19,57 @@ public:
     T &value()
     {
         assert(success());
-        return _value;
+
+        return *_value;
     }
 
-    T &&take_value()
+    const T &value() const
     {
         assert(success());
-        return move(_value);
+
+        return *_value;
     }
 
-    Result result() { return _result; }
+    T take_value()
+    {
+        assert(success());
+
+        return _value.take_value();
+    }
+
+    Result result() const { return _result; }
 
     ResultOr(Result result) : _result{result}, _value{} {}
 
     ResultOr(T value) : _result{SUCCESS}, _value{move(value)} {}
 
-    ResultOr(Result result, T value) : _result{result}, _value{move(value)} {}
-
-    template <typename U>
-    ResultOr(ResultOr<U> result, T value) : _result{result.result()}, _value{move(value)} {}
-
-    bool operator==(Result result)
+    bool operator==(Result result) const
     {
         return _result == result;
     }
 
-    bool operator!=(Result result)
+    bool operator!=(Result result) const
     {
         return _result != result;
     }
 
-    bool operator==(T value)
+    bool operator==(const T &other) const
     {
-        return _value == value;
+        return *_value == value;
     }
 
-    bool operator!=(T value)
+    bool operator!=(const T &other) const
     {
-        return _value != value;
+        return *_value != value;
     }
+
+    operator bool() const { return _result == SUCCESS; }
+
+    T &operator*() { return value(); }
+
+    const T &operator*() const { return value(); }
+
+    T *operator->() { return &value(); }
+
+    const T *operator->() const { return &value(); }
 };

c58ffd3cea7ddcd5fd2c1c73dc834e2a96984550
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2021-01-24 00:58:09 +0100

    libwidget: Take outset into account when computing the size.

--- libraries/libwidget/Widget.cpp
+++ libraries/libwidget/Widget.cpp
@@ -672,6 +672,8 @@ Vec2i Widget::compute_size()
 
     width += _insets.left() + _insets.right();
     height += _insets.top() + _insets.bottom();
+    width += _outsets.left() + _outsets.right();
+    height += _outsets.top() + _outsets.bottom();
 
     if (_max_width)
     {

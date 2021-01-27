654a2a90b9f3e9c1a4c98852caaf11702f8909ab
Stephan Vedder <vedder@mbits.info>
2021-01-27 16:33:08 +0100

    libfile: Start prototyping the ZipArchive class

---
+++ libraries/libfile/ZipArchive.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <libfile/Archive.h>
+
+class ZipArchive : public Archive
+{
+public:
+    ZipArchive(File file);
+
+    inline unsigned int get_entry_count() override
+    {
+        return _entry_count;
+    }
+
+    const char *get_entry_name(unsigned int entry_index) override;
+
+    Result extract(unsigned int entry_index, const char *dest_path) override;
+    Result insert(const char *entry_name, const char *src_path) override;
+
+private:
+    unsigned int _entry_count = 0;
+};

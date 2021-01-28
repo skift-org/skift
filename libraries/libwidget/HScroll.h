4fc0024050735f8188402da77452808ffb924e33
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2021-01-28 23:11:03 +0100

    libwidget: Added HScroll widget.

---
+++ libraries/libwidget/HScroll.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <libwidget/Container.h>
+#include <libwidget/ScrollBar.h>
+#include <libwidget/Widget.h>
+
+class HScroll : public Widget
+{
+private:
+    Container *_host = nullptr;
+    ScrollBar *_scrollbar = nullptr;
+
+public:
+    Container *host() { return _host; }
+
+    HScroll(Widget *parent) : Widget(parent)
+    {
+        _host = new Container(this);
+
+        _scrollbar = new ScrollBar(this);
+        _scrollbar->flags(Widget::NOT_AFFECTED_BY_SCROLL);
+
+        _scrollbar->on(Event::VALUE_CHANGE, [this](auto) {
+            scroll({_scrollbar->value(), scroll().y()});
+            should_repaint();
+        });
+    }
+
+    void do_layout() override
+    {
+        int content_width = _host->size().x();
+
+        _host->container(content().take_left(content_width));
+        _scrollbar->container(content().take_bottom(ScrollBar::SIZE));
+        _scrollbar->update(content_width, content().width(), scroll().y());
+    }
+
+    Vec2i size() override
+    {
+        return {0, _host->size().y()};
+    }
+};

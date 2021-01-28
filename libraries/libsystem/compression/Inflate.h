23083b176da3dc4ed8f9a4007678538493deb244
Stephan Vedder <vedder@mbits.info>
2021-01-28 17:03:06 +0100

    libsystem: Add new error code for invalid data

--- libraries/libsystem/compression/Inflate.h
+++ libraries/libsystem/compression/Inflate.h
@@ -15,15 +15,15 @@ enum BlockType
 class Inflate
 {
 public:
-    Result perform(Vector<uint8_t> input, Vector<uint8_t>& output);
+    Result perform(const Vector<uint8_t> &input, Vector<uint8_t> &output);
 
 private:
     void build_fixed_huffman_alphabet();
-    void build_huffman_alphabet(Vector<unsigned int> &alphabet, Vector<unsigned int> code_bit_lengths);
+    void build_huffman_alphabet(Vector<unsigned int> &alphabet, const Vector<unsigned int> &code_bit_lengths);
 
     void get_bit_length_count(HashMap<unsigned int, unsigned int> &bit_length_count, const Vector<unsigned int> &code_bit_lengths);
     void get_first_code(HashMap<unsigned int, unsigned int> &firstCodes, HashMap<unsigned int, unsigned int> &bit_length_count);
-    void assign_huffman_codes(Vector<unsigned int>& assigned_codes, const Vector<unsigned int> &code_bit_lengths, HashMap<unsigned int, unsigned int> &first_codes);
+    void assign_huffman_codes(Vector<unsigned int> &assigned_codes, const Vector<unsigned int> &code_bit_lengths, HashMap<unsigned int, unsigned int> &first_codes);
 
     Vector<unsigned int> _fixed_alphabet;
     Vector<unsigned int> _fixed_code_bit_lengths;

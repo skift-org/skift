19977663a5cba181a654fc92681d4081ef32ed30
Stephan Vedder <vedder@mbits.info>
2021-01-29 10:56:35 +0100

    libutils: Start working on BitWriter class

--- libraries/libutils/BitWriter.h
+++ libraries/libutils/BitWriter.h
@@ -9,6 +9,48 @@ public:
     {
     }
 
+    ~BitWriter()
+    {
+        flush();
+    }
+
+    inline void put_bits(unsigned int v, const unsigned int num_bits)
+    {
+        _bit_buffer |= bits << os->bitcount;
+        _bit_count += num_bits;
+    }
+
+    inline void put_data(uint8_t *data, unsigned int len)
+    {
+        _data.push_back_many(Vector<uint8_t>(ADOPT, data, len));
+    }
+
+    inline void put_short(unsigned short v)
+    {
+        _data.push_back(v & 0xFF);
+        _data.push_back(v >> 8);
+    }
+
+    inline void align()
+    {
+        _bit_count += -_bit_count & 7;
+        flush();
+    }
+
+    inline void flush()
+    {
+        // Flush one byte at a time
+        while (_bit_count > 8)
+        {
+            _data.emplace_back() = _bit_buffer;
+            _bit_count -= 8;
+            _bit_buffer >>= 8;
+        }
+    }
+
 private:
+    unsigned int _bit_buffer;
+    unsigned int _bit_count;
+    unsigned int _index;
     Vector<uint8_t> &_data;
 };

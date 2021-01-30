ac9d4ee3592710f30d741e2c7d486584f40cecbd
Stephan Vedder <stephan.vedder@gmail.com>
2021-01-30 11:18:40 +0100

    apps: implement archive manager

--- apps/archive-manager/main.cpp
+++ apps/archive-manager/main.cpp
@@ -1,3 +1,4 @@
+#include <libfile/ZipArchive.h>
 #include <libsystem/process/Process.h>
 #include <libwidget/Application.h>
 
@@ -7,7 +8,15 @@ int main(int argc, char **argv)
 {
     Application::initialize(argc, argv);
 
-    auto window = new MainWindow();
+    if (argc != 2)
+    {
+        return PROCESS_FAILURE;
+    }
+
+    auto navigation = make<filepicker::Navigation>();
+    auto archive = make<ZipArchive>(argv[1]);
+
+    auto window = new MainWindow(navigation, archive);
 
     window->show();
 

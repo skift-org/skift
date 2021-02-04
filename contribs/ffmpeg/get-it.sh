38df1175d0c4b422b93ddfa4af40b20f9c1722fe
Stephan Vedder <vedder@mbits.info>
2021-02-04 14:12:38 +0100

    contribs: only do shallow clones of release versions

--- contribs/ffmpeg/get-it.sh
+++ contribs/ffmpeg/get-it.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+VERSION=4.3
 
-git clone https://git.ffmpeg.org/ffmpeg.git sources
+echo "Getting FFMPEG version: $VERSION"
+git clone --depth 1 --branch release/$VERSION https://git.ffmpeg.org/ffmpeg.git sources

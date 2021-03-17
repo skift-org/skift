9a2c5b7724fe20667accf819413fcddc67edf549
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2021-03-17 21:28:54 +0100

    libio: Allow Connections to be closed.

--- libraries/libio/Connection.h
+++ libraries/libio/Connection.h
@@ -16,22 +16,36 @@ class Connection final :
 private:
     RefPtr<Handle> _handle;
 
-    __nonmovable(Connection);
-
 public:
     RefPtr<Handle> handle() override { return _handle; }
 
-    Connection(RefPtr<Handle> handle)
-        : _handle{handle} {}
+    Connection() {}
+    Connection(RefPtr<Handle> handle) : _handle{handle} {}
 
     ResultOr<size_t> read(void *buffer, size_t size) override
     {
+        if (!_handle)
+            return ERR_STREAM_CLOSED;
+
         return _handle->read(buffer, size);
     }
 
     ResultOr<size_t> write(const void *buffer, size_t size) override
     {
+        if (!_handle)
+            return ERR_STREAM_CLOSED;
+
         return _handle->write(buffer, size);
+    }
+
+    bool closed()
+    {
+        return _handle == nullptr;
+    }
+
+    void close()
+    {
+        _handle = nullptr;
     }
 };
 

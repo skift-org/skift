1ef85b5b0355558d597b7ef4b51338fc32bd4382
Stephan Vedder <vedder@mbits.info>
2021-03-18 16:31:45 +0100

    libtest: Start implementing test macros

---
+++ libraries/libtest/AssertEqual.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <assert.h>
+#include <libio/Format.h>
+
+namespace Test
+{
+template <class A, class B>
+inline void _assert_equal(const A a, const B b, const char *file, const char *function, int line)
+{
+    if constexpr (!( __CONFIG_IS_RELEASE__ ))
+    {
+        if (a != b)
+        {
+            assert_failed(IO::format("{} not equal {}", a, b).cstring(), file, function, line);
+        }
+    }
+}
+} // namespace Test
+
+#ifndef assert_equal
+#    define assert_equal(_a, _b) Test::_assert_equal(_a, _b, __FILE__, __FUNCTION__, __LINE__)
+#endif
---
+++ libraries/libtest/AssertNotEqual.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <assert.h>
+#include <libio/Format.h>
+
+namespace Test
+{
+template <class A, class B>
+inline void _assert_not_equal(const A a, const B b, const char *file, const char *function, int line)
+{
+    if constexpr (!(__CONFIG_IS_RELEASE__))
+    {
+        if (a != b)
+        {
+            assert_failed(IO::format("{} equals {}", a, b).cstring(), file, function, line);
+        }
+    }
+}
+
+} // namespace Test
+
+#ifndef assert_not_equal
+#    define assert_not_equal(_a, _b) _assert_not_equal(_a, _b, __FILE__, __FUNCTION__, __LINE__)
+#endif

e20aad9982d37da046e8867af682e2f5e113a88a
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2021-03-23 14:38:40 +0100

    libio: New Bitreader.

--- libraries/libio/BitReader.h
+++ libraries/libio/BitReader.h
@@ -1,112 +1,192 @@
 #pragma once
+
 #include <libio/Read.h>
-#include <libsystem/Common.h>
-#include <libtest/AssertEqual.h>
-#include <libtest/AssertGreaterThan.h>
-#include <libtest/AssertLowerEqual.h>
+#include <libsystem/Logger.h>
+#include <libtest/Assets.h>
+#include <libutils/InlineRingBuffer.h>
 
 namespace IO
 {
 class BitReader
 {
+private:
+    IO::Reader &_reader;
+    utils::InlineRingBuffer<uint8_t, 16> _buffer;
+    size_t _head = 0;
+    bool _end_of_file;
+
 public:
-    inline BitReader(IO::Reader &reader) : _reader(reader)
+    inline BitReader(IO::Reader &reader) : _reader(reader) {}
+
+    inline Result hint(size_t num_bits)
     {
+        size_t num_bytes = __align_up(_head + num_bits, 8) / 8;
+
+        while (_buffer.used() < num_bytes)
+        {
+            uint8_t byte;
+
+            if (TRY(_reader.read(&byte, sizeof(byte))) == 0)
+            {
+                _end_of_file = true;
+            }
+
+            _buffer.put(byte);
+        }
+
+        return SUCCESS;
+    }
+
+    inline void flush()
+    {
+        _head = 0;
+        _buffer.flush();
+    }
+
+    template <class T>
+    inline T grab()
+    {
+        hint(sizeof(T) * 8);
+
+        T value;
+
+        for (size_t i = 0; i < sizeof(T); i++)
+        {
+            (reinterpret_cast<uint8_t *>(&value))[i] = grab_bits(8);
+        }
+
+        return value;
+    }
+
+    template <class T>
+    inline T grab_aligned()
+    {
+        flush();
+        return grab<T>();
+    }
+
+    template <class T>
+    inline T peek()
+    {
+        hint(sizeof(T) * 8);
+
+        T value;
+
+        for (size_t i = 0; i < sizeof(T); i++)
+        {
+            (reinterpret_cast<uint8_t *>(&value))[i] = peek_bits(i * 8, 8);
+        }
+
+        return value;
     }
 
     inline void skip_bits(size_t num_bits)
     {
-        ensure_buffer(num_bits);
-        _bit_index += num_bits;
-        pop_cache();
-    }
+        hint(num_bits);
 
-    template<class T>
-    inline T grab_aligned()
-    {
-        assert_equal(_bit_index, 0);
-        return IO::read<T>(_reader).value();
-    }
-
-    inline unsigned int grab_bits(unsigned int num_bits)
-    {
-        unsigned int ret_val = 0;
-        ensure_buffer(num_bits);
-        for (unsigned int i = 0; i != num_bits; i++)
+        for (size_t i = 0; i < num_bits; i++)
         {
-            ret_val |= (unsigned int)peek_bit(_bit_index) << i;
-            _bit_index++;
-        }
-        pop_cache();
-        return ret_val;
-    }
-
-    inline unsigned int peek_bits(size_t num_bits)
-    {
-        unsigned int ret_val = 0;
-        ensure_buffer(num_bits);
-        for (unsigned int i = 0; i != num_bits; i++)
-        {
-            ret_val |= (unsigned int)peek_bit(_bit_index + i) << i;
-        }
-        return ret_val;
-    }
-
-    inline unsigned int grab_bits_reverse(size_t num_bits)
-    {
-        unsigned int ret_val = 0;
-        ensure_buffer(num_bits);
-        for (unsigned int i = 0; i != num_bits; i++)
-        {
-            ret_val |= (unsigned int)peek_bit(_bit_index) << ((num_bits - 1) - i);
-            _bit_index++;
-        }
-        pop_cache();
-        return ret_val;
-    }
-
-    inline unsigned int peek_bits_reverse(size_t num_bits)
-    {
-        unsigned int ret_val = 0;
-        ensure_buffer(num_bits);
-        for (unsigned int i = 0; i != num_bits; i++)
-        {
-            ret_val |= (unsigned int)peek_bit(_bit_index + i) << ((num_bits - 1) - i);
-        }
-        return ret_val;
-    }
-
-private:
-    inline void ensure_buffer(size_t num_bits)
-    {
-        auto num_bytes = __align_up(num_bits + _bit_index, 8) / 8;
-        
-        while (_cached.count() < num_bytes)
-        {
-            _cached.push_back(IO::read<uint8_t>(_reader).value());
+            grab_bit();
         }
     }
 
-    inline void pop_cache()
+    inline uint8_t grab_bit()
     {
-        while (_bit_index >= 8)
+        uint8_t bit = peek_bit(0);
+
+        _head++;
+        if (_head == 8)
         {
-            _cached.pop();
-            _bit_index -= 8;
+            _head -= 8;
+            _buffer.get(); // grab the byte
         }
 
-        assert_lower_equal(_cached.count(), 1);
+        return bit;
+    }
+
+    inline uint32_t grab_bits(size_t num_bits)
+    {
+        hint(num_bits);
+
+        assert_lower_equal(num_bits, 32);
+
+        uint32_t result = 0;
+
+        for (size_t i = 0; i < num_bits; i++)
+        {
+            uint8_t bit = grab_bit();
+            result |= bit << (i);
+        }
+
+        return result;
     }
 
     inline uint8_t peek_bit(size_t index)
     {
-        assert_greater_than(_cached.count(), index / 8);
-        const auto &cur_byte = _cached[index / 8];
-        return (cur_byte & (1 << (index % 8))) ? 1 : 0;
+        size_t num_bit = _head + index;
+        size_t num_byte = num_bit / 8;
+        size_t bit_within_the_byte = num_bit - (num_byte * 8);
+
+        uint8_t byte = _buffer.peek(num_byte);
+
+        return ((byte) >> (bit_within_the_byte)) & 1;
     }
 
-    Vector<uint8_t> _cached;
-    IO::Reader &_reader;
-    size_t _bit_index = 0;
+    inline uint32_t peek_bits(size_t num_bits)
+    {
+        return peek_bits(0, num_bits);
+    }
+
+    inline uint32_t peek_bits(size_t offset, size_t num_bits)
+    {
+        hint(offset + num_bits);
+
+        assert_lower_equal(num_bits, 32);
+
+        uint32_t result = 0;
+
+        for (size_t i = 0; i < num_bits; i++)
+        {
+            uint8_t bit = peek_bit(offset + i);
+            result |= bit << (i);
+        }
+
+        return result;
+    }
+
+    inline uint32_t grab_bits_reverse(size_t num_bits)
+    {
+        hint(num_bits);
+
+        assert_lower_equal(num_bits, 32);
+
+        uint32_t result = 0;
+
+        for (size_t i = 0; i < num_bits; i++)
+        {
+            uint8_t bit = grab_bit();
+            result |= bit << ((num_bits -1) - i);
+        }
+
+        return result;
+    }
+
+    inline uint32_t peek_bits_reverse(size_t num_bits)
+    {
+        hint(num_bits);
+
+        assert_lower_equal(num_bits, 32);
+
+        uint32_t result = 0;
+
+        for (size_t i = 0; i < num_bits; i++)
+        {
+            uint8_t bit = peek_bit(i);
+            result |= bit << ((num_bits - 1) - i);
+        }
+
+        return result;
+    }
 };
+
 } // namespace IO

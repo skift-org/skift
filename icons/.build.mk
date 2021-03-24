28b1687276fbcbbcb8270e95524b7d4b7c2516f3
Stephan Vedder <stephan.vedder@gmail.com>
2021-03-24 22:09:57 +0100

    meta: only generate 8bit pngs

--- icons/.build.mk
+++ icons/.build.mk
@@ -10,19 +10,19 @@ TARGETS += $(ICONS_AT_18PX) $(ICONS_AT_24PX) $(ICONS_AT_36PX) $(ICONS_AT_48PX)
 $(SYSROOT)/Files/Icons/%@18px.png: icons/%.svg
 	$(DIRECTORY_GUARD)
 	@echo [ImageMagick] $(notdir $@)
-	@convert -background none -resize 18x18 $< $@
+	@convert -depth 8 -background none -resize 18x18 $< $@
 
 $(SYSROOT)/Files/Icons/%@24px.png: icons/%.svg
 	$(DIRECTORY_GUARD)
 	@echo [ImageMagick] $(notdir $@)
-	@convert -background none -resize 24x24 $< $@
+	@convert -depth 8 -background none -resize 24x24 $< $@
 
 $(SYSROOT)/Files/Icons/%@36px.png: icons/%.svg
 	$(DIRECTORY_GUARD)
 	@echo [ImageMagick] $(notdir $@)
-	@convert -background none -resize 36x36 $< $@
+	@convert -depth 8 -background none -resize 36x36 $< $@
 
 $(SYSROOT)/Files/Icons/%@48px.png: icons/%.svg
 	$(DIRECTORY_GUARD)
 	@echo [ImageMagick] $(notdir $@)
-	@convert -background none -resize 48x48 $< $@
+	@convert -depth 8 -background none -resize 48x48 $< $@

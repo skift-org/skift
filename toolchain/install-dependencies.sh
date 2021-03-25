d1746e9641287b4c7d82d6041c2b5ad982bd5e54
(っ◔◡◔)っ♥ CRYPTY ♥ <70380855+todoroki-san@users.noreply.github.com>
2021-03-25 20:39:29 +0100

    Update install-dependencies.sh

--- toolchain/install-dependencies.sh
+++ toolchain/install-dependencies.sh
@@ -1,6 +1,48 @@
 #!/bin/bash
-apt update
-apt install -y nasm gcc make binutils grub-pc-bin qemu-system-x86
-apt install -y build-essential bison flex libgmp3-dev libmpc-dev
-apt install -y xorriso mtools imagemagick libmpfr-dev texinfo libfuse-dev
-apt install -y gcc-multilib clang-tidy uuid-dev
+
+# Debian or Ubuntu based distros
+if [ -f /etc/debian-release ]; then
+        sudo apt update
+        sudo apt upgrade
+
+        sudo apt install nasm gcc make binutils grub-pc-bin xorriso mtools imagemagick git qemu qemu-kvm
+
+elif [ -f /etc/ubuntu-release ]; then
+        sudo apt update
+        sudo apt upgrade
+
+        sudo apt install nasm gcc make binutils grub-pc-bin xorriso mtools imagemagick git qemu qemu-kvm
+
+# RedHat or RedHat based distros
+elif [ -f /etc/redhat-release ]; then
+        cat /etc/redhat-release
+
+        sudo dnf update
+        sudo dnf upgrade
+
+        sudo dnf install nasm gcc make binutils xorriso mtools ImageMagick git qemu qemu-kvm
+
+elif [ -f /etc/fedora-release ]; then
+        cat /etc/fedora-release
+
+        sudo dnf update
+        sudo dnf upgrade
+
+        sudo dnf install nasm gcc make binutils xorriso mtools ImageMagick git qemu qemu-kvm
+
+elif [ -f /etc/centos-release ]; then
+        cat /etc/centos-release
+
+        sudo dnf update
+        sudo dnf upgrade
+
+        sudo dnf install nasm gcc make binutils xorriso mtools ImageMagick git qemu qemu-kvm
+
+# Arch or Arch based distros
+elif [ -f /etc/arch-release ]; then
+        cat /etc/redhat-release
+
+        sudo pacman -Syu
+
+        sudo pacman -S base-devel bison flex mpc mpfr texinfo
+fi

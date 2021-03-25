00e5004e9940596c1c5d55bc7af43d8a0e3729c9
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2021-03-25 22:18:11 +0100

    libutils: Added Chrono.

---
+++ libraries/libutils/Chrono.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <abi/Syscalls.h>
+#include <libsystem/Logger.h>
+
+namespace Utils
+{
+
+class Chrono
+{
+private:
+    const char *_name;
+    Tick _start;
+
+    __nonmovable(Chrono);
+    __noncopyable(Chrono);
+
+public:
+    Chrono(const char *name)
+    {
+        _name = name;
+        hj_system_tick(&_start);
+    }
+
+    ~Chrono()
+    {
+        Tick end;
+        hj_system_tick(&end);
+
+        logger_trace("Chrono '%s' ended at %u", _name, end - _start);
+    }
+};
+
+} // namespace Utils

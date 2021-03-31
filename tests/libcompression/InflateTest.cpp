e5335464893e8a6939d6ebf0cdad0bf7fa24fa47
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2021-03-31 08:18:00 +0200

    libcompression: Ported more test from tinif.

--- tests/libcompression/InflateTest.cpp
+++ tests/libcompression/InflateTest.cpp
@@ -1,7 +1,11 @@
 #include <libcompression/Inflate.h>
 #include <libtest/Asserts.h>
 
+#include "libtest/AssertEqual.h"
 #include "tests/Driver.h"
+
+// Test from https://github.com/jibsen/tinf
+// Copyright (c) 2003-2019 Joergen Ibsen
 
 TEST(inflate_empty)
 {
@@ -11,16 +15,64 @@ TEST(inflate_empty)
 
     IO::MemoryReader mem_reader(data, sizeof(data));
     IO::MemoryWriter mem_writer;
-
     Compression::Inflate inf;
-
     auto result = inf.perform(mem_reader, mem_writer);
+
     // The inflate should have been succesful
     assert_equal(result.result(), Result::SUCCESS);
     // The algorithm consumed all input bytes
     assert_equal(result.value(), sizeof(data));
     // Should have generated no output
     assert_equal(mem_writer.length().value(), 0);
+}
+
+TEST(inflate_empty_no_literals)
+{
+	/* Empty buffer, dynamic with 256 as only literal/length code
+	 *
+	 * You could argue that since the RFC only has an exception allowing
+	 * one symbol for the distance tree, the literal/length tree should
+	 * be complete. However gzip allows this.
+	 *
+	 * See also: https://github.com/madler/zlib/issues/75
+	 */
+    static const unsigned char data[] = {
+        0x05, 0xCA, 0x81, 0x00, 0x00, 0x00, 0x00, 0x00, 0x90, 0xFF,
+        0x6B, 0x01, 0x00};
+
+    IO::MemoryReader mem_reader(data, sizeof(data));
+    IO::MemoryWriter mem_writer;
+    Compression::Inflate inf;
+    auto result = inf.perform(mem_reader, mem_writer);
+
+    assert_equal(result.result(), Result::SUCCESS);
+    assert_equal(mem_writer.length().value(), 0);
+}
+
+TEST(inflate_rle)
+{
+    /* 256 zero bytes compressed using RLE (only one distance code) */
+    const uint8_t data[] = {
+        0xE5, 0xC0, 0x81, 0x00, 0x00, 0x00, 0x00, 0x80, 0xA0, 0xFC,
+        0xA9, 0x07, 0x39, 0x73, 0x01};
+
+    IO::MemoryReader mem_reader(data, sizeof(data));
+    IO::MemoryWriter mem_writer;
+    Compression::Inflate inf;
+    auto result = inf.perform(mem_reader, mem_writer);
+
+    // The inflate should have been succesful
+    assert_equal(result.result(), Result::SUCCESS);
+    // The algorithm consumed all input bytes
+    assert_equal(result.value(), sizeof(data));
+    // Should have generated 256 bytes
+    assert_equal(mem_writer.length().value(), 256);
+    // All bytes should be zero
+    auto uncompressed = Slice(mem_writer.slice());
+    for (size_t i = 0; i < uncompressed.size(); i++)
+    {
+        assert_equal(((uint8_t *)uncompressed.start())[i], 0);
+    }
 }
 
 TEST(inflate_huffman)
@@ -35,10 +87,9 @@ TEST(inflate_huffman)
 
     IO::MemoryReader mem_reader(data, sizeof(data));
     IO::MemoryWriter mem_writer;
-
     Compression::Inflate inf;
-
     auto result = inf.perform(mem_reader, mem_writer);
+
     // The inflate should have been succesful
     assert_equal(result.result(), Result::SUCCESS);
     // The algorithm consumed all input bytes
@@ -53,29 +104,102 @@ TEST(inflate_huffman)
     }
 }
 
-TEST(inflate_rle)
+TEST(inflate_max_matchlen)
 {
-    /* 256 zero bytes compressed using RLE (only one distance code) */
-    const uint8_t data[] = {
-        0xE5, 0xC0, 0x81, 0x00, 0x00, 0x00, 0x00, 0x80, 0xA0, 0xFC,
-        0xA9, 0x07, 0x39, 0x73, 0x01};
+	/* 259 zero bytes compressed using literal/length code 285 (len 258) */
+    static const unsigned char data[] = {
+        0xED, 0xCC, 0x81, 0x00, 0x00, 0x00, 0x00, 0x80, 0xA0, 0xFC,
+        0xA9, 0x17, 0xB9, 0x00, 0x2C};
 
     IO::MemoryReader mem_reader(data, sizeof(data));
     IO::MemoryWriter mem_writer;
-
     Compression::Inflate inf;
-
     auto result = inf.perform(mem_reader, mem_writer);
+
     // The inflate should have been succesful
     assert_equal(result.result(), Result::SUCCESS);
+
     // The algorithm consumed all input bytes
     assert_equal(result.value(), sizeof(data));
-    // Should have generated 256 bytes
-    assert_equal(mem_writer.length().value(), 256);
+
     // All bytes should be zero
     auto uncompressed = Slice(mem_writer.slice());
     for (size_t i = 0; i < uncompressed.size(); i++)
     {
         assert_equal(((uint8_t *)uncompressed.start())[i], 0);
     }
+}
+
+TEST(inflate_max_matchlen_alt)
+{
+	/* 259 zero bytes compressed using literal/length code 284 + 31 (len 258)
+	 *
+	 * Technically, this is outside the range specified in the RFC, but
+	 * gzip allows it.
+	 *
+	 * See also: https://github.com/madler/zlib/issues/75
+	 */
+    static const unsigned char data[] = {
+        0xE5, 0xCC, 0x81, 0x00, 0x00, 0x00, 0x00, 0x80, 0xA0, 0xFC,
+        0xA9, 0x07, 0xB9, 0x00, 0xFC, 0x05};
+
+    IO::MemoryReader mem_reader(data, sizeof(data));
+    IO::MemoryWriter mem_writer;
+    Compression::Inflate inf;
+    auto result = inf.perform(mem_reader, mem_writer);
+
+    // The inflate should have been succesful
+    assert_equal(result.result(), Result::SUCCESS);
+
+    // The algorithm consumed all input bytes
+    assert_equal(result.value(), sizeof(data));
+
+    // All bytes should be zero
+    auto uncompressed = Slice(mem_writer.slice());
+    for (size_t i = 0; i < uncompressed.size(); i++)
+    {
+        assert_equal(((uint8_t *)uncompressed.start())[i], 0);
+    }
+}
+
+TEST(inflate_max_matchdist)
+{
+	/* A match of length 3 with a distance of 32768 */
+    static const unsigned char data[] = {
+        0xED, 0xDD, 0x01, 0x01, 0x00, 0x00, 0x08, 0x02, 0x20, 0xED,
+        0xFF, 0xE8, 0xFA, 0x11, 0x1C, 0x61, 0x9A, 0xF7, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xE0,
+        0xFE, 0xFF, 0x05};
+
+
+    IO::MemoryReader mem_reader(data, sizeof(data));
+    IO::MemoryWriter mem_writer;
+    Compression::Inflate inf;
+    auto result = inf.perform(mem_reader, mem_writer);
+
+    // The inflate should have been succesful
+    assert_equal(result.result(), Result::SUCCESS);
+
+    // The algorithm consumed all input bytes
+    assert_equal(result.value(), sizeof(data));
+
+    assert_equal(mem_writer.length().value(), 32771);
+
+    auto uncompressed = Slice(mem_writer.slice());
+    const uint8_t* out = (const uint8_t*)uncompressed.start();
+
+    assert_equal(out[0], 2);
+	assert_equal(out[1], 1);
+    assert_equal(out[2], 0);
+
+    for (size_t i = 3; i < uncompressed.size() - 3; i++)
+    {
+        assert_equal(((uint8_t *)uncompressed.start())[i], 0);
+    }
+    
+    assert_equal(out[uncompressed.size() - 3], 2);
+	assert_equal(out[uncompressed.size() - 2], 1);
+	assert_equal(out[uncompressed.size() - 1], 0);
 }

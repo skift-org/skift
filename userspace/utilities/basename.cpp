027bb9ad17b2f8e95de61b7bd0f09d32a046152c
Stephan Vedder <vedder@mbits.info>
2021-04-09 16:50:43 +0200

    apps: rework basename

--- userspace/utilities/basename.cpp
+++ userspace/utilities/basename.cpp
@@ -1,15 +1,45 @@
 #include <libio/Streams.h>
+#include <libutils/ArgParse.h>
 #include <libutils/Path.h>
 
-int main(int argc, char **argv)
+int main(int argc, char const *argv[])
 {
-    if (argc == 1)
+    ArgParse args;
+
+    args.usage("NAME...");
+    args.usage("OPTIONS... NAME...");
+
+    args.show_help_if_no_operand_given();
+
+    args.prologue("Print NAME with any leading directory components removed.");
+
+    bool option_all = false;
+    bool option_zero = false;
+    args.option(option_all, 'a', "multiple", "Support multiple arguments and treat each as a NAME");
+    args.option(option_zero, 'z', "zero", "End each output line with NUL, not newline");
+
+    if (args.eval(argc, argv) != PROCESS_SUCCESS)
     {
-        IO::errln("usage: %s path", argv[0]);
         return PROCESS_FAILURE;
     }
 
-    IO::outln("{}", Path::parse(argv[1]).basename());
+    for (auto filepath : args.argv())
+    {
+        auto basename = Path::parse(filepath).basename();
+        if (option_zero)
+        {
+            IO::write(IO::out(), basename);
+        }
+        else
+        {
+            IO::outln("{}", basename);
+        }
+
+        if (!option_all)
+        {
+            break;
+        }
+    }
 
     return PROCESS_SUCCESS;
 }

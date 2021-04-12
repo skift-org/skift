505a97d1f1940a51b988b79a5bc6907066b39c93
Stephan Vedder <vedder@mbits.info>
2021-04-12 11:35:35 +0200

    apps: rewrite cat to use ArgParse

--- userspace/utilities/cat.cpp
+++ userspace/utilities/cat.cpp
@@ -3,38 +3,82 @@
 #include <libio/File.h>
 #include <libio/Streams.h>
 #include <libsystem/Result.h>
+#include <libutils/ArgParse.h>
 #include <libutils/Array.h>
 
-Result cat(const char *path)
+static bool option_linenumbers = false;
+
+Result cat(IO::Reader &reader)
 {
-    IO::File file{path, OPEN_READ};
+    if (option_linenumbers)
+    {
+        IO::Scanner scanner(reader);
+        size_t line = 1;
 
-    TRY(file.result());
+        while (!scanner.ended())
+        {
+            TRY(IO::format(IO::out(), "{0} ", line));
+            TRY(IO::copy_line(scanner, IO::out()));
+            line++;
+        }
 
-    return IO::copy(file, IO::out());
+        return Result::SUCCESS;
+    }
+    else
+    {
+        return IO::copy(reader, IO::out());
+    }
 }
 
-int main(int argc, char **argv)
+int main(int argc, char const *argv[])
 {
-    if (argc == 1)
+    ArgParse args;
+    args.should_abort_on_failure();
+    // TODO: remove this once **eval** handles "--help" correctly
+    args.show_help_if_no_operand_given();
+
+    args.prologue("Concatenate FILE(s) to standard output.");
+
+    args.usage("OPTION... FILE...");
+
+    args.option(option_linenumbers, 'n', "number", "Number all output lines");
+
+    if (args.eval(argc, argv) != PROCESS_SUCCESS)
     {
-        /* TODO: stdin option */
-        return PROCESS_SUCCESS;
+        return PROCESS_FAILURE;
     }
 
     Result result;
-    int exit_code = PROCESS_SUCCESS;
-
-    for (int i = 1; i < argc; i++)
+    if (args.argc() == 0)
     {
-        result = cat(argv[i]);
-
+        result = cat(IO::in());
         if (result != SUCCESS)
         {
-            IO::errln("{}: {}: {}", argv[0], argv[i], get_result_description(result));
-            exit_code = PROCESS_FAILURE;
+            IO::errln("{}: {}: {}", argv[0], "STDIN", get_result_description(result));
+            return PROCESS_FAILURE;
         }
     }
 
-    return exit_code;
+    int process_result = PROCESS_SUCCESS;
+    for (auto filepath : args.argv())
+    {
+        IO::File file(filepath, OPEN_READ);
+
+        if (file.result() != Result::SUCCESS)
+        {
+            IO::errln("{}: {}: {}", argv[0], filepath, get_result_description(file.result()));
+            process_result = PROCESS_FAILURE;
+            continue;
+        }
+
+        result = cat(file);
+
+        if (result != SUCCESS)
+        {
+            IO::errln("{}: {}: {}", argv[0], filepath, get_result_description(result));
+            process_result = PROCESS_FAILURE;
+        }
+    }
+
+    return process_result;
 }

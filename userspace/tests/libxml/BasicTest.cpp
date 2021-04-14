35260a4be6e190f7745ca7984402fed6716c0d13
Stephan Vedder <stephan.vedder@gmail.com>
2021-04-14 21:42:39 +0200

    libxml: fix parsing

--- userspace/tests/libxml/BasicTest.cpp
+++ userspace/tests/libxml/BasicTest.cpp
@@ -5,7 +5,7 @@
 TEST(xml_valid)
 {
     String xml_string =
-R"(<Test TestId="0001" TestType="CMD">
+        R"(<Test TestId="0001" TestType="CMD">
   <Name>Convert number to string</Name>
   <CommandLine>Examp1.EXE</CommandLine>
   <Input>1</Input>
@@ -13,6 +13,10 @@ R"(<Test TestId="0001" TestType="CMD">
 </Test>)";
 
     IO::MemoryReader reader(xml_string);
-    Xml::Document doc;
-    Assert::equal(Xml::parse(reader).result(), Result::SUCCESS);
+    auto result = Xml::parse(reader);
+    Assert::equal(result.result(), Result::SUCCESS);
+
+    Xml::Document doc = result.unwrap();
+    Assert::equal(doc.root().name(), "Test");
+    Assert::equal(doc.root().children().count(), 4);
 }

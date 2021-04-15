6f4a501a8e68fe6a39df5a688ac0e624640e0caa
Stephan Vedder <vedder@mbits.info>
2021-04-15 10:33:16 +0200

    libxml: add attribute parsing

--- userspace/tests/libxml/BasicTest.cpp
+++ userspace/tests/libxml/BasicTest.cpp
@@ -1,23 +1,28 @@
+#include <libio/File.h>
 #include <libxml/Parser.h>
 
 #include "tests/Driver.h"
 
 TEST(xml_valid)
 {
-    String xml_string =
-        R"(<Test TestId="0001" TestType="CMD">
-  <Name>Convert number to string</Name>
-  <CommandLine>Examp1.EXE</CommandLine>
-  <Input>1</Input>
-  <Output>One</Output>
-</Test>)";
-
-    IO::MemoryReader reader(xml_string);
-    auto result = Xml::parse(reader);
+    IO::File file("/Files/Tests/xml/basic.xml", OPEN_READ);
+    Assert::equal(file.exist(), true);
+    auto result = Xml::parse(file);
     Assert::equal(result.result(), Result::SUCCESS);
 
     Xml::Document doc = result.unwrap();
+
+    // Check the root
     Assert::equal(doc.root().name(), "Test");
     Assert::equal(doc.root().attributes().count(), 2);
     Assert::equal(doc.root().children().count(), 4);
+
+    // Check the testid attribute
+    Assert::is_true(doc.root().attributes().has_key("TestId"));
+    Assert::equal(doc.root().attributes()["TestId"], "0001");
+
+    // Check the name child
+    auto &child = doc.root().children()[0];
+    Assert::equal(child.name(), "Name");
+    Assert::equal(child.content(), "Convert number to string");
 }

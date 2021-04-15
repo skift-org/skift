391e0eaff66ead650fd0b397141dd77360f26ca5
Stephan Vedder <vedder@mbits.info>
2021-04-15 11:06:22 +0200

    libxml: test all svg files

---
+++ userspace/tests/libxml/SvgTest.cpp
@@ -0,0 +1,27 @@
+#include <libio/Directory.h>
+#include <libio/File.h>
+#include <libxml/Parser.h>
+
+#include "tests/Driver.h"
+
+TEST(xml_all_svg_test)
+{
+    IO::Directory icon_dir("/Files/Icons");
+
+    Assert::is_true(icon_dir.exist());
+
+    for (const auto &entry : icon_dir.entries())
+    {
+        auto path = Path::parse(IO::format("/Files/Icons/{}", entry.name));
+        if (path.extension() != "svg")
+        {
+            continue;
+        }
+
+        IO::File file(path, OPEN_READ);
+        Assert::is_true(file.exist());
+
+        auto result = Xml::parse(file);
+        Assert::equal(result.result(), Result::SUCCESS);
+    }
+}

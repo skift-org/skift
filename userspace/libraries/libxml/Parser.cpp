bfab631d059af199fb4af32167bbeacca7ba3381
Stephan Vedder <stephan.vedder@gmail.com>
2021-04-15 07:41:36 +0200

    libxml: start working on attribute parsing

--- userspace/libraries/libxml/Parser.cpp
+++ userspace/libraries/libxml/Parser.cpp
@@ -1,5 +1,6 @@
 #include <libio/NumberScanner.h>
 #include <libsystem/Logger.h>
+#include <libutils/HashMap.h>
 #include <libxml/Parser.h>
 
 ResultOr<String> read_comment(IO::Scanner &scan)
@@ -21,22 +22,65 @@ ResultOr<String> read_comment(IO::Scanner &scan)
     return builder.finalize();
 }
 
-ResultOr<String> read_tag(IO::Scanner &scan)
+Result read_attribute(IO::Scanner &scan, String &name, String &value)
 {
     StringBuilder builder{};
 
+    while (scan.current() != '=')
+    {
+        builder.append(scan.current());
+        scan.forward();
+    }
+    name = builder.finalize();
+    scan.skip('=');
+    while (scan.current() != ' ' && scan.current() != '>')
+    {
+        builder.append(scan.current());
+        scan.forward();
+    }
+    value = builder.finalize();
+    return Result::SUCCESS;
+}
+
+ResultOr<String> read_tag(IO::Scanner &scan, HashMap<String, String> &attributes, bool read_attributes = false)
+{
+    StringBuilder builder{};
+
+    // Read tag-name
     bool scan_name = true;
-    // Read start-tag
     while (scan.current() != '>' && scan.do_continue())
     {
+        // There may be no whitespace before the tagname
+        // See https://www.w3.org/TR/REC-xml/#sec-starttags
+        if (scan_name)
+        {
+            if (scan.current() == ' ')
+            {
+                logger_error("Whitespaces before tagname not allowed");
+                return Result::ERR_INVALID_DATA;
+            }
+
+            builder.append(scan.current());
+        }
+        else if (scan.current() != ' ')
+        {
+            if (read_attributes)
+            {
+                String name, value;
+                read_attribute(scan, name, value);
+                attributes[name] = value;
+            }
+            else
+            {
+                logger_error("Unexpected symbol in end-tag");
+                return Result::ERR_INVALID_DATA;
+            }
+        }
+
+        scan.forward();
+
         if (scan.current() == ' ')
             scan_name = false;
-
-        if (scan_name)
-            builder.append(scan.current());
-
-        //TODO: handle attributes
-        scan.forward();
     }
     scan.forward();
 
@@ -46,13 +90,14 @@ ResultOr<String> read_tag(IO::Scanner &scan)
 ResultOr<String> read_end_tag(IO::Scanner &scan)
 {
     scan.forward(2);
-    return read_tag(scan);
+    HashMap<String, String> tmp;
+    return read_tag(scan, tmp, false);
 }
 
-ResultOr<String> read_start_tag(IO::Scanner &scan)
+ResultOr<String> read_start_tag(IO::Scanner &scan, HashMap<String, String> &attributes)
 {
     scan.forward();
-    return read_tag(scan);
+    return read_tag(scan, attributes, false);
 }
 
 Result read_node(IO::Scanner &scan, Xml::Node &node)
@@ -94,7 +139,7 @@ Result read_node(IO::Scanner &scan, Xml::Node &node)
         // Start-Tag
         else if (node.name().empty())
         {
-            node.name() = TRY(read_start_tag(scan));
+            node.name() = TRY(read_start_tag(scan, node.attributes()));
 
             StringBuilder builder{};
             while (scan.current() != '<')

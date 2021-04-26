99311f739100e079e07ff144c9b2e79478850208
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2021-04-26 20:18:06 +0200

    apps: Added a simple exemple application.

---
+++ userspace/apps/exemple/.build.mk
@@ -0,0 +1,4 @@
+APPS += EXEMPLE
+
+EXEMPLE_NAME = exemple
+EXEMPLE_LIBS = widget settings async graphic xml system io compression
---
+++ userspace/apps/exemple/main.cpp
@@ -0,0 +1,44 @@
+#include <libwidget/Application.h>
+#include <libwidget/Button.h>
+#include <libwidget/Elements.h>
+#include <libwidget/Layouts.h>
+#include <libwidget/TitleBar.h>
+#include <libwidget/Window.h>
+#include <libwidget/components/StatefullComponent.h>
+
+using namespace Widget;
+
+struct CounterComponent :
+    public StatefullComponent<int>
+{
+    RefPtr<Element> build()
+    {
+        return hflow({
+            fill(label(IO::format("You clicked {} times", state()))),
+            button(Button::TEXT, "Click Me!", [this] { state(state() + 1); }),
+        });
+    }
+};
+
+WIDGET_BUILDER(CounterComponent, counter);
+
+struct MainWindow : public Window
+{
+    MainWindow() : Window() {}
+
+    RefPtr<Element> build() override
+    {
+        return vflow({
+            titlebar(Graphic::Icon::get("duck"), "exemple"),
+            spacing(16, counter()),
+        });
+    }
+};
+
+int main(int, char const *[])
+{
+    auto win = new MainWindow();
+    win->resize_to_content();
+    win->show();
+    return Application::the()->run();
+}

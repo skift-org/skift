8d56cd80889e435f89d664354dcba9eab48af1c2
Stephan Vedder <stephan.vedder@gmail.com>
2021-05-01 10:19:24 +0200

    apps: update font manager

--- userspace/apps/font-manager/main.cpp
+++ userspace/apps/font-manager/main.cpp
@@ -1,6 +1,5 @@
 #include <libwidget/Application.h>
-#include <libwidget/Container.h>
-#include <libwidget/TitleBar.h>
+#include <libwidget/Components.h>
 #include <libwidget/Window.h>
 
 int main(int, char **)
@@ -8,14 +7,14 @@ int main(int, char **)
     Widget::Window *window = new Widget::Window(WINDOW_RESIZABLE);
     window->size(Math::Vec2i(500, 400));
 
-    window->root()->layout(VFLOW(0));
+    window->root()->DONT_USE_ME_layout(VFLOW(0));
 
-    window->root()->add<Widget::TitleBar>(Graphic::Icon::get("duck"), "Demos");
+    window->root()->add(Widget::titlebar(Graphic::Icon::get("font-format"), "Font manager"));
 
-    auto navbar = window->root()->add<Widget::Container>();
+    auto navbar = window->root()->add<Widget::Element>();
 
     navbar->insets(Insetsi(4, 4));
-    navbar->layout(HGRID(4));
+    navbar->DONT_USE_ME_layout(VFLOW(4));
 
     window->show();
 

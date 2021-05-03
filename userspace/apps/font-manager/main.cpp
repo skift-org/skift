7d97665991093c61a839717c727d84c539552b90
Stephan Vedder <stephan.vedder@gmail.com>
2021-05-03 22:27:39 +0200

    apps: fix fontmanager compilation

--- userspace/apps/font-manager/main.cpp
+++ userspace/apps/font-manager/main.cpp
@@ -7,16 +7,13 @@ int main(int, char **)
     Widget::Window *window = new Widget::Window(WINDOW_RESIZABLE);
     window->size(Math::Vec2i(500, 400));
 
-    window->root()->DONT_USE_ME_layout(VFLOW(0));
-
     window->root()->add(Widget::titlebar(Graphic::Icon::get("font-format"), "Font manager"));
 
     auto navbar = window->root()->add<Widget::Element>();
 
     navbar->insets(Insetsi(4, 4));
-    navbar->DONT_USE_ME_layout(VFLOW(4));
 
     window->show();
 
-    return Widget::Application::the()->run();
+    return Widget::Application::the().run();
 }

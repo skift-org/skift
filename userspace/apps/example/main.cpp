159e1af89a52c0b97d1d79a6a8924609e6b2a0ef
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2021-05-03 23:01:44 +0200

    example: Use the new stateful functional API.

--- userspace/apps/example/main.cpp
+++ userspace/apps/example/main.cpp
@@ -5,43 +5,33 @@
 
 using namespace Widget;
 
-struct CounterComponent : public Statefull<int>
-{
-    RefPtr<Element> build(int state) override
-    {
-        return hflow({
-            fill(label(IO::format("You clicked {} times", state))),
-            basic_button("Click Me!", [this, state] { update(state + 1); }),
-        });
-    }
-};
-
-WIDGET_BUILDER(CounterComponent, counter);
-
-struct MainWindow : public Window
-{
-    RefPtr<Element> build() override
-    {
-        return vflow({
-            titlebar(Graphic::Icon::get("duck"), "Example"),
-            spacing(16, counter()),
-        });
-    }
-};
-
-struct ExampleApplication : public Application
-{
-    OwnPtr<Window> build() override
-    {
-        return own<MainWindow>();
-    }
-};
-
 int main(int argc, char const *argv[])
 {
+    // clang-format off
+
     UNUSED(argc);
     UNUSED(argv);
 
-    ExampleApplication app;
+    Application app;
+
+    auto win = window(
+        vflow({
+            titlebar(Graphic::Icon::get("duck"), "Example"),
+            spacing(
+                16,
+                stateful<int>([](auto state, auto update) {
+                    return hflow({
+                        fill(label(IO::format("You clicked {} times", state))),
+                        basic_button("Click Me!", [=] { update(state + 1); }),
+                    });
+                })
+            ),
+        })
+    );
+
+    win->show();
+
     return app.run();
+
+    // clang-format on
 }

fdea7cebdb23423494f3cd0e2a90c5412ddac249
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2021-05-20 23:24:05 +0200

    example: Improved coding style.

--- userspace/apps/example/main.cpp
+++ userspace/apps/example/main.cpp
@@ -5,33 +5,41 @@
 
 using namespace Widget;
 
+namespace Example
+{
+
+struct Application : public Widget::Application
+{
+    OwnPtr<Window> build() override
+    {
+        // clang-format off
+
+        return window(
+            vflow({
+                titlebar(Graphic::Icon::get("duck"), "Example"),
+                spacing(
+                    16,
+                    stateful<int>([](auto state, auto update) {
+                        return hflow({
+                            fill(label(IO::format("You clicked {} times", state))),
+                            basic_button("Click Me!", [=] { update(state + 1); }),
+                        });
+                    })
+                ),
+            })
+        );
+
+        // clang-format on
+    }
+};
+
+} // namespace Example
+
 int main(int argc, char const *argv[])
 {
     UNUSED(argc);
     UNUSED(argv);
 
-    Application app;
-
-    // clang-format off
-
-    auto win = window(
-        vflow({
-            titlebar(Graphic::Icon::get("duck"), "Example"),
-            spacing(
-                16,
-                stateful<int>([](auto state, auto update) {
-                    return hflow({
-                        fill(label(IO::format("You clicked {} times", state))),
-                        basic_button("Click Me!", [=] { update(state + 1); }),
-                    });
-                })
-            ),
-        })
-    );
-
-    // clang-format on
-
-    win->show();
-
+    Example::Application app;
     return app.run();
 }

681619c78501b1736f7fafcf045b8eeacf2c03b5
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2021-05-20 23:22:49 +0200

    about: Improved coding style.

--- userspace/apps/about/main.cpp
+++ userspace/apps/about/main.cpp
@@ -6,82 +6,84 @@
 #include <libwidget/Views.h>
 
 using namespace Widget;
+using namespace Graphic;
 
-static auto logo_based_on_color_scheme()
-{
-    auto path = Widget::theme_is_dark()
-                    ? "/Applications/about/logo-white.png"
-                    : "/Applications/about/logo-black.png";
-
-    return Graphic::Bitmap::load_from_or_placeholder(path);
-}
-
-static void show_license()
+namespace About
 {
 
-    auto texteditor = Widget::texteditor(Widget::TextModel::open("/Files/license.md"));
-    texteditor->readonly(true);
-    texteditor->font(Graphic::Font::get("mono").unwrap());
-    texteditor->focus();
+static auto logo()
+{
+    auto logo_path = theme_is_dark()
+                         ? "/Applications/about/logo-white.png"
+                         : "/Applications/about/logo-black.png";
 
-    // clang-format off
+    auto logo_image = Bitmap::load_from_or_placeholder(logo_path);
 
-    auto license_window = window(
-        vflow({
-            titlebar(Graphic::Icon::get("information"), "License"),
-            fill(texteditor),
-        })
-    );
-
-    license_window->show();
-
-    // clang-format on
+    return spacing({32, 64}, image(logo_image, BitmapScaling::CENTER));
 }
+
+static auto license()
+{
+    return outline_button("License", [] {
+        auto editor = texteditor(TextModel::open("/Files/license.md"));
+        editor->readonly(true);
+        editor->font(Font::get("mono").unwrap());
+        editor->focus();
+
+        auto license_window = window(
+            vflow({
+                titlebar(Icon::get("information"), "License"),
+                fill(editor),
+            }));
+
+        license_window->show();
+    });
+}
+
+struct Application : public Widget::Application
+{
+    OwnPtr<Window> build() override
+    {
+        // clang-format off
+
+        return window(
+            vflow(4, {
+                titlebar(Icon::get("information"), "About"),
+
+                spacing(6,
+                    vflow(4,{
+                        logo(),
+                        label("The skift operating system.", Anchor::CENTER),
+                        label(__BUILD_VERSION__, Anchor::CENTER),
+                        label(__BUILD_GITREF__ "/" __BUILD_CONFIG__, Anchor::CENTER),
+                        label("Copyright © 2018-2021", Anchor::CENTER),
+                        label("N. Van Bossuyt & contributors.", Anchor::CENTER),
+                    })
+                ),
+
+                spacing(6,
+                    hflow(4,{
+                        license(),
+                        spacer(),
+                        filled_button("Ok", [this] {
+                            exit(PROCESS_SUCCESS);
+                        })
+                    })
+                )
+            })
+        );
+
+        // clang-format on
+    }
+};
+
+} // namespace About
 
 int main(int argc, char const *argv[])
 {
     UNUSED(argc);
     UNUSED(argv);
 
-    Application app;
-
-    // clang-format off
-
-    auto win = window(
-        vflow(4, {
-            titlebar(Graphic::Icon::get("information"), "About"),
-
-            spacing(6,
-                vflow(4,{
-                    spacing(
-                        {32, 64},
-                        image(logo_based_on_color_scheme(), Graphic::BitmapScaling::CENTER)
-                    ),
-                    label("The skift operating system.", Anchor::CENTER),
-                    label(__BUILD_VERSION__, Anchor::CENTER),
-                    label(__BUILD_GITREF__ "/" __BUILD_CONFIG__, Anchor::CENTER),
-                    label("Copyright © 2018-2021", Anchor::CENTER),
-                    label("N. Van Bossuyt & contributors.", Anchor::CENTER),
-                })
-            ),
-
-            spacing(6,
-                hflow(4,{
-                    outline_button("License",  [] {
-                        show_license();
-                    }),
-                    spacer(),
-                    filled_button("Ok", [&] {
-                        app.exit(PROCESS_SUCCESS);
-                    })
-                })
-            )
-        })
-    );
-
-    // clang-format on
-
-    win->show();
-
+    About::Application app;
     return app.run();
 }

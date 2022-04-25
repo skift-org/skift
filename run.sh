cee1ca6800d2f9014763de2b5d69e9f52bf8a8cd
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2022-04-25 23:15:38 +0200

    meta: Poor man compiler explorer.

--- run.sh
+++ run.sh
@@ -2,3 +2,11 @@
 mkdir -p bin
 
 clang++ $(cat compile_flags.txt) -o bin/$1.out $1.cpp && ./bin/$1.out
+
+echo
+echo ----------------------------
+echo
+date
+echo
+echo "Command exited with code $?"
+echo
---
+++ watch.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+mkdir -p bin
+
+while [ : ]
+do
+clang++ -O3 -S $(cat compile_flags.txt) $1.cpp -o - 2> bin/err.txt | c++filt > bin/code.asm
+./run.sh $1 2> /dev/null > bin/out.txt
+sleep 3s
+done

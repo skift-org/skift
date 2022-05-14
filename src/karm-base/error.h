acc1c575976c7992d493b1b219aaf8e9e9e29c0a
smnx <me@smnx.sh>
2022-05-14 10:39:55 +0200

    base: Allow Error code to be confverted to Str.

--- src/karm-base/error.h
+++ src/karm-base/error.h
@@ -3,53 +3,58 @@
 #include "_prelude.h"
 
 #include "macros.h"
+#include "string.h"
 
 namespace Karm::Base {
 
+#define FOREACH_ERROR(ERROR)         \
+    ERROR(OK)                        \
+    ERROR(NOT_FOUND)                 \
+    ERROR(PERMISSION_DENIED)         \
+    ERROR(CONNECTION_REFUSED)        \
+    ERROR(CONNECTION_RESET)          \
+    ERROR(HOST_UNREACHABLE)          \
+    ERROR(NETWORK_UNREACHABLE)       \
+    ERROR(CONNECTION_ABORTED)        \
+    ERROR(NOT_CONNECTED)             \
+    ERROR(ADDR_IN_USE)               \
+    ERROR(ADDR_NOT_AVAILABLE)        \
+    ERROR(NETWORK_DOWN)              \
+    ERROR(BROKEN_PIPE)               \
+    ERROR(ALREADY_EXISTS)            \
+    ERROR(WOULD_BLOCK)               \
+    ERROR(NOT_A_DIRECTORY)           \
+    ERROR(IS_A_DIRECTORY)            \
+    ERROR(DIRECTORY_NOT_EMPTY)       \
+    ERROR(READ_ONLY_FILESYSTEM)      \
+    ERROR(FILESYSTEM_LOOP)           \
+    ERROR(STALE_NETWORK_FILE_HANDLE) \
+    ERROR(INVALID_INPUT)             \
+    ERROR(INVALID_DATA)              \
+    ERROR(TIMED_OUT)                 \
+    ERROR(WRITE_ZERO)                \
+    ERROR(STORAGE_FULL)              \
+    ERROR(NOT_SEEKABLE)              \
+    ERROR(FILESYSTEM_QUOTA_EXCEEDED) \
+    ERROR(FILE_TOO_LARGE)            \
+    ERROR(RESOURCE_BUSY)             \
+    ERROR(EXECUTABLE_FILE_BUSY)      \
+    ERROR(DEADLOCK)                  \
+    ERROR(CROSSES_DEVICES)           \
+    ERROR(TOO_MANY_LINKS)            \
+    ERROR(INVALID_FILENAME)          \
+    ERROR(ARGUMENT_LIST_TOO_LONG)    \
+    ERROR(INTERRUPTED)               \
+    ERROR(UNSUPPORTED)               \
+    ERROR(UNEXPECTED_EOF)            \
+    ERROR(OUT_OF_MEMORY)             \
+    ERROR(OTHER)
+
 struct [[nodiscard]] Error {
     enum Code {
-        OK,
-
-        NOT_FOUND,
-        PERMISSION_DENIED,
-        CONNECTION_REFUSED,
-        CONNECTION_RESET,
-        HOST_UNREACHABLE,
-        NETWORK_UNREACHABLE,
-        CONNECTION_ABORTED,
-        NOT_CONNECTED,
-        ADDR_IN_USE,
-        ADDR_NOT_AVAILABLE,
-        NETWORK_DOWN,
-        BROKEN_PIPE,
-        ALREADY_EXISTS,
-        WOULD_BLOCK,
-        NOT_A_DIRECTORY,
-        IS_A_DIRECTORY,
-        DIRECTORY_NOT_EMPTY,
-        READ_ONLY_FILESYSTEM,
-        FILESYSTEM_LOOP,
-        STALE_NETWORK_FILE_HANDLE,
-        INVALID_INPUT,
-        INVALID_DATA,
-        TIMED_OUT,
-        WRITE_ZERO,
-        STORAGE_FULL,
-        NOT_SEEKABLE,
-        FILESYSTEM_QUOTA_EXCEEDED,
-        FILE_TOO_LARGE,
-        RESOURCE_BUSY,
-        EXECUTABLE_FILE_BUSY,
-        DEADLOCK,
-        CROSSES_DEVICES,
-        TOO_MANY_LINKS,
-        INVALID_FILENAME,
-        ARGUMENT_LIST_TOO_LONG,
-        INTERRUPTED,
-        UNSUPPORTED,
-        UNEXPECTED_EOF,
-        OUT_OF_MEMORY,
-        OTHER,
+#define ITER(NAME) NAME,
+        FOREACH_ERROR(ITER)
+#undef ITER
     } _code;
 
     constexpr Error() : _code(OK) {}
@@ -58,8 +63,22 @@ struct [[nodiscard]] Error {
     operator bool() const { return _code == OK; }
 
     Code code() const { return _code; }
+
     Error none() const { return *this; }
+
     Error unwrap() const { return *this; }
+
+    Base::Str msg() {
+        switch (_code) {
+#define ITER(NAME) \
+    case NAME:     \
+        return #NAME;
+            FOREACH_ERROR(ITER)
+#undef ITER
+        default:
+            return "UNKNOWN";
+        }
+    }
 };
 
 inline constexpr Error::Code OK = Error::OK;

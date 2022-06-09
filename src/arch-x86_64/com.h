d2e83d2d087d3d411daf691af1462ca44c7d8d89
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2022-06-09 22:28:56 +0200

    arch-x86_64: Simple COM driver.

---
+++ src/arch-x86_64/com.h
@@ -0,0 +1,130 @@
+#pragma once
+
+#include "asm.h"
+
+namespace x86_64 {
+
+struct Com {
+    enum Ports {
+        COM1 = 0x3F8,
+        COM2 = 0x2F8,
+        COM3 = 0x3E8,
+        COM4 = 0x2E8,
+    };
+
+    enum Regs {
+        DATA = 0,
+        INTERRUPT = 1,
+        BAUD_RATE_LOW = 0,
+        BAUD_RATE_HIGH = 1,
+        INTERRUPT_IDENTIFICATOR = 2,
+        FIFO_CONTROLLER = 2,
+        LINE_CONTROL = 3,
+        MODEM_CONTROL = 4,
+        LINE_STATUS = 5,
+        MODEM_STATUS = 6,
+        SCRATCH_REGISTER = 7,
+    };
+
+    enum LineCtrls {
+        DATA_SIZE_5 = 0,
+        DATA_SIZE_6 = 1,
+        DATA_SIZE_7 = 2,
+        DATA_SIZE_8 = 3,
+        DLAB_STATUS = 1 << 7,
+    };
+
+    enum ModemCtrl {
+        MODEM_DTR = 1 << 0,
+        MODEM_RTS = 1 << 1,
+        MODEM_OUT1 = 1 << 2,
+        MODEM_OUT2 = 1 << 3,
+        MODEM_LOOPBACK = 1 << 4,
+    };
+
+    enum Int {
+        WHEN_DATA_AVAILABLE = 1 << 0,
+        WHEN_TRANSMITTER_EMPTY = 1 << 1,
+        WHEN_BREAK_ERROR = 1 << 2,
+        WHEN_STATUS_UPDATE = 1 << 3,
+    };
+
+    enum LineStatus {
+        DATA_READY = 1 << 0,
+        OVERRUN_ERROR = 1 << 1,
+        PARITY_ERROR = 1 << 2,
+        FRAMING_ERROR = 1 << 3,
+        BREAK_INDICATOR = 1 << 4,
+        TRANSMITTER_BUF_EMPTY = 1 << 5,
+        TRANSMITTER_EMPTY = 1 << 6,
+        IMPENDING_ERROR = 1 << 7,
+    };
+
+    Ports _port;
+
+    Com(Ports port) : _port(port) {}
+
+    void init() {
+        writeReg(INTERRUPT_IDENTIFICATOR, 0);
+
+        // Turn on dlab for setting baud rate
+        writeReg(LINE_CONTROL, DLAB_STATUS);
+
+        // Set bauds
+        writeReg(BAUD_RATE_LOW, 115200 / 9600);
+        writeReg(BAUD_RATE_HIGH, 0);
+
+        // we want 8bit caracters + clear dlab
+        writeReg(LINE_CONTROL, DATA_SIZE_8);
+
+        // turn on communication + redirect UART interrupt into ICU
+        writeReg(MODEM_CONTROL, MODEM_DTR | MODEM_RTS | MODEM_OUT2);
+        writeReg(INTERRUPT, WHEN_DATA_AVAILABLE);
+    }
+
+    void writeReg(Regs reg, uint8_t value) {
+        out8((uint16_t)_port + reg, value);
+    }
+
+    uint8_t readReg(Regs reg) {
+        return in8((uint16_t)_port + reg);
+    }
+
+    bool canRead() {
+        return (readReg(LINE_STATUS) & DATA_READY);
+    }
+
+    bool canWrite() {
+        return (readReg(LINE_STATUS) & TRANSMITTER_BUF_EMPTY);
+    }
+
+    void waitWrite() {
+        while (!canWrite()) {
+        }
+    }
+
+    void waitRead() {
+        while (!canRead()) {
+        }
+    }
+
+    void putc(char c) {
+        waitWrite();
+        writeReg(DATA, c);
+    }
+
+    char getc() {
+        waitRead();
+        return readReg(DATA);
+    }
+
+    size_t write(void const *buf, size_t size) {
+        for (size_t i = 0; i < size; i++) {
+            putc(((char const *)buf)[i]);
+        }
+
+        return size;
+    }
+};
+
+} // namespace x86_64

4a88a389c42265cf1da4e2ec1db401c536a0f616
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2022-07-21 08:41:33 +0200

    karm-text: Add Scan::nextUint, Scan::nextInt, Scan::nextFloat.

--- src/karm-text/scan.h
+++ src/karm-text/scan.h
@@ -36,7 +36,7 @@ struct _Scan {
         return E::decodeUnit(r, curr) ? r : U'�';
     }
 
-    Rune peek(size_t count) {
+    Rune peek(size_t count = 0) {
         auto save = _cursor;
         next(count);
         auto r = curr();
@@ -124,6 +124,121 @@ struct _Scan {
     Str end() {
         return {_begin, _cursor};
     }
+
+    /* --- Number parsing --------------------------------------------------- */
+
+    Opt<uint8_t> _parseDigit(Rune rune, size_t base = 10) {
+        rune = tolower(rune);
+        uint8_t result = 255;
+
+        if (isalpha(rune))
+            result = rune - 'a' + 10;
+        else if (isdigit(rune))
+            result = rune - '0';
+
+        if (result >= base)
+            return NONE;
+
+        return result;
+    }
+
+    Opt<uint8_t> nextDigit(size_t base = 10) {
+        if (ended())
+            return NONE;
+
+        auto d = _parseDigit(peek(), base);
+
+        if (d)
+            next();
+
+        return d;
+    }
+
+    Opt<uint64_t> nextUint(size_t base = 10) {
+        bool isNum = false;
+        uint64_t result = 0;
+
+        while (!ended()) {
+            auto maybeDigit = nextDigit(base);
+            if (!maybeDigit) {
+                break;
+            }
+            isNum = true;
+            result = result * base + maybeDigit.unwrap();
+        }
+
+        if (!isNum)
+            return NONE;
+
+        return result;
+    }
+
+    Opt<int64_t> nextInt(size_t base = 10) {
+        bool isNeg = false;
+        bool isNum = false;
+        int64_t result = 0;
+
+        if (peek(0) == '-' && _parseDigit(peek(1), base)) {
+            isNeg = true;
+            isNum = true;
+            next();
+        }
+
+        while (!ended()) {
+            auto maybeDigit = nextDigit(base);
+            if (!maybeDigit) {
+                break;
+            }
+            isNum = true;
+            result = result * base + maybeDigit.unwrap();
+        }
+
+        if (!isNum)
+            return NONE;
+
+        if (isNeg)
+            result = -result;
+
+        return result;
+    }
+
+#ifndef __osdk_freestanding__
+
+    Opt<double> nextFloat(size_t base = 10) {
+        int64_t ipart = 0.0;
+        double fpart = 0.0;
+        int64_t exp = 0;
+
+        if (peek(0) != '.' || !_parseDigit(peek(1), base)) {
+            ipart = try$(nextInt(base));
+        }
+        if (skip('.')) {
+            double multiplier = (1.0 / base);
+            while (!ended()) {
+                auto maybeDigit = nextDigit(base);
+                if (!maybeDigit) {
+                    break;
+                }
+                fpart += maybeDigit.unwrap() * multiplier;
+                multiplier /= base;
+            }
+        }
+
+        if (skip('e') || skip('E')) {
+            auto maybeExp = nextInt(base);
+            if (maybeExp) {
+                exp = maybeExp.unwrap();
+            }
+        }
+
+        if (ipart < 0) {
+            return ipart - fpart * pow(base, exp);
+        } else {
+            return ipart + fpart * pow(base, exp);
+        }
+    }
+
+#endif
 };
 
 using Scan = _Scan<Utf8>;

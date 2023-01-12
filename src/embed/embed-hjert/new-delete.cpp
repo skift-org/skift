e7732ca08dd450ee7052778821cfe1ceaaabb97a
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2023-01-13 00:23:47 +0100

    embed-hjert: Implement new and delete operators.

--- src/embed/embed-hjert/new-delete.cpp
+++ src/embed/embed-hjert/new-delete.cpp
@@ -1,26 +1,59 @@
-#include <karm-base/panic.h>
-#include <karm-base/std.h>
+#include <hjert-core/mem.h>
+#include <karm-base/lock.h>
+#include <karm-logger/logger.h>
+#include <libheap/libheap.h>
 
-void *__attribute__((weak)) operator new(size_t) {
-    notImplemented();
+/* --- Heap Implementation -------------------------------------------------- */
+
+static Lock _heapLock;
+static Heap _heapImpl = {
+    .alloc = [](void *, size_t size) -> void * {
+        uintptr_t start = Hjert::Mem::heap()
+                              .allocRange(size)
+                              .unwrap("heap: failed to allocate block")
+                              .start;
+        return reinterpret_cast<void *>(start);
+    },
+    .free = [](void *, void *ptr, size_t size) {
+        Hjert::Mem::heap()
+            .free(Hal::HeapRange(reinterpret_cast<uintptr_t>(ptr), size))
+            .unwrap("heap: failed to free block");
+    },
+    .log = [](void *, enum HeapLogType type, const char *msg, va_list) {
+        if (type == HEAP_ERROR) {
+            logError("heap: {}", msg);
+        }
+    },
+};
+
+/* --- New/Delete Implementation -------------------------------------------- */
+
+void *__attribute__((weak)) operator new(size_t size) {
+    LockScope scope(_heapLock);
+    return heap_calloc(&_heapImpl, size, 1);
 }
 
-void *__attribute__((weak)) operator new[](size_t) {
-    notImplemented();
+void *__attribute__((weak)) operator new[](size_t size) {
+    LockScope scope(_heapLock);
+    return heap_calloc(&_heapImpl, size, 1);
 }
 
-void __attribute__((weak)) operator delete(void *) {
-    notImplemented();
+void __attribute__((weak)) operator delete(void *ptr) {
+    LockScope scope(_heapLock);
+    heap_free(&_heapImpl, ptr);
 }
 
-void __attribute__((weak)) operator delete[](void *) {
-    notImplemented();
+void __attribute__((weak)) operator delete[](void *ptr) {
+    LockScope scope(_heapLock);
+    heap_free(&_heapImpl, ptr);
 }
 
-void __attribute__((weak)) operator delete(void *, size_t) {
-    notImplemented();
+void __attribute__((weak)) operator delete(void *ptr, size_t) {
+    LockScope scope(_heapLock);
+    heap_free(&_heapImpl, ptr);
 }
 
-void __attribute__((weak)) operator delete[](void *, size_t) {
-    notImplemented();
+void __attribute__((weak)) operator delete[](void *ptr, size_t) {
+    LockScope scope(_heapLock);
+    heap_free(&_heapImpl, ptr);
 }

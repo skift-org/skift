2b89421f4b0b69dc58813227e23cb2a770b0ac84
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2023-02-08 23:36:30 +0100

    meta: Improved image generation code.

--- meta/plugins/start-cmd.py
+++ meta/plugins/start-cmd.py
@@ -1,8 +1,11 @@
 import os
 
-from osdk import utils, shell, builder
+from osdk import utils, shell, builder, const
 from osdk.cmds import Cmd, append
 from osdk.args import Args
+from osdk.logger import Logger
+from pathlib import Path
+from typing import Protocol
 
 
 def kvmAvailable() -> bool:
@@ -12,42 +15,96 @@ def kvmAvailable() -> bool:
     return False
 
 
+class Image:
+    logger: Logger
+    root: str
+
+    def __init__(self, id: str):
+        self.logger = Logger(f"image:{id}")
+        self.root = f".osdk/images/{id}"
+
+    def install(self, componentSpec: str, targetSpec: str, dest: str):
+        self.logger.log(f"Installing {componentSpec} to {dest}...")
+        component = builder.build(componentSpec, targetSpec)
+        shell.mkdir(Path(f"{self.root}/{dest}").parent)
+        shell.cp(component, f"{self.root}/{dest}")
+
+    def cp(self, src: str, dest: str):
+        self.logger.log(f"Copying {src} to {dest}...")
+        shell.mkdir(Path(f"{self.root}/{dest}").parent)
+        shell.cp(src, f"{self.root}/{dest}")
+
+    def mkdir(self, path: str):
+        self.logger.log(f"Creating directory {path}...")
+        shell.mkdir(f"{self.root}/{path}")
+
+    def wget(self, url: str, dest: str):
+        self.logger.log(f"Downloading {url} to {dest}...")
+        shell.mkdir(Path(f"{self.root}/{dest}").parent)
+        shell.wget(url, f"{self.root}/{dest}")
+
+
+class Machine(Protocol):
+    logger: Logger
+
+    def __init__(self, id: str):
+        self.logger = Logger(f"machine:{id}")
+
+    def boot(self, image: Image) -> None:
+        ...
+
+
+class QemuSystemAmd64(Machine):
+    logError = False
+    useDebug = False
+
+    def __init__(self, logError: bool = False, useDebug: bool = False):
+        super().__init__("qemu-system-x86_64")
+        self.logError = logError
+        self.useDebug = useDebug
+
+    def boot(self, image: Image) -> None:
+        self.logger.log("Booting...")
+
+        ovmf = shell.wget(
+            "https://retrage.github.io/edk2-nightly/bin/RELEASEX64_OVMF.fd")
+
+        qemuCmd: list[str] = [
+            "qemu-system-x86_64",
+            "-machine", "q35",
+            "-no-reboot",
+            "-no-shutdown",
+            "-serial", "mon:stdio",
+            "-bios", ovmf,
+            "-m", "256M",
+            "-smp", "4",
+            "-drive", f"file=fat:rw:{image.root},media=disk,format=raw",
+        ]
+
+        if self.logError:
+            qemuCmd += ["-d", "guest_errors,cpu_reset,int"]
+
+        if self.useDebug:
+            qemuCmd += ["-s", "-S"]
+
+        if kvmAvailable():
+            qemuCmd += ["-enable-kvm"]
+        else:
+            print("KVM not available, using QEMU-TCG")
+
+        shell.exec(*qemuCmd)
+
+
 def bootCmd(args: Args) -> None:
-    imageDir = shell.mkdir(".osdk/images/efi-x86_64")
-    efiBootDir = shell.mkdir(f"{imageDir}/EFI/BOOT")
-    bootDir = shell.mkdir(f"{imageDir}/boot")
+    image = Image("efi-x86_64")
 
-    ovmf = shell.wget(
-        "https://retrage.github.io/edk2-nightly/bin/RELEASEX64_OVMF.fd")
+    image.install("hjert", "kernel-x86_64", "boot/kernel.elf")
+    image.install("loader", "efi-x86_64", "EFI/BOOT/BOOTX64.EFI")
+    image.install("system-srv", "skift-x86_64", "servers/system")
+    image.cp("meta/image/loader.json", "boot/loader.json")
 
-    hjert = builder.build("hjert", "kernel-x86_64")
-    shell.cp(hjert, f"{bootDir}/kernel.elf")
-
-    loader = builder.build("loader", "efi-x86_64")
-    shell.cp(loader, f"{efiBootDir}/BOOTX64.EFI")
-
-    systemSrv = builder.build("system-srv", "skift-x86_64")
-    shell.cp(systemSrv, f"{imageDir}/system-srv")
-
-    qemuCmd: list[str] = [
-        "qemu-system-x86_64",
-        "-machine", "q35",
-        "-no-reboot",
-        "-no-shutdown",
-        # "-d", "guest_errors,cpu_reset,int",
-        "-serial", "mon:stdio",
-        "-bios", ovmf,
-        "-m", "256M",
-        "-smp", "4",
-        "-drive", f"file=fat:rw:{imageDir},media=disk,format=raw",
-    ]
-
-    if kvmAvailable():
-        qemuCmd += ["-enable-kvm"]
-    else:
-        print("KVM not available, using QEMU-TCG")
-
-    shell.exec(*qemuCmd)
+    machine = QemuSystemAmd64()
+    machine.boot(image)
 
 
 append(Cmd("s", "start", "Boot the system", bootCmd))

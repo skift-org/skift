085b96280b4e603b62118f01b28047466e18f8b7
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2023-03-23 19:57:09 +0100

    image-viewer: Simple mockup ui.

--- src/apps/image-viewer-app/main.cpp
+++ src/apps/image-viewer-app/main.cpp
@@ -1,22 +1,45 @@
 #include <karm-main/main.h>
 #include <karm-ui/app.h>
 #include <karm-ui/dialog.h>
+#include <karm-ui/input.h>
 #include <karm-ui/layout.h>
 #include <karm-ui/scafold.h>
 #include <karm-ui/view.h>
 
+auto NOP(Ui::Node &) {}
+
+Ui::Child toolbar() {
+    return Ui::toolbar(
+        Ui::button(NOP, Ui::ButtonStyle::subtle(), Mdi::Icon::MAGNIFY_PLUS),
+        Ui::button(NOP, Ui::ButtonStyle::subtle(), Mdi::Icon::MAGNIFY_MINUS),
+        Ui::button(NOP, Ui::ButtonStyle::subtle(), Mdi::Icon::FULLSCREEN),
+        Ui::grow(NONE),
+        Ui::button(NOP, Ui::ButtonStyle::subtle(), Mdi::Icon::PENCIL, "Edit"));
+}
+
+Ui::Child controls() {
+    return Ui::hflow(
+               Ui::button(NOP, Ui::ButtonStyle::subtle(), Mdi::Icon::ARROW_LEFT),
+               Ui::button(NOP, Ui::ButtonStyle::subtle(), Mdi::Icon::ARROW_RIGHT)) |
+           Ui::spacing({0, 0, 0, 8}) |
+           Ui::center();
+}
+
 Res<> entryPoint(CliArgs args) {
     auto titlebar = Ui::titlebar(Mdi::IMAGE, "Image Viewer");
 
-    auto content = Ui::image(try$(Media::loadImage(args[0])));
+    auto content =
+        Ui::image(try$(Media::loadImage(args[0])), 8) |
+        Ui::spacing(8) |
+        Ui::fit();
 
-    auto layout = Ui::maxSize(
-        {700, 500},
-        Ui::vflow(
-            titlebar,
-            Ui::grow(
-                Ui::fit(
-                    content))));
+    auto layout = Ui::vflow(
+                      titlebar,
+                      toolbar(),
+                      content | Ui::grow(),
+                      controls()) |
+                  Ui::maxSize({700, 500}) |
+                  Ui::dialogLayer();
 
-    return Ui::runApp(args, Ui::dialogLayer(layout));
+    return Ui::runApp(args, layout);
 }

d7867e9f8365247f55ab90c6aa5cb2cd268842fe
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2023-04-04 16:24:37 +0200

    settings-app: Working home buttons.

--- src/apps/settings-app/page-home.cpp
+++ src/apps/settings-app/page-home.cpp
@@ -5,31 +5,31 @@
 
 namespace Settings {
 
-Ui::Child tileButton(Mdi::Icon icon, String text) {
+Ui::Child tileButton(Ui::OnPress onPress, Mdi::Icon icon, String text) {
     return Ui::vflow(
                Ui::icon(icon, 36) | Ui::center() | Ui::grow(),
                Ui::text(text) | Ui::center()) |
            Ui::spacing(8) |
            Ui::bound() |
            Ui::minSize({96, 72}) |
-           Ui::button(
-               [](auto &) {
-               },
-               Ui::ButtonStyle::secondary());
+           Ui::button(std::move(onPress),
+                      Ui::ButtonStyle::secondary());
 }
 
 Ui::Child pageHome(State const &) {
     return Ui::grid(
                Ui::GridStyle::simpleFixed({3, 96}, {3, 120}, 8),
                {
-                   tileButton(Mdi::ACCOUNT, "Accounts"),
-                   tileButton(Mdi::PALETTE, "Personalization"),
-                   tileButton(Mdi::WIDGETS_OUTLINE, "Applications"),
-                   tileButton(Mdi::LAPTOP, "System"),
-                   tileButton(Mdi::WIFI, "Network"),
-                   tileButton(Mdi::SECURITY, "Security & Privacy"),
-                   tileButton(Mdi::UPDATE, "Updates"),
-                   tileButton(Mdi::INFORMATION_OUTLINE, "About"),
+                   tileButton(Model::bind<GoTo>(Page::ACCOUNT), Mdi::ACCOUNT, "Accounts"),
+                   tileButton(Model::bind<GoTo>(Page::PERSONALIZATION), Mdi::PALETTE, "Personalization"),
+                   tileButton(Model::bind<GoTo>(Page::APPLICATIONS), Mdi::WIDGETS_OUTLINE, "Applications"),
+
+                   tileButton(Model::bind<GoTo>(Page::SYSTEM), Mdi::LAPTOP, "System"),
+                   tileButton(Model::bind<GoTo>(Page::NETWORK), Mdi::WIFI, "Network"),
+                   tileButton(Model::bind<GoTo>(Page::SECURITY), Mdi::SECURITY, "Security & Privacy"),
+
+                   tileButton(Model::bind<GoTo>(Page::UPDATES), Mdi::UPDATE, "Updates"),
+                   tileButton(Model::bind<GoTo>(Page::ABOUT), Mdi::INFORMATION_OUTLINE, "About"),
                }) |
            Ui::spacing(8) | Ui::center() | Ui::bound() | Ui::grow();
 }

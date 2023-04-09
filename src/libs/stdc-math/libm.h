e83d781badd46a38049d97e1f6734fce306358ee
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2023-04-09 19:38:44 +0200

    stdc-math: Added missing fenv.h

--- src/libs/stdc-math/libm.h
+++ src/libs/stdc-math/libm.h
@@ -21,8 +21,7 @@
 
 #if LDBL_MANT_DIG == 53 && LDBL_MAX_EXP == 1024
 #elif LDBL_MANT_DIG == 64 && LDBL_MAX_EXP == 16384 && __BYTE_ORDER == __LITTLE_ENDIAN
-union ldshape
-{
+union ldshape {
     long double f;
     struct
     {
@@ -31,8 +30,7 @@ union ldshape
     } i;
 };
 #elif LDBL_MANT_DIG == 113 && LDBL_MAX_EXP == 16384 && __BYTE_ORDER == __LITTLE_ENDIAN
-union ldshape
-{
+union ldshape {
     long double f;
     struct
     {
@@ -48,8 +46,7 @@ union ldshape
     } i2;
 };
 #elif LDBL_MANT_DIG == 113 && LDBL_MAX_EXP == 16384 && __BYTE_ORDER == __BIG_ENDIAN
-union ldshape
-{
+union ldshape {
     long double f;
     struct
     {
@@ -68,35 +65,27 @@ union ldshape
 #    error Unsupported long double representation
 #endif
 
-#define FORCE_EVAL(x)                         \
-    do                                        \
-    {                                         \
-        if (sizeof(x) == sizeof(float))       \
-        {                                     \
-            volatile float __x;               \
-            __x = (x);                        \
-            (void)__x;                        \
-        }                                     \
-        else if (sizeof(x) == sizeof(double)) \
-        {                                     \
-            volatile double __x;              \
-            __x = (x);                        \
-            (void)__x;                        \
-        }                                     \
-        else                                  \
-        {                                     \
-            volatile long double __x;         \
-            __x = (x);                        \
-            (void)__x;                        \
-        }                                     \
+#define FORCE_EVAL(x)                             \
+    do {                                          \
+        if (sizeof(x) == sizeof(float)) {         \
+            volatile float __x;                   \
+            __x = (x);                            \
+            (void)__x;                            \
+        } else if (sizeof(x) == sizeof(double)) { \
+            volatile double __x;                  \
+            __x = (x);                            \
+            (void)__x;                            \
+        } else {                                  \
+            volatile long double __x;             \
+            __x = (x);                            \
+            (void)__x;                            \
+        }                                         \
     } while (0)
 
 /* Get two 32 bit ints from a double.  */
 #define EXTRACT_WORDS(hi, lo, d) \
-    do                           \
-    {                            \
-        union                    \
-        {                        \
+    do {                         \
+        union {                  \
             double f;            \
             uint64_t i;          \
         } __u;                   \
@@ -107,10 +96,8 @@ union ldshape
 
 /* Get the more significant 32 bit int from a double.  */
 #define GET_HIGH_WORD(hi, d) \
-    do                       \
-    {                        \
-        union                \
-        {                    \
+    do {                     \
+        union {              \
             double f;        \
             uint64_t i;      \
         } __u;               \
@@ -120,10 +107,8 @@ union ldshape
 
 /* Get the less significant 32 bit int from a double.  */
 #define GET_LOW_WORD(lo, d)     \
-    do                          \
-    {                           \
-        union                   \
-        {                       \
+    do {                        \
+        union {                 \
             double f;           \
             uint64_t i;         \
         } __u;                  \
@@ -133,10 +118,8 @@ union ldshape
 
 /* Set a double from two 32 bit ints.  */
 #define INSERT_WORDS(d, hi, lo)                          \
-    do                                                   \
-    {                                                    \
-        union                                            \
-        {                                                \
+    do {                                                 \
+        union {                                          \
             double f;                                    \
             uint64_t i;                                  \
         } __u;                                           \
@@ -146,10 +129,8 @@ union ldshape
 
 /* Set the more significant 32 bits of a double from an int.  */
 #define SET_HIGH_WORD(d, hi)           \
-    do                                 \
-    {                                  \
-        union                          \
-        {                              \
+    do {                               \
+        union {                        \
             double f;                  \
             uint64_t i;                \
         } __u;                         \
@@ -161,10 +142,8 @@ union ldshape
 
 /* Set the less significant 32 bits of a double from an int.  */
 #define SET_LOW_WORD(d, lo)             \
-    do                                  \
-    {                                   \
-        union                           \
-        {                               \
+    do {                                \
+        union {                         \
             double f;                   \
             uint64_t i;                 \
         } __u;                          \
@@ -176,10 +155,8 @@ union ldshape
 
 /* Get a 32 bit int from a float.  */
 #define GET_FLOAT_WORD(w, d) \
-    do                       \
-    {                        \
-        union                \
-        {                    \
+    do {                     \
+        union {              \
             float f;         \
             uint32_t i;      \
         } __u;               \
@@ -189,10 +166,8 @@ union ldshape
 
 /* Set a float from a 32 bit int.  */
 #define SET_FLOAT_WORD(d, w) \
-    do                       \
-    {                        \
-        union                \
-        {                    \
+    do {                     \
+        union {              \
             float f;         \
             uint32_t i;      \
         } __u;               \
@@ -242,5 +217,9 @@ long double __tanl(long double, long double, int);
 /* polynomial evaluation */
 long double __polevll(long double, long double const *, int);
 long double __p1evll(long double, long double const *, int);
+
+int __signbit(double x);
+int __signbitf(float x);
+int __signbitl(long double x);
 
 #endif

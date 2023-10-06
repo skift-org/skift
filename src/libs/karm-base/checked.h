62ae40778e0b75d1c51811ad79ad31e8d93c17bb
VAN BOSSUYT Nicolas <nivb@odoo.com>
2023-10-06 17:55:09 +0200

    karm-math: Initial bigint implementation.

--- src/libs/karm-base/checked.h
+++ src/libs/karm-base/checked.h
@@ -7,47 +7,64 @@
 namespace Karm {
 
 template <typename T>
+inline constexpr usize BITS = sizeof(T) * CHAR_BIT;
+
+template <typename T>
 inline constexpr T MAX = Meta::Signed<T> ? 1 + ~(T(1) << (sizeof(T) * CHAR_BIT - 1)) : ~(T(0));
 
 template <typename T>
 inline constexpr T MIN = Meta::Signed<T> ? -(MAX<T> - 1) : 0;
 
 template <typename T>
-Res<T> checkedAdd(T op1, T op2) {
-    if (op1 > 0 and op2 > 0 and op1 > MAX<T> - op2) {
-        return Error::other("numeric overflow");
-    }
-    if (op1 < 0 and op2 < 0 and op1 < MIN<T> - op2) {
-        return Error::other("numeric underflow");
-    }
-    return {op1 + op2};
+bool willAddOverflow(T lhs, T rhs) {
+    return (lhs > 0 and rhs > 0) and (lhs > MAX<T> - rhs or MAX<T> - lhs < rhs);
 }
 
 template <typename T>
-Res<T> checkedSub(T op1, T op2) {
-    if (op1 > 0 and op2 < 0 and op1 > MAX<T> + op2) {
-        return Error::other("numeric overflow");
-    }
-    if (op1 < 0 and op2 > 0 and op1 < MIN<T> + op2) {
-        return Error::other("numeric underflow");
-    }
-    return {op1 - op2};
+bool willAddUnderflow(T lhs, T rhs) {
+    return (lhs < 0 and rhs < 0) and (lhs > MIN<T> - rhs or MIN<T> - lhs < rhs);
 }
 
 template <typename T>
-Res<T> checkedInc(T op) {
-    if (op == MAX<T>) {
+bool willSubOverflow(T lhs, T rhs) {
+    return (lhs > 0 and rhs < 0) and (lhs > MAX<T> - rhs);
+}
+
+template <typename T>
+bool willSubUnderflow(T lhs, T rhs) {
+    return (lhs < 0 and rhs > 0) and (lhs < MIN<T> - rhs);
+}
+
+template <typename T>
+Res<T> checkedAdd(T lhs, T rhs) {
+    if (willAddOverflow(lhs, rhs))
         return Error::other("numeric overflow");
-    }
-    return {op + 1};
+    if (willAddUnderflow(lhs, rhs))
+        return Error::other("numeric underflow");
+    return Ok(lhs + rhs);
+}
+
+template <typename T>
+Res<T> checkedSub(T lhs, T rhs) {
+    if (willSubOverflow(lhs, rhs))
+        return Error::other("numeric overflow");
+    if (willSubUnderflow(lhs, rhs))
+        return Error::other("numeric underflow");
+    return Ok(lhs - rhs);
+}
+
+template <typename T>
+Res<T> checkedInc(T val) {
+    if (willAddOverflow(val, 1))
+        return Error::other("numeric overflow");
+    return Ok(val + 1);
 }
 
 template <typename T>
 Res<T> checkedDec(T op) {
-    if (op == MIN<T>) {
+    if (op == MIN<T>)
         return Error::other("numeric underflow");
-    }
-    return {op - 1};
+    return Ok(op - 1);
 }
 
 } // namespace Karm

269b64c21c0e4e06115e407fe6f93c75cc53d53f
VAN BOSSUYT Nicolas <nivb@odoo.com>
2023-12-27 18:58:18 +0100

    loader: Use FileReader instead of File.

--- src/kernel/loader/loader.cpp
+++ src/kernel/loader/loader.cpp
@@ -25,7 +25,7 @@ Res<> loadEntry(Entry const &entry) {
     payload.add(Handover::SELF, 0, payloadMem.prange());
 
     logInfo("loader: loading kernel file...");
-    Sys::File kernelFile = try$(Sys::File::open(entry.kernel.url));
+    auto kernelFile = try$(Sys::File::open(entry.kernel.url));
     auto kernelMem = try$(Sys::mmap().map(kernelFile));
     Elf::Image image{kernelMem.bytes()};
     payload.add(Handover::FILE, 0, kernelMem.prange().as<USizeRange>());

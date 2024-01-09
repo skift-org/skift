cfd3881dac2dce3df06555c8a16705a4d5041047
VAN BOSSUYT Nicolas <nivb@odoo.com>
2024-01-09 11:40:07 +0100

    meta: Install liburing-dev on ubuntu.

--- meta/scripts/setup-ubuntu.sh
+++ meta/scripts/setup-ubuntu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 apt update
-apt install build-essential git ninja-build libsdl2-dev nasm gcc-multilib qemu-system-x86 mtools
+apt install build-essential git ninja-build libsdl2-dev nasm gcc-multilib qemu-system-x86 mtools liburing-dev
 bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)" llvm 17

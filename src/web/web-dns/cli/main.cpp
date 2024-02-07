0eb951409c0adb1a2e9c2b42c61ccd9021a5c806
VAN BOSSUYT Nicolas <nivb@odoo.com>
2024-02-07 14:58:42 +0100

    web-dns-cli: Allow passing the domain to resolve as a parameter.

--- src/web/web-dns/cli/main.cpp
+++ src/web/web-dns/cli/main.cpp
@@ -1,9 +1,14 @@
 #include <karm-sys/entry.h>
 #include <web-dns/dns.h>
 
-Res<> entryPoint(Sys::Ctx &) {
+Res<> entryPoint(Sys::Ctx &ctx) {
+    auto &args = Sys::useArgs(ctx);
+    if (args.len() != 1) {
+        Sys::println("usage: {} <domain>", args.self());
+        return Error::invalidInput("invalid number of arguments");
+    }
     auto dns = try$(Web::Dns::Client::connect(Web::Dns::GOOGLE));
-    auto addr = try$(dns.resolve("google.com"));
-    Sys::println("dns resolved domain 'google.com' to {}", addr);
+    auto addr = try$(dns.resolve(args[0]));
+    Sys::println("dns resolved domain '{}' to {}", args[0], addr);
     return Ok();
 }

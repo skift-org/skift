cc1e62aa7269dc7bc2da0a7433a0ee877a3d9371
VAN BOSSUYT Nicolas <nivb@odoo.com>
2024-02-20 07:51:22 +0100

    karm-base: Always inline atomics.

--- src/libs/karm-base/atomic.h
+++ src/libs/karm-base/atomic.h
@@ -21,60 +21,60 @@ template <typename T>
 struct Atomic {
     T _val{};
 
-    constexpr Atomic() = default;
+    always_inline constexpr Atomic() = default;
 
-    Atomic(T const &val)
+    always_inline Atomic(T const &val)
         : _val(val) {
     }
 
-    Atomic(T &&val)
+    always_inline Atomic(T &&val)
         : _val(std::move(val)) {
     }
 
-    T xchg(T desired, MemOrder order = MemOrder::SEQ_CST) {
+    always_inline T xchg(T desired, MemOrder order = MemOrder::SEQ_CST) {
         return __atomic_exchange_n(&_val, desired, order);
     }
 
-    bool cmpxchg(T expected, T desired, MemOrder order = MemOrder::SEQ_CST) {
+    always_inline bool cmpxchg(T expected, T desired, MemOrder order = MemOrder::SEQ_CST) {
         if (order == ACQ_REL or order == RELAXED)
             return __atomic_compare_exchange_n(&_val, &expected, desired, false, RELAXED, ACQUIRE);
 
         return __atomic_compare_exchange_n(&_val, &expected, desired, false, order, order);
     }
 
-    T fetchAdd(T desired, MemOrder order = MemOrder::SEQ_CST) {
+    always_inline T fetchAdd(T desired, MemOrder order = MemOrder::SEQ_CST) {
         return __atomic_fetch_add(&_val, desired, order);
     }
 
-    T fetchSub(T desired, MemOrder order = MemOrder::SEQ_CST) {
+    always_inline T fetchSub(T desired, MemOrder order = MemOrder::SEQ_CST) {
         return __atomic_fetch_sub(&_val, desired, order);
     }
 
-    T fetchInc(MemOrder order = MemOrder::SEQ_CST) {
+    always_inline T fetchInc(MemOrder order = MemOrder::SEQ_CST) {
         return __atomic_fetch_add(&_val, 1, order);
     }
 
-    void inc(MemOrder order = MemOrder::SEQ_CST) {
+    always_inline void inc(MemOrder order = MemOrder::SEQ_CST) {
         __atomic_add_fetch(&_val, 1, order);
     }
 
-    void dec(MemOrder order = MemOrder::SEQ_CST) {
+    always_inline void dec(MemOrder order = MemOrder::SEQ_CST) {
         __atomic_sub_fetch(&_val, 1, order);
     }
 
-    T fetchDec(MemOrder order = MemOrder::SEQ_CST) {
+    always_inline T fetchDec(MemOrder order = MemOrder::SEQ_CST) {
         return __atomic_fetch_sub(&_val, 1, order);
     }
 
-    T load(MemOrder order = MemOrder::SEQ_CST) {
+    always_inline T load(MemOrder order = MemOrder::SEQ_CST) {
         return __atomic_load_n(&_val, order);
     }
 
-    void store(T desired, MemOrder order = MemOrder::SEQ_CST) {
+    always_inline void store(T desired, MemOrder order = MemOrder::SEQ_CST) {
         __atomic_store_n(&_val, desired, order);
     }
 
-    bool lockFree() {
+    always_inline bool lockFree() {
         return __atomic_is_lock_free(&_val);
     }
 };

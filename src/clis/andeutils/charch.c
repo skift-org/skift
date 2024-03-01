23fb02e75c0d0bd4c29796e18eb07a31bef9b0c3
AnatoliyL2 <161717159+AnatoliyL2@users.noreply.github.com>
2024-03-01 20:55:09 +0300

    Create charch.c

---
+++ src/clis/andeutils/charch.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+/*
+* Copyright (C) 2024 AnatoliyL.
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the Andesaurus Free Software License version 1.1 as published by AnatoliyL.
+* See COPYING file for more details.
+*/
+int main(int argc, char **argv)
+{
+  for (int i = 0; i < strlen(argv[1]); i++)
+    {
+      if (argv[1][i] == argv[2][0])
+        argv[1][i] = argv[3][0];
+    }
+  printf("%s\n", argv[1]);
+  return (0);
+}

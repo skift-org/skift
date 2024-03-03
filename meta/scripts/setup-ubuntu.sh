b1186f1469d48f7283eb14a9f659e3c79ebcce4a
VAN BOSSUYT Nicolas <nivb@odoo.com>
2024-03-03 14:43:33 +0100

    meta: Properly fail when installation of deps fails on ubuntu.

--- meta/scripts/setup-ubuntu.sh
+++ meta/scripts/setup-ubuntu.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 apt update
 apt install build-essential git ninja-build libsdl2-dev nasm gcc-multilib qemu-system-x86 mtools liburing-dev
 bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)" llvm 17

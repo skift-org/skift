4f9a68bba5a2094e5ca824c97337b50c236e488f
VAN BOSSUYT Nicolas <nivb@odoo.com>
2024-03-22 15:32:02 +0100

    meta: Do not let the user run ./skift.sh as root

--- meta/scripts/setup-ubuntu.sh
+++ meta/scripts/setup-ubuntu.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+export DEBIAN_FRONTEND=noninteractive
+
 apt update
 apt install build-essential git ninja-build libsdl2-dev nasm gcc-multilib qemu-system-x86 mtools liburing-dev
 bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)" llvm 17
--- skift.sh
+++ skift.sh
@@ -10,6 +10,18 @@ function is_ubuntu() {
     return 1
 }
 
+if [ "$EUID" -eq 0 ]; then
+    echo "Please do not run this script as root."
+
+    if [ "$CUTEKIT_ALLOW_ROOT" == "1" ]; then
+        echo "CUTEKIT_ALLOW_ROOT is set, continuing..."
+    else
+        echo "If you know what you are doing, set CUTEKIT_ALLOW_ROOT=1 and try again."
+        echo "Aborting."
+        exit 1
+    fi
+fi
+
 if [ "$CUTEKIT_NOVENV" == "1" ]; then
     echo "CUTEKIT_NOVENV is set, skipping virtual environment setup."
     exec cutekit $@

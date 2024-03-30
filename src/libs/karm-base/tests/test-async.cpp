bdbd9ba7373373bdaf879469a6684a088a0f7cf7
VAN BOSSUYT Nicolas <nivb@odoo.com>
2024-03-30 17:13:55 +0100

    karm-base: Bring back async tests.
    They go lost for some reasons.

--- src/libs/karm-base/tests/test-async.cpp
+++ src/libs/karm-base/tests/test-async.cpp
@@ -3,8 +3,74 @@
 
 namespace Karm::Base::Tests {
 
+test$(testSenderOne) {
+    auto sender = Async::One<int>{10};
+    auto res = Async::run(sender);
+    expectEq$(res, 10);
+    return Ok();
+}
+
 Async::_Task<int> taskValue() {
     co_return 42;
 }
 
+test$(testTaskValue) {
+    auto res = Async::run(taskValue());
+    expectEq$(res, 42);
+    return Ok();
+}
+
+Async::_Task<int> taskOuter() {
+    co_return co_await taskValue();
+}
+
+test$(testTaskOuter) {
+    auto res = Async::run(taskOuter());
+    expectEq$(res, 42);
+    return Ok();
+}
+
+test$(testTaskDetach) {
+    int res = 0xdead;
+    Async::detach(taskValue(), [&](int r) {
+        res = r;
+    });
+    expectEq$(res, 42);
+    return Ok();
+}
+
+test$(testPromiseOneFuture) {
+    Opt<Async::_Future<int>> future;
+    {
+        Async::_Promise<int> promise;
+        future = promise.future();
+        promise.resolve(42);
+    }
+    auto res = Async::run(*future);
+    expectEq$(res, 42);
+    return Ok();
+}
+
+test$(testPromiseMultipleFutures) {
+    Opt<Async::_Future<int>> f1, f2, f3;
+    {
+        Async::_Promise<int> promise;
+
+        f1 = promise.future();
+        f2 = promise.future();
+        f3 = promise.future();
+
+        promise.resolve(42);
+    }
+
+    auto res1 = Async::run(*f1);
+    auto res2 = Async::run(*f2);
+    auto res3 = Async::run(*f3);
+
+    expectEq$(res1, 42);
+    expectEq$(res2, 42);
+    expectEq$(res3, 42);
+
+    return Ok();
+}
 } // namespace Karm::Base::Tests

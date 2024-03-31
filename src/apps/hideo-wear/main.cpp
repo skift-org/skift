c5355bf5768d6d0d785a315e2912a7fdbc5805ed
VAN BOSSUYT Nicolas <nivb@odoo.com>
2024-03-31 22:05:45 +0200

    hideo-wear: Simple layout.

--- src/apps/hideo-wear/main.cpp
+++ src/apps/hideo-wear/main.cpp
@@ -4,13 +4,34 @@
 namespace Hideo::Wear {
 
 Ui::Child app() {
-    return Ui::text("Hello, world!") |
-           Ui::center() |
-           Ui::minSize(192);
+    auto [date, time] = DateTime{};
+    auto dateTime = Io::format(
+        // Mon. 28 Jul
+        "{}. {} {}",
+        date.month.abbr(),
+        date.dayOfMonth() + 1,
+        date.year.val()
+    );
+
+    auto clock = Ui::vflow(
+        16,
+        Layout::Align::CENTER,
+        Ui::displayMedium("{02}:{02}", time.hour, time.minute),
+        Ui::titleMedium(dateTime.unwrap())
+    );
+
+    return Ui::stack(
+               Ui::image(Media::loadImageOrFallback("bundle://skift-wallpapers/images/abstract.qoi"_url).unwrap()) | Ui::cover(),
+               clock | Ui::center()
+           ) |
+           Ui::maxSize(192);
 }
 
 } // namespace Hideo::Wear
 
 Res<> entryPoint(Sys::Ctx &ctx) {
-    return Ui::runApp(ctx, Hideo::Wear::app());
+    return Ui::runApp(
+        ctx,
+        Hideo::Wear::app()
+    );
 }

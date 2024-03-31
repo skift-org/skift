cf8dfc5ad69bd34f3b1a6d793023458f2bd4fbb9
VAN BOSSUYT Nicolas <nivb@odoo.com>
2024-03-31 20:40:31 +0200

    karm-mime: Update requirements.

--- src/libs/karm-mime/mime.cpp
+++ src/libs/karm-mime/mime.cpp
@@ -27,38 +27,40 @@ Opt<Mime> sniffBytes(Bytes bytes) {
     return NONE;
 }
 
-struct {
+struct Suffix2Mime {
     Str suffix;
     Str mime;
-} SUFFIXES2MIME[] = {
-    {"html", "text/html; charset=UTF-8"},
-    {"css", "text/css; charset=UTF-8"},
-    {"js", "application/javascript; charset=UTF-8"},
-    {"pdf", "application/pdf"},
-    {"png", "image/png"},
-    {"jpg", "image/jpeg"},
-    {"jpeg", "image/jpeg"},
-    {"ico", "image/x-icon"},
-    {"svg", "image/svg+xml"},
-    {"woff", "font/woff"},
-    {"woff2", "font/woff2"},
-    {"ttf", "font/ttf"},
-    {"eot", "application/vnd.ms-fontobject"},
-    {"otf", "font/otf"},
-    {"json", "application/json"},
-    {"txt", "text/plain; charset=UTF-8"},
-    {"mov", "video/quicktime"},
-    {"mp4", "video/mp4"},
-    {"webm", "video/webm"},
-    {"exe", "application/x-msdownload"},
-    {"zip", "application/zip"},
-    {"tar", "application/x-tar"},
-    {"gz", "application/gzip"},
-    {"bz2", "application/x-bzip2"},
-    {"7z", "application/x-7z-compressed"},
-    {"rar", "application/vnd.rar"},
-    {"xz", "application/x-xz"},
-    {"webp", "image/webp"},
+};
+
+static constexpr Array SUFFIXES2MIME = {
+    Suffix2Mime{"html", "text/html; charset=UTF-8"},
+    Suffix2Mime{"css", "text/css; charset=UTF-8"},
+    Suffix2Mime{"js", "application/javascript; charset=UTF-8"},
+    Suffix2Mime{"pdf", "application/pdf"},
+    Suffix2Mime{"png", "image/png"},
+    Suffix2Mime{"jpg", "image/jpeg"},
+    Suffix2Mime{"jpeg", "image/jpeg"},
+    Suffix2Mime{"ico", "image/x-icon"},
+    Suffix2Mime{"svg", "image/svg+xml"},
+    Suffix2Mime{"woff", "font/woff"},
+    Suffix2Mime{"woff2", "font/woff2"},
+    Suffix2Mime{"ttf", "font/ttf"},
+    Suffix2Mime{"eot", "application/vnd.ms-fontobject"},
+    Suffix2Mime{"otf", "font/otf"},
+    Suffix2Mime{"json", "application/json"},
+    Suffix2Mime{"txt", "text/plain; charset=UTF-8"},
+    Suffix2Mime{"mov", "video/quicktime"},
+    Suffix2Mime{"mp4", "video/mp4"},
+    Suffix2Mime{"webm", "video/webm"},
+    Suffix2Mime{"exe", "application/x-msdownload"},
+    Suffix2Mime{"zip", "application/zip"},
+    Suffix2Mime{"tar", "application/x-tar"},
+    Suffix2Mime{"gz", "application/gzip"},
+    Suffix2Mime{"bz2", "application/x-bzip2"},
+    Suffix2Mime{"7z", "application/x-7z-compressed"},
+    Suffix2Mime{"rar", "application/vnd.rar"},
+    Suffix2Mime{"xz", "application/x-xz"},
+    Suffix2Mime{"webp", "image/webp"},
 };
 
 Opt<Mime> sniffSuffix(Str suffix) {
@@ -69,35 +71,37 @@ Opt<Mime> sniffSuffix(Str suffix) {
     return NONE;
 }
 
-struct {
+struct Mime2Icon {
     Str type;
     Str subtype;
     Mdi::Icon icon;
-} MIME2ICON[] = {
-    {"text", "", Mdi::FILE_DOCUMENT},
-    {"text", "html", Mdi::LANGUAGE_HTML5},
-    {"text", "css", Mdi::LANGUAGE_CSS3},
-    {"text", "javascript", Mdi::LANGUAGE_JAVASCRIPT},
-    {"text", "plain", Mdi::FILE_DOCUMENT},
+};
 
-    {"image", "", Mdi::IMAGE},
-    {"image", "jpeg", Mdi::FILE_JPG_BOX},
-    {"image", "png", Mdi::FILE_PNG_BOX},
+static constexpr Array MIME2ICON = {
+    Mime2Icon{"text", "", Mdi::FILE_DOCUMENT},
+    Mime2Icon{"text", "html", Mdi::LANGUAGE_HTML5},
+    Mime2Icon{"text", "css", Mdi::LANGUAGE_CSS3},
+    Mime2Icon{"text", "javascript", Mdi::LANGUAGE_JAVASCRIPT},
+    Mime2Icon{"text", "plain", Mdi::FILE_DOCUMENT},
 
-    {"video", "", Mdi::FILMSTRIP},
+    Mime2Icon{"image", "", Mdi::IMAGE},
+    Mime2Icon{"image", "jpeg", Mdi::FILE_JPG_BOX},
+    Mime2Icon{"image", "png", Mdi::FILE_PNG_BOX},
 
-    {"font", "", Mdi::FORMAT_FONT},
+    Mime2Icon{"video", "", Mdi::FILMSTRIP},
 
-    {"application", "pdf", Mdi::FILE_PDF_BOX},
-    {"application", "json", Mdi::CODE_JSON},
-    {"application", "zip", Mdi::ZIP_BOX},
-    {"application", "tar", Mdi::ZIP_BOX},
-    {"application", "gz", Mdi::ZIP_BOX},
-    {"application", "bz2", Mdi::ZIP_BOX},
-    {"application", "7z-compressed", Mdi::ZIP_BOX},
-    {"application", "rar", Mdi::ZIP_BOX},
-    {"application", "x-xz", Mdi::ZIP_BOX},
-    {"application", "x-msdownload", Mdi::COG_BOX},
+    Mime2Icon{"font", "", Mdi::FORMAT_FONT},
+
+    Mime2Icon{"application", "pdf", Mdi::FILE_PDF_BOX},
+    Mime2Icon{"application", "json", Mdi::CODE_JSON},
+    Mime2Icon{"application", "zip", Mdi::ZIP_BOX},
+    Mime2Icon{"application", "tar", Mdi::ZIP_BOX},
+    Mime2Icon{"application", "gz", Mdi::ZIP_BOX},
+    Mime2Icon{"application", "bz2", Mdi::ZIP_BOX},
+    Mime2Icon{"application", "7z-compressed", Mdi::ZIP_BOX},
+    Mime2Icon{"application", "rar", Mdi::ZIP_BOX},
+    Mime2Icon{"application", "x-xz", Mdi::ZIP_BOX},
+    Mime2Icon{"application", "x-msdownload", Mdi::COG_BOX},
 };
 
 Mdi::Icon iconFor(Mime const &mime) {

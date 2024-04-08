a2de4fb478ccb4a4d9149eb80cbde216050621b9
VAN BOSSUYT Nicolas <nivb@odoo.com>
2024-04-08 15:45:59 +0200

    karm-io: Marked most Rexpr as constexpr.

--- src/libs/karm-io/expr.h
+++ src/libs/karm-io/expr.h
@@ -32,20 +32,20 @@ Match match(Re::Expr auto expr, _Str<E> input) {
 /* --- Combinators ---------------------------------------------------------- */
 
 /// Returns true if either of the expressions match pass as parameters
-inline Expr auto either(Expr auto... exprs) {
+inline constexpr Expr auto either(Expr auto... exprs) {
     return [=](auto &scan) {
         return (exprs(scan) or ...);
     };
 }
 
-inline Expr auto operator|(Expr auto a, Expr auto b) {
+inline constexpr Expr auto operator|(Expr auto a, Expr auto b) {
     return either(a, b);
 }
 
 /// Returns true if all of the expressions match.
 /// The expressions are evaluated in order.
 /// If any expression fails, the scanner is rewound to the state before the first expression.
-inline Expr auto chain(Expr auto... exprs) {
+inline constexpr Expr auto chain(Expr auto... exprs) {
     return [=](auto &scan) {
         auto saved = scan;
         if ((exprs(scan) and ...))
@@ -55,12 +55,12 @@ inline Expr auto chain(Expr auto... exprs) {
     };
 }
 
-inline Expr auto operator&(Expr auto a, Expr auto b) {
+inline constexpr Expr auto operator&(Expr auto a, Expr auto b) {
     return chain(a, b);
 }
 
 /// Inverts the result of the expression.
-inline Expr auto negate(Expr auto expr) {
+inline constexpr Expr auto negate(Expr auto expr) {
     return [=](auto &scan) {
         auto saved = scan;
         if (not expr(scan)) {
@@ -74,13 +74,13 @@ inline Expr auto negate(Expr auto expr) {
     };
 }
 
-inline Expr auto operator~(Expr auto expr) {
+inline constexpr Expr auto operator~(Expr auto expr) {
     return negate(expr);
 }
 
 /// Consumes until the expression matches or the end of the input is reached.
 /// scanner is rewound to the last unmatched rune.
-inline Expr auto until(Expr auto expr) {
+inline constexpr Expr auto until(Expr auto expr) {
     return [=](auto &scan) {
         auto saved = scan;
         while (not expr(scan) and not scan.ended()) {
@@ -93,7 +93,7 @@ inline Expr auto until(Expr auto expr) {
 }
 
 /// Consumes until the expression matches or the end of the input is reached.
-inline Expr auto untilAndConsume(Expr auto expr) {
+inline constexpr Expr auto untilAndConsume(Expr auto expr) {
     return [=](auto &scan) {
         while (not expr(scan) and not scan.ended())
             scan.next();
@@ -105,7 +105,7 @@ inline Expr auto untilAndConsume(Expr auto expr) {
 
 /// Try to match an expression `atLeast` times and and stops when `atMost` times
 /// is reached, return true if atLeast is reached otherwise rewind the scanner.
-inline Expr auto nOrN(usize atLeast, usize atMost, Expr auto expr) {
+inline constexpr Expr auto nOrN(usize atLeast, usize atMost, Expr auto expr) {
     return [=](auto &scan) {
         usize count = 0;
         auto saved = scan;
@@ -125,38 +125,38 @@ inline Expr auto nOrN(usize atLeast, usize atMost, Expr auto expr) {
 }
 
 /// Returns true if the expression matches exactly n times.
-inline Expr auto exactly(usize n, Expr auto expr) {
+inline constexpr Expr auto exactly(usize n, Expr auto expr) {
     return nOrN(n, n, expr);
 }
 
 /// Returns true if the expression matches at least n times.
-inline Expr auto atLeast(usize n, Expr auto expr) {
+inline constexpr Expr auto atLeast(usize n, Expr auto expr) {
     return nOrN(n, MAX<usize>, expr);
 }
 
 /// Returns true if the expression matches at most n times.
-inline Expr auto atMost(usize n, Expr auto expr) {
+inline constexpr Expr auto atMost(usize n, Expr auto expr) {
     return nOrN(0, n, expr);
 }
 
 /// Returns true if the expression matches zero or more times.
-inline Expr auto zeroOrMore(Expr auto expr) {
+inline constexpr Expr auto zeroOrMore(Expr auto expr) {
     return nOrN(0, MAX<usize>, expr);
 }
 
 /// Returns true if the expression matches one or more times.
-inline Expr auto oneOrMore(Expr auto expr) {
+inline constexpr Expr auto oneOrMore(Expr auto expr) {
     return nOrN(1, MAX<usize>, expr);
 }
 
 /// Returns true if the expression matches zero or one times.
-inline Expr auto zeroOrOne(Expr auto expr) {
+inline constexpr Expr auto zeroOrOne(Expr auto expr) {
     return nOrN(0, 1, expr);
 }
 
 // Returns true if the expression matches at most n times.
 // If the expression matches more than n times, the scanner is rewound to the start of the first match.
-inline Expr auto atMost(usize n, auto expr) {
+inline constexpr Expr auto atMost(usize n, auto expr) {
     return nOrN(0, n, expr);
 }
 
@@ -189,13 +189,13 @@ inline Expr auto trap(Expr auto expr, auto cb) {
 /* --- Tokens --------------------------------------------------------------- */
 
 /// Match nothing and return true.
-inline Expr auto nothing() {
+inline constexpr Expr auto nothing() {
     return [](auto &) {
         return true;
     };
 }
 
-inline Expr auto any() {
+inline constexpr Expr auto any() {
     return [](auto &scan) {
         scan.next();
         return true;
@@ -203,14 +203,14 @@ inline Expr auto any() {
 }
 
 /// Match the end of the input.
-inline Expr auto eof() {
+inline constexpr Expr auto eof() {
     return [](auto &scan) {
         return scan.ended();
     };
 }
 
 /// Match a word and consume it.
-inline Expr auto word(Str word) {
+inline constexpr Expr auto word(Str word) {
     return [=](auto &scan) {
         return scan.skip(word);
     };
@@ -218,7 +218,7 @@ inline Expr auto word(Str word) {
 
 /// Match a single character and consume it.
 /// Multiple characters can be passed to match any of them.
-inline Expr auto single(auto... c) {
+inline constexpr Expr auto single(auto... c) {
     return [=](auto &scan) {
         if (((scan.curr() == (Rune)c) or ...)) {
             scan.next();
@@ -229,7 +229,7 @@ inline Expr auto single(auto... c) {
 }
 
 /// Match a single character against a ctype function and consume it.
-inline Expr auto ctype(CType auto ctype) {
+inline constexpr Expr auto ctype(CType auto ctype) {
     return [=](auto &scan) {
         if (ctype(scan.curr())) {
             scan.next();
@@ -240,7 +240,7 @@ inline Expr auto ctype(CType auto ctype) {
 }
 
 /// Match a character range and consume it if it lies within the range.
-inline Expr auto range(Rune start, Rune end) {
+inline constexpr Expr auto range(Rune start, Rune end) {
     return [=](auto &scan) {
         if (scan.curr() >= start and scan.curr() <= end) {
             scan.next();
@@ -253,47 +253,47 @@ inline Expr auto range(Rune start, Rune end) {
 /* --- Posix Classes -------------------------------------------------------- */
 
 /// Match an ASCII Character and consume it.
-inline Expr auto ascii() {
+inline constexpr Expr auto ascii() {
     return ctype(isAscii);
 }
 
 /// Match an ASCII upper case letter and consume it.
-inline Expr auto upper() {
+inline constexpr Expr auto upper() {
     return ctype(isAsciiUpper);
 }
 
 /// Match an ASCII lower case letter and consume it.
-inline Expr auto lower() {
+inline constexpr Expr auto lower() {
     return ctype(isAsciiLower);
 }
 
 /// Match an ASCII letter and consume it.
-inline Expr auto alpha() {
+inline constexpr Expr auto alpha() {
     return upper() | lower();
 }
 
 /// Match an ASCII digit and consume it.
-inline Expr auto digit() {
+inline constexpr Expr auto digit() {
     return ctype(isAsciiDecDigit);
 }
 
 /// Match an ASCII hexadecimal digit and consume it.
-inline Expr auto xdigit() {
+inline constexpr Expr auto xdigit() {
     return ctype(isAsciiHexDigit);
 }
 
 /// Match an ASCII alphanumeric character and consume it.
-inline Expr auto alnum() {
+inline constexpr Expr auto alnum() {
     return ctype(isAsciiAlphaNum);
 }
 
 /// Match a work made of ASCII letters and underscores and consume it.
-inline Expr auto word() {
+inline constexpr Expr auto word() {
     return alnum() | single('_');
 }
 
 /// Match punctuation and consume it.
-inline Expr auto punct() {
+inline constexpr Expr auto punct() {
     return single(
         '!', '"', '#', '$', '%', '&', '\'', '(',
         ')', '*', '+', ',', '-', '.', '/', ':',
@@ -303,12 +303,12 @@ inline Expr auto punct() {
 }
 
 /// Match ascii whitespace and consume it.
-inline Expr auto space() {
+inline constexpr Expr auto space() {
     return single(' ', '\t', '\n', '\r');
 }
 
 /// Match a blank space and consume it.
-inline Expr auto blank() {
+inline constexpr Expr auto blank() {
     return single(' ', '\t');
 }
 
@@ -316,7 +316,7 @@ inline Expr auto blank() {
 
 /// Match a separator and consume it.
 /// A separator is a expr surrounded by spaces.
-inline Expr auto separator(Expr auto expr) {
+inline constexpr Expr auto separator(Expr auto expr) {
     return zeroOrMore(space()) &
            expr &
            zeroOrMore(space());
@@ -326,7 +326,7 @@ inline Expr auto separator(Expr auto expr) {
 /// A separator is an expression surrounded by spaces.
 /// If the separator is not found, the expression still matches.
 /// And whitespaces are consumed.
-inline Expr auto optSeparator(Expr auto expr) {
+inline constexpr Expr auto optSeparator(Expr auto expr) {
     return zeroOrMore(space()) &
            zeroOrOne(expr) &
            zeroOrMore(space());
@@ -334,10 +334,10 @@ inline Expr auto optSeparator(Expr auto expr) {
 
 } // namespace Karm::Re
 
-inline Karm::Re::Expr auto operator""_re(char const *str, usize len) {
+inline constexpr Karm::Re::Expr auto operator""_re(char const *str, usize len) {
     return Karm::Re::word(Str{str, len});
 }
 
-inline Karm::Re::Expr auto operator""_re(char c) {
+inline constexpr Karm::Re::Expr auto operator""_re(char c) {
     return Karm::Re::single(c);
 }

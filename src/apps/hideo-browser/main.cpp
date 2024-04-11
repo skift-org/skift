62c9ba4b725afabf03af452c27bcb70b5e9a1513
VAN BOSSUYT Nicolas <nivb@odoo.com>
2024-04-11 15:08:57 +0200

    hideo-browser: Implemented html/xhtml loading.

--- src/apps/hideo-browser/main.cpp
+++ src/apps/hideo-browser/main.cpp
@@ -1,7 +1,11 @@
 #include <hideo-base/scafold.h>
+#include <karm-mime/mime.h>
 #include <karm-sys/entry.h>
+#include <karm-sys/file.h>
 #include <karm-ui/app.h>
+#include <web-html/parser.h>
 #include <web-view/view.h>
+#include <web-xml/parser.h>
 
 namespace Hideo::Browser {
 
@@ -23,5 +27,32 @@ Ui::Child app() {
 } // namespace Hideo::Browser
 
 Res<> entryPoint(Sys::Ctx &ctx) {
+    auto args = Sys::useArgs(ctx);
+    auto url = args.len()
+                   ? try$(Mime::parseUrlOrPath(args[0]))
+                   : "bundle://hideo-browser/start-page.html"_url;
+
+    auto mime = Mime::sniffSuffix(url.path.suffix());
+
+    if (not mime.has())
+        return Error::invalidInput("cannot determine MIME type");
+
+    logInfo("loading {}...", url);
+    auto dom = makeStrong<Web::Dom::Document>();
+    auto file = try$(Sys::File::open(url));
+    auto buf = try$(Io::readAllUtf8(file));
+
+    if (mime->is("text/html"_mime)) {
+        Web::Html::Parser parser{dom};
+        parser.write(buf);
+    } else if (mime->is("text/xml"_mime)) {
+        Io::SScan scan{buf};
+        Web::Xml::Parser parser;
+        dom = try$(parser.parse(scan, Web::HTML));
+    } else {
+        logError("unsupported MIME type: {}", mime);
+        return Error::invalidInput("unsupported MIME type");
+    }
+
     return Ui::runApp(ctx, Hideo::Browser::app());
 }

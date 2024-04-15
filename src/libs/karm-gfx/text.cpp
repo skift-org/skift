6f0e9e7cc132a08f39948f8c1a100874b7604484
VAN BOSSUYT Nicolas <nivb@odoo.com>
2024-04-15 15:02:00 +0200

    karm-gfx: Split Text between an header and source file.

---
+++ src/libs/karm-gfx/text.cpp
@@ -0,0 +1,216 @@
+#include "text.h"
+
+namespace Karm::Gfx {
+
+Text::Text(TextStyle style, Str str) : _style(style) {
+    clear();
+    _spaceWidth = _style.font.advance(_style.font.glyph(' '));
+    auto m = _style.font.metrics();
+    _lineHeight = m.ascend;
+    append(str);
+}
+
+// MARK: Text --------------------------------------------------------------
+
+void Text::_beginBlock() {
+    _blocks.pushBack({
+        .cells = {_cells.len(), 0},
+    });
+}
+
+void Text::_appendRune(Rune rune) {
+    if (last(_blocks).newline)
+        _beginBlock();
+
+    if (rune == ' ') {
+        last(_blocks).spaces++;
+    } else if (rune == '\t') {
+        // HACK: tab width is 4 spaces
+        last(_blocks).spaces += 4;
+    } else if (rune == '\n') {
+        last(_blocks).newline = true;
+    } else if (not isAsciiPrint(rune)) {
+        // ignore
+    } else {
+        if (last(_blocks).hasWhitespace())
+            _beginBlock();
+
+        auto glyph = _style.font.glyph(rune);
+        _cells.pushBack({
+            .glyph = glyph,
+        });
+        last(_blocks).cells.size++;
+    }
+}
+
+void Text::clear() {
+    _cells.clear();
+    _blocks.clear();
+    _blocksMeasured = false;
+    _beginBlock();
+    _lines.clear();
+}
+
+void Text::append(Slice<Rune> runes) {
+    for (auto rune : runes) {
+        _appendRune(rune);
+    }
+}
+
+// MARK: Layout -------------------------------------------------------------
+
+void Text::_measureBlocks() {
+    for (auto &block : _blocks) {
+        auto adv = 0.0f;
+        bool first = true;
+        Media::Glyph prev{0};
+        for (usize i = block.cells.start; i < block.cells.end(); i++) {
+            auto &cell = _cells[i];
+            if (not first)
+                adv += _style.font.kern(prev, cell.glyph);
+            else
+                first = false;
+
+            cell.pos = adv;
+            adv += _style.font.advance(cell.glyph);
+            prev = cell.glyph;
+        }
+        block.width = adv;
+    }
+}
+
+void Text::_wrapLines(f64 width) {
+    _lines.clear();
+
+    Line line{{0, 0}};
+    bool first = true;
+    f64 adv = 0;
+    for (usize i = 0; i < _blocks.len(); i++) {
+        auto &block = _blocks[i];
+        auto fullWidth = block.fullWidth(_style);
+        if (adv + block.width > width and _style.wordwrap and _style.multiline and not first) {
+            _lines.pushBack(line);
+            line = {{i, 1}};
+            adv = fullWidth;
+
+            if (block.newline) {
+                _lines.pushBack(line);
+                line = {{i + 1, 0}};
+                adv = 0;
+            }
+        } else {
+            line.blocks.size++;
+
+            if (block.newline and _style.multiline) {
+                _lines.pushBack(line);
+                line = {{i + 1, 0}};
+                adv = 0;
+            } else {
+                adv += fullWidth;
+            }
+        }
+        first = false;
+    }
+
+    _lines.pushBack(line);
+}
+
+f64 Text::_layoutVerticaly() {
+    auto m = _style.font.metrics();
+    f64 baseline = m.linegap / 2;
+    for (auto &line : _lines) {
+        baseline += m.ascend;
+        line.baseline = baseline;
+        baseline += m.linegap + m.descend;
+    }
+    return baseline - m.linegap / 2;
+}
+
+f64 Text::_layoutHorizontaly(f64 width) {
+    f64 maxWidth = 0;
+    for (auto &line : _lines) {
+        if (not line.blocks.any())
+            continue;
+
+        f64 pos = 0;
+        for (usize i = line.blocks.start; i < line.blocks.end(); i++) {
+            auto &block = _blocks[i];
+            block.pos = pos;
+            pos += block.fullWidth(*this);
+        }
+
+        auto lastBlock = _blocks[line.blocks.end() - 1];
+        line.width = lastBlock.pos + lastBlock.width;
+        maxWidth = max(maxWidth, line.width);
+        auto free = width - line.width;
+
+        switch (_style.align) {
+        case TextAlign::LEFT:
+            break;
+
+        case TextAlign::CENTER:
+            for (usize i = line.blocks.start; i < line.blocks.end(); i++) {
+                auto &block = _blocks[i];
+                block.pos += free / 2;
+            }
+            break;
+
+        case TextAlign::RIGHT:
+            for (usize i = line.blocks.start; i < line.blocks.end(); i++) {
+                auto &block = _blocks[i];
+                block.pos += free;
+            }
+            break;
+        }
+    }
+
+    return maxWidth;
+}
+
+Math::Vec2f Text::layout(f64 width) {
+    if (isEmpty(_blocks))
+        return {};
+
+    // Blocks measurements can be reused between layouts changes
+    // only line wrapping need to be re-done
+    if (not _blocksMeasured) {
+        _measureBlocks();
+        _blocksMeasured = true;
+    }
+
+    _wrapLines(width);
+    auto textHeight = _layoutVerticaly();
+    auto textWidth = _layoutHorizontaly(width);
+    return {textWidth, textHeight};
+}
+
+// MARK: Paint -------------------------------------------------------------
+
+void Text::paint(Context &ctx) const {
+    auto m = _style.font.metrics();
+    auto baseline = m.ascend + m.linegap / 2;
+
+    ctx.save();
+    ctx.textFont(_style.font);
+    if (_style.color)
+        ctx.fillStyle(*_style.color);
+
+    for (auto const &line : _lines) {
+        for (usize b = line.blocks.start; b < line.blocks.end(); b++) {
+            auto const &block = _blocks[b];
+
+            for (usize c = block.cells.start; c < block.cells.end(); c++) {
+                auto const &cell = _cells[c];
+                ctx.fill({block.pos + cell.pos, baseline}, cell.glyph);
+            }
+        }
+
+        baseline += m.linegap + m.descend + m.ascend;
+    }
+
+    ctx.restore();
+}
+
+// MARK: Query -------------------------------------------------------------
+
+} // namespace Karm::Gfx
--- src/libs/karm-gfx/text.h
+++ src/libs/karm-gfx/text.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <karm-logger/logger.h>
 #include <karm-media/font.h>
 
 #include "context.h"
@@ -57,18 +58,14 @@ struct TextStyle {
 };
 
 struct Text {
-    TextStyle _style;
-
     struct Cell {
         Media::Glyph glyph;
-        f64 pos = 0; // Position of the glyph within the block
+        f64 pos = 0; //< Position of the glyph within the block
     };
-
-    Vec<Cell> _cells;
 
     struct Block {
         USizeRange cells;
-        size_t spaces = 0;
+        usize spaces = 0;
         bool newline = false;
 
         f64 pos = 0; // Position of the block within the line
@@ -87,213 +84,55 @@ struct Text {
         }
     };
 
-    Vec<Block> _blocks;
-    bool _blocksMeasured = false;
-
     struct Line {
         USizeRange blocks;
         f64 baseline = 0; // Baseline of the line within the text
         f64 width = 0;
     };
 
+    TextStyle _style;
+    Vec<Cell> _cells;
+    Vec<Block> _blocks;
     Vec<Line> _lines;
 
+    // Various cached values
+    bool _blocksMeasured = false;
     f64 _spaceWidth{};
+    f64 _lineHeight{};
 
-    Text(TextStyle style, Str str = "") : _style(style) {
-        clear();
-        _spaceWidth = _style.font.advance(_style.font.glyph(' '));
-        append(str);
-    }
+    Text(TextStyle style, Str str = "");
 
-    void _beginBlock() {
-        _blocks.pushBack({{_cells.len(), 0}});
-    }
+    // MARK: Text --------------------------------------------------------------
 
-    void _appendRune(Rune rune) {
-        if (last(_blocks).newline)
-            _beginBlock();
+    void _beginBlock();
 
-        if (rune == ' ') {
-            last(_blocks).spaces++;
-        } else if (rune == '\t') {
-            // HACK: tab width is 4 spaces
-            last(_blocks).spaces += 4;
-        } else if (rune == '\n') {
-            last(_blocks).newline = true;
-        } else if (not isAsciiPrint(rune)) {
-            // ignore
-        } else {
-            if (last(_blocks).hasWhitespace())
-                _beginBlock();
+    void _appendRune(Rune rune);
 
-            auto glyph = _style.font.glyph(rune);
-            _cells.pushBack({glyph});
-            last(_blocks).cells.size++;
-        }
-    }
-
-    void clear() {
-        _cells.clear();
-        _blocks.clear();
-        _blocksMeasured = false;
-        _blocks.pushBack({{0, 0}});
-        _lines.clear();
-    }
+    void clear();
 
     template <typename E>
     void append(_Str<E> str) {
-        for (auto rune : iterRunes(str)) {
+        for (auto rune : iterRunes(str))
             _appendRune(rune);
-        }
     }
 
-    void _measureBlocks() {
-        for (auto &block : _blocks) {
-            auto adv = 0.0f;
-            bool first = true;
-            Media::Glyph prev{0};
-            for (usize i = block.cells.start; i < block.cells.end(); i++) {
-                auto &cell = _cells[i];
-                if (not first)
-                    adv += _style.font.kern(prev, cell.glyph);
-                else
-                    first = false;
+    void append(Slice<Rune> runes);
 
-                cell.pos = adv;
-                adv += _style.font.advance(cell.glyph);
-                prev = cell.glyph;
-            }
-            block.width = adv;
-        }
-    }
+    // MARK: Layout -------------------------------------------------------------
 
-    void _wrapLines(f64 width) {
-        _lines.clear();
+    void _measureBlocks();
 
-        Line line{{0, 0}};
-        bool first = true;
-        f64 adv = 0;
-        for (usize i = 0; i < _blocks.len(); i++) {
-            auto &block = _blocks[i];
-            auto fullWidth = block.fullWidth(_style);
-            if (adv + block.width > width and _style.wordwrap and _style.multiline and not first) {
-                _lines.pushBack(line);
-                line = {{i, 1}};
-                adv = fullWidth;
+    void _wrapLines(f64 width);
 
-                if (block.newline) {
-                    _lines.pushBack(line);
-                    line = {{i + 1, 0}};
-                    adv = 0;
-                }
-            } else {
-                line.blocks.size++;
+    f64 _layoutVerticaly();
 
-                if (block.newline and _style.multiline) {
-                    _lines.pushBack(line);
-                    line = {{i + 1, 0}};
-                    adv = 0;
-                } else {
-                    adv += fullWidth;
-                }
-            }
-            first = false;
-        }
+    f64 _layoutHorizontaly(f64 width);
 
-        _lines.pushBack(line);
-    }
+    Math::Vec2f layout(f64 width);
 
-    f64 _layoutVerticaly() {
-        auto m = _style.font.metrics();
-        f64 baseline = m.linegap / 2;
-        for (auto &line : _lines) {
-            baseline += m.ascend;
-            line.baseline = baseline;
-            baseline += m.linegap + m.descend;
-        }
-        return baseline - m.linegap / 2;
-    }
+    // MARK: Paint -------------------------------------------------------------
 
-    f64 _layoutHorizontaly(f64 width) {
-        f64 maxWidth = 0;
-        for (auto &line : _lines) {
-            if (not line.blocks.any())
-                continue;
-
-            f64 pos = 0;
-            for (usize i = line.blocks.start; i < line.blocks.end(); i++) {
-                auto &block = _blocks[i];
-                block.pos = pos;
-                pos += block.fullWidth(*this);
-            }
-
-            auto lastBlock = _blocks[line.blocks.end() - 1];
-            line.width = lastBlock.pos + lastBlock.width;
-            maxWidth = max(maxWidth, line.width);
-            auto free = width - line.width;
-
-            switch (_style.align) {
-            case TextAlign::LEFT:
-                break;
-
-            case TextAlign::CENTER:
-                for (usize i = line.blocks.start; i < line.blocks.end(); i++) {
-                    auto &block = _blocks[i];
-                    block.pos += free / 2;
-                }
-                break;
-
-            case TextAlign::RIGHT:
-                for (usize i = line.blocks.start; i < line.blocks.end(); i++) {
-                    auto &block = _blocks[i];
-                    block.pos += free;
-                }
-                break;
-            }
-        }
-
-        return maxWidth;
-    }
-
-    Math::Vec2f layout(f64 width) {
-        if (isEmpty(_blocks)) {
-            return {};
-        }
-
-        if (not _blocksMeasured) {
-            _measureBlocks();
-            _blocksMeasured = true;
-        }
-
-        _wrapLines(width);
-        auto textHeight = _layoutVerticaly();
-        auto textWidth = _layoutHorizontaly(width);
-        return {textWidth, textHeight};
-    }
-
-    void paint(Context &ctx) const {
-        auto m = _style.font.metrics();
-        auto baseline = m.ascend + m.linegap / 2;
-
-        ctx.save();
-        ctx.textFont(_style.font);
-        if (_style.color)
-            ctx.fillStyle(*_style.color);
-
-        for (auto const &line : _lines) {
-            for (usize b = line.blocks.start; b < line.blocks.end(); b++) {
-                auto const &block = _blocks[b];
-
-                for (usize c = block.cells.start; c < block.cells.end(); c++) {
-                    auto const &cell = _cells[c];
-                    ctx.fill({block.pos + cell.pos, baseline}, cell.glyph);
-                }
-            }
-            baseline += m.linegap + m.descend + m.ascend;
-        }
-        ctx.restore();
-    }
+    void paint(Context &ctx) const;
 };
 
 } // namespace Karm::Gfx

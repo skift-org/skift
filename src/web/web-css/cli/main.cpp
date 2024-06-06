d18dc5339bd4ff121dc151c67e68f20825b13d78
VAN BOSSUYT Nicolas <me@smnx.sh>
2024-06-06 15:21:17 +0200

    web-css: Improved the debug cli.

--- src/web/web-css/cli/main.cpp
+++ src/web/web-css/cli/main.cpp
@@ -3,9 +3,44 @@
 #include <karm-sys/entry.h>
 #include <karm-sys/file.h>
 #include <web-css/mod.h>
+#include <web-css/parser.h>
 
-Async::Task<> entryPointAsync(Sys::Ctx &) {
-    Sys::println("Parsing:");
-    co_try$(Web::Css::fetchStylesheet("bundle://web-css.cli/exemple_1.css"_url));
-    co_return Ok();
+Async::Task<> entryPointAsync(Sys::Ctx &ctx) {
+    auto args = Sys::useArgs(ctx);
+
+    if (args.len() != 2) {
+        Sys::errln("usage: web-css.cli <verb> <url>\n");
+        co_return Error::invalidInput();
+    }
+
+    auto verb = args[0];
+    auto url = co_try$(Mime::parseUrlOrPath(args[1]));
+
+    if (verb == "dump-stylesheet") {
+        logInfo("fetching & parsing {}\n", url);
+        co_try$(Web::Css::fetchStylesheet(url));
+        logInfo("Stylesheet parsed\n");
+        Sys::println("{}", Web::Css::fetchStylesheet(url).unwrap());
+        co_return Ok();
+    } else if (verb == "dump-sst") {
+        auto file = co_try$(Sys::File::open(url));
+        auto buf = co_try$(Io::readAllUtf8(file));
+        Io::SScan s{buf};
+        Web::Css::Sst sst = co_try$(Web::Css::consumeRuleList(s));
+        Sys::println("{}", sst);
+        co_return Ok();
+    } else if (verb == "dump-token") {
+        auto file = co_try$(Sys::File::open(url));
+        auto buf = co_try$(Io::readAllUtf8(file));
+        Io::SScan s{buf};
+        Web::Css::Token tok;
+        do {
+            tok = co_try$(Web::Css::nextToken(s));
+            Sys::println("{}", tok);
+        } while (tok.type != Web::Css::Token::END_OF_FILE);
+        co_return Ok();
+    } else {
+        Sys::errln("unknown verb: {} (expected: dump-stylesheet, dump-sst, dump-token)\n", verb);
+        co_return Error::invalidInput();
+    }
 }

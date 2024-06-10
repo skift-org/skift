d998fbfaeee4888cbb675b684ca6121a367b9da5
VAN BOSSUYT Nicolas <me@smnx.sh>
2024-06-10 14:25:23 +0200

    grund: Added service meta files.

---
+++ src/apps/hideo-shell/srv/main.cpp
@@ -0,0 +1,35 @@
+#include <karm-sys/entry.h>
+#include <karm-ui/app.h>
+
+#include "../app.h"
+
+Async::Task<> entryPointAsync(Sys::Ctx &ctx) {
+    auto args = useArgs(ctx);
+    bool isMobile = Sys::useFormFactor() == Sys::FormFactor::MOBILE;
+
+    Hideo::Shell::State state = {
+        .isMobile = isMobile,
+        .dateTime = Sys::dateTime(),
+        .background = co_try$(Media::loadImageOrFallback("bundle://skift-wallpapers/images/abstract.qoi"_url)),
+        .noti = {},
+        .manifests = {
+            makeStrong<Hideo::Shell::Manifest>(Mdi::INFORMATION_OUTLINE, "About"s, Gfx::BLUE_RAMP),
+            makeStrong<Hideo::Shell::Manifest>(Mdi::CALCULATOR, "Calculator"s, Gfx::ORANGE_RAMP),
+            makeStrong<Hideo::Shell::Manifest>(Mdi::PALETTE_SWATCH, "Color Picker"s, Gfx::RED_RAMP),
+            makeStrong<Hideo::Shell::Manifest>(Mdi::COUNTER, "Counter"s, Gfx::GREEN_RAMP),
+            makeStrong<Hideo::Shell::Manifest>(Mdi::DUCK, "Demos"s, Gfx::YELLOW_RAMP),
+            makeStrong<Hideo::Shell::Manifest>(Mdi::FILE, "Files"s, Gfx::ORANGE_RAMP),
+            makeStrong<Hideo::Shell::Manifest>(Mdi::FORMAT_FONT, "Fonts"s, Gfx::BLUE_RAMP),
+            makeStrong<Hideo::Shell::Manifest>(Mdi::EMOTICON, "Hello World"s, Gfx::RED_RAMP),
+            makeStrong<Hideo::Shell::Manifest>(Mdi::IMAGE, "Icons"s, Gfx::GREEN_RAMP),
+            makeStrong<Hideo::Shell::Manifest>(Mdi::IMAGE, "Image Viewer"s, Gfx::YELLOW_RAMP),
+            makeStrong<Hideo::Shell::Manifest>(Mdi::CUBE, "Ray Tracer"s, Gfx::BLUE_RAMP),
+            makeStrong<Hideo::Shell::Manifest>(Mdi::COG, "Settings"s, Gfx::ZINC_RAMP),
+            makeStrong<Hideo::Shell::Manifest>(Mdi::TABLE, "Spreadsheet"s, Gfx::GREEN_RAMP),
+            makeStrong<Hideo::Shell::Manifest>(Mdi::WIDGETS, "Widget Gallery"s, Gfx::BLUE_RAMP),
+        },
+        .instances = {}
+    };
+
+    co_return Ui::runApp(ctx, Hideo::Shell::app(std::move(state)));
+}

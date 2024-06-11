01e9144a65544648b48846152a28c7879909c257
VAN BOSSUYT Nicolas <nivb@odoo.com>
2024-06-11 09:38:30 +0200

    web-style: Added a simple tool for listing supported props.

---
+++ src/web/web-style/cli/main.cpp
@@ -0,0 +1,26 @@
+#include <karm-io/emit.h>
+#include <karm-io/funcs.h>
+#include <karm-sys/entry.h>
+#include <karm-sys/file.h>
+#include <web-style/props.h>
+
+Async::Task<> entryPointAsync(Sys::Ctx &ctx) {
+    auto args = Sys::useArgs(ctx);
+
+    if (args.len() != 1) {
+        Sys::errln("usage: web-style.cli <verb>\n");
+        co_return Error::invalidInput();
+    }
+
+    auto verb = args[0];
+
+    if (verb == "list-props") {
+        Web::Style::Prop::foreach([]<typename T>(Meta::Type<T>) {
+            Sys::println("{}", T::name());
+        });
+        co_return Ok();
+    } else {
+        Sys::errln("unknown verb: {} (expected: dump-stylesheet, dump-sst, dump-token)\n", verb);
+        co_return Error::invalidInput();
+    }
+}

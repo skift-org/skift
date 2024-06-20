2c9c43030c60c82717b7bc6a10fe72aaca98d818
VAN BOSSUYT Nicolas <me@smnx.sh>
2024-06-20 13:14:17 +0200

    vaev-view: Hooked the rendering into the View.

--- src/web/vaev-view/view.cpp
+++ src/web/vaev-view/view.cpp
@@ -1,19 +1,30 @@
 #include <karm-ui/view.h>
 #include <vaev-base/color.h>
 
+#include "render.h"
 #include "view.h"
 
 namespace Vaev::View {
 
 struct View : public Ui::View<View> {
     Strong<Dom::Document> _dom;
+    Opt<Strong<Paint::Node>> _paintRoot;
 
     View(Strong<Dom::Document> dom) : _dom(dom) {}
 
     void paint(Gfx::Context &g, Math::Recti) override {
+        if (not _paintRoot)
+            _paintRoot = render(*_dom, bound().cast<Px>());
+
         g.save();
         g.clear(bound(), WHITE);
+        (*_paintRoot)->paint(g);
         g.restore();
+    }
+
+    void layout(Math::Recti bound) override {
+        _paintRoot = NONE;
+        Ui::View<View>::layout(bound);
     }
 
     Math::Vec2i size(Math::Vec2i, Ui::Hint) override {

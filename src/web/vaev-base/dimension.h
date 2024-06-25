3366a9259ce1ce0ccdccc55380e9cd1e3433e431
VAN BOSSUYT Nicolas <me@smnx.sh>
2024-06-25 14:02:32 +0200

    vaev-base: Implemented more units.

---
+++ src/web/vaev-base/dimension.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "angle.h"
+#include "frequency.h"
+#include "length.h"
+#include "resolution.h"
+#include "time.h"
+
+namespace Vaev {
+
+using _Dimension = Union<
+    Length,
+    Angle,
+    Time,
+    Frequency,
+    Resolution>;
+
+struct Dimension : public _Dimension {
+    using _Dimension::_Dimension;
+};
+
+} // namespace Vaev

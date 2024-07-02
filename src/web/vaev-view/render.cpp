cf5df1678a7f70192c201a16cc8a3dafddfcf625
VAN BOSSUYT Nicolas <nivb@odoo.com>
2024-07-02 11:48:44 +0200

    vaev-view: Load <link rel="stylesheet">.

--- src/web/vaev-view/render.cpp
+++ src/web/vaev-view/render.cpp
@@ -10,11 +10,26 @@
 namespace Vaev::View {
 
 static void _collectStyle(Dom::Node const &node, Style::StyleBook &sb) {
-    if (auto *el = node.is<Dom::Element>(); el and el->tagName == Html::STYLE) {
+    auto *el = node.is<Dom::Element>();
+    if (el and el->tagName == Html::STYLE) {
         auto text = el->textContent();
         Io::SScan textScan{text};
         auto sheet = Css::parseStylesheet(textScan);
         sb.add(std::move(sheet));
+    } else if (el and el->tagName == Html::LINK) {
+        auto rel = el->getAttribute(Html::REL_ATTR);
+        if (rel == "stylesheet"s) {
+            auto href = el->getAttribute(Html::HREF_ATTR);
+            if (not href)
+                return;
+            auto url = Mime::parseUrlOrPath(*href);
+            if (not url)
+                return;
+            auto sheet = Css::fetchStylesheet(url.take());
+            if (not sheet)
+                return;
+            sb.add(sheet.take());
+        }
     } else {
         for (auto &child : node.children())
             _collectStyle(*child, sb);
@@ -24,7 +39,11 @@ static void _collectStyle(Dom::Node const &node, Style::StyleBook &sb) {
 RenderResult render(Dom::Document const &dom, Style::Media const &media, Vec2Px viewport) {
     Style::StyleBook stylebook;
     stylebook.add(Css::fetchStylesheet("bundle://vaev-view/user-agent.css"_url).take("user agent stylesheet not available"));
+
+    auto start = Sys::now();
     _collectStyle(dom, stylebook);
+    auto elapsed = Sys::now() - start;
+    logDebug("style collection time: {}ms", elapsed.toUSecs() / 1000.0);
 
     Style::Computer computer{media, stylebook};
     auto layoutRoot = Layout::build(computer, dom);

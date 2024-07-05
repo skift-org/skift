6a5619b2c248d5bfeadc303e09a45e4bbc626808
VAN BOSSUYT Nicolas <me@smnx.sh>
2024-07-05 09:02:29 +0200

    karm-kira: Added animation to toggle().

--- src/libs/karm-kira/toggle.cpp
+++ src/libs/karm-kira/toggle.cpp
@@ -1,17 +1,23 @@
+#include <karm-ui/anim.h>
 #include <karm-ui/input.h>
 
 namespace Karm::Kira {
 
 struct Toggle : public Ui::View<Toggle> {
     bool _value = false;
+    Ui::Easedf _anim;
     Ui::OnChange<bool> _onChange;
     Ui::MouseListener _mouseListener;
 
     Toggle(bool value, Ui::OnChange<bool> onChange)
         : _value(value), _onChange(std::move(onChange)) {
+        _anim.set(*this, _value ? 1.0 : 0.0);
     }
 
     void reconcile(Toggle &o) override {
+        if (_value != o._value) {
+            _anim.animate(*this, _value ? 1.0 : 0.0, 0.1);
+        }
         _value = o._value;
         _onChange = std::move(o._onChange);
     }
@@ -19,46 +25,45 @@ struct Toggle : public Ui::View<Toggle> {
     void paint(Gfx::Context &g, Math::Recti) override {
         g.save();
 
-        auto thumb =
-            bound()
-                .hsplit(26)
-                .get(_value)
-                .shrink(_value ? 4 : 6);
+        Math::Recti thumb = {
+            bound().x + (isize)(6 + 26 * _anim.value()),
+            bound().y + 6,
+            14,
+            14,
+        };
 
-        if (_value) {
-            g.fillStyle(_mouseListener.isHover() ? Ui::ACCENT600 : Ui::ACCENT700);
-            g.fill(bound(), 999);
+        auto inactiveBackground = (_mouseListener.isHover() ? Ui::GRAY600 : Ui::GRAY700);
+        auto activeBackground = (_mouseListener.isHover() ? Ui::ACCENT600 : Ui::ACCENT700);
 
-            g.fillStyle(Gfx::WHITE);
-            g.fill(thumb, 999);
+        g.fillStyle(inactiveBackground.lerpWith(activeBackground, _anim.value()));
+        g.fill(bound(), 999);
 
-            if (_mouseListener.isPress()) {
-                g.strokeStyle(Gfx::stroke(Ui::ACCENT600)
-                                  .withWidth(1)
-                                  .withAlign(Gfx::INSIDE_ALIGN));
-                g.stroke(bound(), 999);
-            }
-        } else {
-            g.fillStyle(_mouseListener.isHover() ? Ui::GRAY600 : Ui::GRAY700);
-            g.fill(bound(), 999);
+        auto inactiveForeground = (_mouseListener.isHover() ? Ui::GRAY400 : Ui::GRAY500);
+        auto activeForeground = Gfx::WHITE;
 
-            g.fillStyle(_mouseListener.isHover() ? Ui::GRAY400 : Ui::GRAY500);
-            g.fill(thumb, 999);
+        g.fillStyle(inactiveForeground.lerpWith(activeForeground, _anim.value()));
+        g.fill(thumb, 999);
 
-            if (_mouseListener.isPress()) {
-                g.strokeStyle(Gfx::stroke(Ui::GRAY600)
-                                  .withWidth(1)
-                                  .withAlign(Gfx::INSIDE_ALIGN));
-                g.stroke(bound(), 999);
-            }
+        if (_mouseListener.isPress()) {
+            auto inactivePressed = Ui::GRAY600;
+            auto activePressed = Ui::ACCENT600;
+
+            g.strokeStyle(Gfx::stroke(_value ? activePressed : inactivePressed)
+                              .withWidth(1)
+                              .withAlign(Gfx::INSIDE_ALIGN));
+            g.stroke(bound(), 999);
         }
 
         g.restore();
     }
 
     void event(Sys::Event &e) override {
+        if (_anim.needRepaint(*this, e))
+            Ui::shouldRepaint(*this);
+
         if (_mouseListener.listen(*this, e)) {
             _value = not _value;
+            _anim.animate(*this, _value ? 1.0 : 0.0, 0.1);
             _onChange(*this, _value);
         }
     }

fdef2712014697da9266d6e98e996b91198c52e8
VAN BOSSUYT Nicolas <me@smnx.sh>
2024-08-29 08:33:53 +0200

    hideo-camera: Initial mockup of the ui.

--- src/apps/hideo-camera/app.cpp
+++ src/apps/hideo-camera/app.cpp
@@ -1,14 +1,88 @@
 #include <hideo-base/scafold.h>
+#include <karm-ui/input.h>
 
 #include "app.h"
 
 namespace Hideo::Camera {
 
+Ui::Child appContent() {
+    auto viewport = Ui::stack(
+        Ui::box({.backgroundFill = Gfx::BLUE400}, Ui::empty()),
+        Ui::canvas([](Gfx::Canvas &g, Math::Vec2i size) {
+            g.strokeStyle(Gfx::Stroke{
+                .fill = Gfx::WHITE.withOpacity(0.5),
+                .width = 1,
+            });
+
+            // Rule of third guidelines
+            g.stroke(Math::Edgei{0, size.y / 3, size.x, size.y / 3}.cast<f64>());
+            g.stroke(Math::Edgei{0, size.y * 2 / 3, size.x, size.y * 2 / 3}.cast<f64>());
+
+            g.stroke(Math::Edgei{size.x / 3, 0, size.x / 3, size.y}.cast<f64>());
+            g.stroke(Math::Edgei{size.x * 2 / 3, 0, size.x * 2 / 3, size.y}.cast<f64>());
+
+            // Focus reticle
+            g.stroke(Math::Ellipsef{size.cast<f64>() / 2., 32});
+        })
+    );
+
+    auto topBar =
+        Ui::hflow(
+            Ui::button(
+                Ui::NOP,
+                Ui::ButtonStyle::regular().withForegroundFill(Gfx::WHITE).withRadii(999),
+                Mdi::COG
+            ),
+
+            Ui::grow(NONE),
+
+            Ui::button(
+                Ui::NOP,
+                Ui::ButtonStyle::regular().withForegroundFill(Gfx::WHITE).withRadii(999),
+                Mdi::TUNE
+            )
+        ) |
+        Ui::insets({24, 24});
+
+    auto bottomBar =
+        Ui::hflow(
+            Ui::button(
+                Ui::NOP,
+                Ui::ButtonStyle::regular().withRadii(999).withPadding(12),
+                Media::Icon(Mdi::CAMERA_FLIP, 24)
+            ) | Ui::center(),
+
+            Ui::button(
+                Ui::NOP,
+                Ui::ButtonStyle::regular().withRadii(999).withPadding(16),
+                Media::Icon(Mdi::CAMERA, 38)
+            ) | Ui::center() |
+                Ui::grow(),
+            Ui::button(
+                Ui::NOP,
+                Ui::ButtonStyle::outline().withRadii(999).withPadding(12),
+                Ui::empty(24)
+            ) | Ui::center()
+        ) |
+        Ui::box({
+            .padding = {24, 16},
+            .backgroundFill = Gfx::BLACK.withOpacity(0.25),
+        });
+
+    return Ui::vflow(
+        Ui::stack(
+            viewport,
+            Ui::vflow(topBar)
+        ) | Ui::grow(),
+        bottomBar
+    );
+}
+
 Ui::Child app() {
     return Hideo::scafold({
         .icon = Mdi::CAMERA,
         .title = "Camera"s,
-        .body = slot$(Ui::empty()),
+        .body = appContent,
     });
 }
 

d20b87d1edfddc5742c52b04cf7120fca1e16125
VAN BOSSUYT Nicolas <me@smnx.sh>
2024-09-15 23:17:02 +0200

    karm-ui: Moved the inspector code to it's own source file.

---
+++ src/libs/karm-ui/app.cpp
@@ -0,0 +1,74 @@
+#include "app.h"
+
+#include "_embed.h"
+
+#include "host.h"
+#include "input.h"
+#include "layout.h"
+
+namespace Karm::Ui {
+
+Child inspector(Child child) {
+    return hflow(
+        child | Ui::grow(),
+        separator(),
+        vflow(
+            4,
+            icon(Mdi::FLASK, 24, GRAY600) | Ui::insets(6) | Ui::center(),
+            separator(),
+            button(
+                [](auto &n) {
+                    debugShowLayoutBounds = !debugShowLayoutBounds;
+                    Ui::shouldLayout(n);
+                },
+                ButtonStyle::subtle(),
+                Mdi::RULER_SQUARE
+            ),
+            button(
+                [](auto &n) {
+                    debugShowRepaintBounds = !debugShowRepaintBounds;
+                    Ui::shouldLayout(n);
+                },
+                ButtonStyle::subtle(),
+                Mdi::BRUSH
+            ),
+            button(
+                [](auto &n) {
+                    debugShowEmptyBounds = !debugShowEmptyBounds;
+                    Ui::shouldLayout(n);
+                },
+                ButtonStyle::subtle(),
+                Mdi::BORDER_NONE_VARIANT
+            ),
+            button(
+                [](auto &n) {
+                    debugShowScrollBounds = !debugShowScrollBounds;
+                    Ui::shouldLayout(n);
+                },
+                ButtonStyle::subtle(),
+                Mdi::ARROW_UP_DOWN
+            ),
+            button(
+                [](auto &n) {
+                    debugShowPerfGraph = !debugShowPerfGraph;
+                    Ui::shouldLayout(n);
+                },
+                ButtonStyle::subtle(),
+                Mdi::CHART_HISTOGRAM
+            )
+        ) |
+            Ui::insets(4) |
+            Ui::box({
+                .backgroundFill = GRAY800,
+            })
+    );
+}
+
+Res<> runApp(Sys::Context &ctx, Child root) {
+    auto &args = useArgs(ctx);
+    if (args.has("--debug"))
+        root = root | inspector;
+    return try$(_Embed::makeHost(root))->run();
+}
+
+} // namespace Karm::Ui
--- src/libs/karm-ui/app.h
+++ src/libs/karm-ui/app.h
@@ -2,78 +2,12 @@
 
 #include <karm-sys/context.h>
 
-#include "_embed.h"
-
-#include "host.h"
-#include "input.h"
-#include "layout.h"
-#include "reducer.h"
+#include "node.h"
 
 namespace Karm::Ui {
 
-Child inspector(Child child) {
-    return hflow(
-        child | Ui::grow(),
-        separator(),
-        vflow(
-            4,
-            icon(Mdi::FLASK, 24, GRAY600) | Ui::insets(6) | Ui::center(),
-            separator(),
-            button(
-                [](auto &n) {
-                    debugShowLayoutBounds = !debugShowLayoutBounds;
-                    Ui::shouldLayout(n);
-                },
-                ButtonStyle::subtle(),
-                Mdi::RULER_SQUARE
-            ),
-            button(
-                [](auto &n) {
-                    debugShowRepaintBounds = !debugShowRepaintBounds;
-                    Ui::shouldLayout(n);
-                },
-                ButtonStyle::subtle(),
-                Mdi::BRUSH
-            ),
-            button(
-                [](auto &n) {
-                    debugShowEmptyBounds = !debugShowEmptyBounds;
-                    Ui::shouldLayout(n);
-                },
-                ButtonStyle::subtle(),
-                Mdi::BORDER_NONE_VARIANT
-            ),
-            button(
-                [](auto &n) {
-                    debugShowScrollBounds = !debugShowScrollBounds;
-                    Ui::shouldLayout(n);
-                },
-                ButtonStyle::subtle(),
-                Mdi::ARROW_UP_DOWN
-            ),
-            button(
-                [](auto &n) {
-                    debugShowPerfGraph = !debugShowPerfGraph;
-                    Ui::shouldLayout(n);
-                },
-                ButtonStyle::subtle(),
-                Mdi::CHART_HISTOGRAM
-            )
-        ) |
-            Ui::insets(4) |
-            Ui::box({
-                .backgroundFill = GRAY800,
-            })
-    );
-}
+Child inspector(Child child);
 
-inline Res<> runApp(Sys::Context &ctx, Child root) {
-    auto &args = useArgs(ctx);
-    if (args.has("--debug")) {
-        root = inspector(root);
-    }
-    auto host = try$(_Embed::makeHost(root));
-    return host->run();
-}
+Res<> runApp(Sys::Context &ctx, Child root);
 
 } // namespace Karm::Ui

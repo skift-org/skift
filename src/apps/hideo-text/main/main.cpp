9f4657a50cc5da4facaeeb5b4585ddf1cfc73501
VAN BOSSUYT Nicolas <nivb@odoo.com>
2024-11-03 15:43:36 +0100

    hideo-text: Update the structure of the app.

---
+++ src/apps/hideo-text/main/main.cpp
@@ -0,0 +1,16 @@
+#include <karm-sys/entry.h>
+#include <karm-sys/file.h>
+#include <karm-ui/app.h>
+
+#include "../app.h"
+
+Async::Task<> entryPointAsync(Sys::Context &ctx) {
+    auto &args = useArgs(ctx);
+    Opt<Mime::Url> url;
+    Res<String> text = Ok(""s);
+    if (args.len()) {
+        url = co_try$(Mime::parseUrlOrPath(args[0]));
+        text = Sys::readAllUtf8(*url);
+    }
+    co_return Ui::runApp(ctx, Hideo::Text::app(url, text));
+}

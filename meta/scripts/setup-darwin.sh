c275f9c539e340b9b7e0d09b24ca7c4c27f3e069
Akinimaginable <akn@etrange.org>
2024-11-13 00:54:16 +0100

    meta: Added brew checks, lld, qemu (#431)

--- meta/scripts/setup-darwin.sh
+++ meta/scripts/setup-darwin.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-brew install nasm llvm sdl2 python3 ninja libmagic pkg-config
+if ! command -v brew &> /dev/null
+then
+    echo "Homebrew is not installed. Please install Homebrew first."
+    echo '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
+    exit 1
+fi
+
+brew install libmagic lld llvm nasm ninja pkg-config python3 qemu sdl2

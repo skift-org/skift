b3712e98b2a56aab818ebe165a833a8fb7c38f0c
VAN BOSSUYT Nicolas <me@smnx.sh>
2024-11-24 15:16:09 +0100

    karm-scene: Added image support.

--- src/libs/karm-scene/image.h
+++ src/libs/karm-scene/image.h
@@ -1,10 +1,30 @@
 #pragma once
 
+#include <karm-image/picture.h>
+
 #include "base.h"
 
 namespace Karm::Scene {
 
 struct Image : public Node {
+    Math::Rectf _bound;
+    ::Image::Picture _picture;
+
+    Image(Math::Rectf bound, ::Image::Picture picture)
+        : _bound(bound), _picture(std::move(picture)) {
+    }
+
+    Math::Rectf bound() override {
+        return _bound;
+    }
+
+    void paint(Gfx::Canvas &ctx, Math::Rectf r) override {
+        if (not r.colide(bound()))
+            return;
+
+        ctx.blit(_bound.cast<isize>(), _picture.pixels());
+    }
+
     void repr(Io::Emit &e) const override {
         e("(image z:{})", zIndex);
     }

932e9c4672c9c6a09f6495a85b9f00542cd18a23
VAN BOSSUYT Nicolas <nivb@odoo.com>
2024-12-08 13:35:31 +0100

    meta: Added ccache deps.

--- meta/scripts/setup-ubuntu.sh
+++ meta/scripts/setup-ubuntu.sh
@@ -5,5 +5,5 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 
 apt update
-apt install build-essential git ninja-build libsdl2-dev nasm gcc-multilib qemu-system-x86 mtools liburing-dev
+apt install ccache build-essential git ninja-build libsdl2-dev nasm gcc-multilib qemu-system-x86 mtools liburing-dev
 bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)" llvm 19

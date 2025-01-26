058846f60dfad7ba6b0e950d04b78e50b7c9abf6
VAN BOSSUYT Nicolas <nivb@odoo.com>
2025-01-26 15:08:32 +0100

    meta: Fix build on ubuntu.

--- meta/scripts/setup-ubuntu.sh
+++ meta/scripts/setup-ubuntu.sh
@@ -4,6 +4,6 @@ set -e
 
 export DEBIAN_FRONTEND=noninteractive
 
-apt update
-apt install ccache build-essential git ninja-build libsdl2-dev nasm gcc-multilib qemu-system-x86 mtools liburing-dev
-bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)" llvm 19
+apt-get update -y --no-install-recommends
+apt-get install -y --no-install-recommends ccache build-essential git ninja-build libsdl2-dev nasm gcc-multilib qemu-system-x86 mtools liburing-dev
+bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)" llvm 19 all

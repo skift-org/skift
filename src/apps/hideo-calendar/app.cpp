be22e168ebc1df298b0fdb2d07ec8e135c15c196
sleepy-monax <me@smnx.sh>
2025-03-08 08:01:25 +0100

    hideo-calendar: Migrated to C++20 modules.

--- src/apps/hideo-calendar/app.cpp
+++ src/apps/hideo-calendar/app.cpp
@@ -1,12 +1,14 @@
+module;
+
 #include <karm-kira/scaffold.h>
 #include <karm-ui/layout.h>
 #include <mdi/calendar.h>
 
-#include "app.h"
+export module Hideo.Calendar;
 
 namespace Hideo::Calendar {
 
-Ui::Child app() {
+export Ui::Child app() {
     return Kr::scaffold({
         .icon = Mdi::CALENDAR,
         .title = "Calendar"s,

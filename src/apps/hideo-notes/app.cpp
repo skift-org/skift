f8f810b23824f514723bd3e134fa542273607e80
sleepy-monax <me@smnx.sh>
2025-03-09 09:23:57 +0100

    hideo-notes: Migrated to C++20 modules.

--- src/apps/hideo-notes/app.cpp
+++ src/apps/hideo-notes/app.cpp
@@ -1,12 +1,14 @@
+module;
+
 #include <karm-kira/scaffold.h>
 #include <karm-ui/layout.h>
 #include <mdi/format-list-bulleted.h>
 
-#include "app.h"
+export module Hideo.Notes;
 
 namespace Hideo::Notes {
 
-Ui::Child app() {
+export Ui::Child app() {
     return Kr::scaffold({
         .icon = Mdi::FORMAT_LIST_BULLETED,
         .title = "Notes"s,

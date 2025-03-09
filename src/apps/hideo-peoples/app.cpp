e5195aea8b89e737e6d6d655b962bec716a16eeb
sleepy-monax <me@smnx.sh>
2025-03-09 09:24:05 +0100

    hideo-peoples: Migrated to C++20 modules.

--- src/apps/hideo-peoples/app.cpp
+++ src/apps/hideo-peoples/app.cpp
@@ -1,12 +1,14 @@
+module;
+
 #include <karm-kira/scaffold.h>
 #include <karm-ui/layout.h>
 #include <mdi/account.h>
 
-#include "app.h"
+export module Hideo.Peoples;
 
 namespace Hideo::Peoples {
 
-Ui::Child app() {
+export Ui::Child app() {
     return Kr::scaffold({
         .icon = Mdi::ACCOUNT,
         .title = "Peoples"s,

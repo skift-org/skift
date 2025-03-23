e602dfed5e28418c4574519a6a3bc7ff9b733bf3
sleepy-monax <me@smnx.sh>
2025-03-23 12:43:21 +0100

    meta: Bump llvm to 20.

--- meta/scripts/setup-ubuntu.sh
+++ meta/scripts/setup-ubuntu.sh
@@ -6,4 +6,4 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update -y --no-install-recommends
 apt-get install -y --no-install-recommends ccache build-essential git ninja-build libsdl2-dev nasm gcc-multilib qemu-system-x86 mtools liburing-dev
-bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)" llvm 19 all
+bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)" llvm 20 all
